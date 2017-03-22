3261c537d7972c82b25366ec4a2ad61692c25565
aaronsace <aaronsace@hotmail.com>
2017-03-22 23:46:46 +0000

    Revert "Improvement"
    This reverts commit be4d991c52d039056fe77251aa3d2a7135665e4e.

--- Miners/NheqminerCpu.ps1
+++ Miners/NheqminerCpu.ps1
@@ -5,12 +5,10 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Port = 3334
 
-$Threads = 2
-
 [PSCustomObject]@{
     Type = 'CPU'
     Path = $Path
-    Arguments = -Join ('-a ', $Port, ' -l $($Pools.Equihash.Host):$($Pools.Equihash.Port) -u $($Pools.Equihash.User) -t ', $Threads)
+    Arguments = -Join ('-a ', $Port, ' -l $($Pools.Equihash.Host):$($Pools.Equihash.Port) -u $($Pools.Equihash.User)')
     HashRates = [PSCustomObject]@{Equihash = '$($Stats.' + $Name + '_Equihash_HashRate.Week)'}
     API = 'Nheqminer'
     Port = $Port
