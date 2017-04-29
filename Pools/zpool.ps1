549de30f39fbc314b07b6597015d0d7815ea0a15
snakethejake81 <jake@jakewarren.com>
2017-04-29 12:20:34 -0700

    Added Algos
    Decred, Lbry and Sib

--- Pools/zpool.ps1
+++ Pools/zpool.ps1
@@ -18,19 +18,41 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Locations = 'US'
 
-#Can filter algos here
-$algos = $zpool_Request | Get-Member -Type NoteProperty | Select Name | Where {$_.Name -in "Equihash","ethash","Lyra2z","Cryptonight","Sia","Yescrypt","BlakeVanilla","Lyra2RE2","Skein","Quibit","NeoScrypt","Groestl","lbry"}
+$algos = $zpool_Request | Get-Member -Type NoteProperty | Select Name | Where {$_.Name -in 
+                                                                                    "Cryptonight",
+                                                                                    "Equihash",
+                                                                                    "Ethash",
+                                                                                    "Groestl",
+                                                                                    "Keccak",
+                                                                                    "Lyra2RE2",
+                                                                                    "Lyra2z",
+                                                                                    "Myriad-Groestl",
+                                                                                    "NeoScrypt",
+                                                                                    "Quibit",
+                                                                                    "Scrypt",
+                                                                                    "Sia",
+                                                                                    "Skein",
+                                                                                    "X11",
+                                                                                    "Yescrypt",
+                                                                                    "Lbry",
+                                                                                    "Decred",
+                                                                                    "Sib"}
 
 $algos | foreach { 
 	$Algorithm = $zpool_Request."$($_.name)".name
 	$Coin = (Get-Culture).TextInfo.ToTitleCase(($zpool_Request."$($_.name)".name -replace "-", " ")) -replace " "
 	
-    #Profits appear to be calculated differently between the algos on zpool, need to account for this.
+    #Switch Profit calculation based on algo.
 	switch ("$($_.name)")
         {
             'equihash' 
                 {
                     $Stat = Set-Stat -Name "$($Name)_$($Algorithm)_Profit" -Value ([decimal]$zpool_Request.$Algorithm.estimate_current/1000)
+                    $Price = (($Stat.Live*(1-[Math]::Min($Stat.Day_Fluctuation,1)))+($Stat.Day*(0+[Math]::Min($Stat.Day_Fluctuation,1)))) 
+                }
+            'decred' 
+                {
+                    $Stat = Set-Stat -Name "$($Name)_$($Algorithm)_Profit" -Value ([decimal]$zpool_Request.$Algorithm.estimate_current/1000000000)
                     $Price = (($Stat.Live*(1-[Math]::Min($Stat.Day_Fluctuation,1)))+($Stat.Day*(0+[Math]::Min($Stat.Day_Fluctuation,1)))) 
                 }
             default
