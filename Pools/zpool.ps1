6a8c2aa48d2d1d74d1dc2b1311d8684bd3342c66
snakethejake81 <jake@jakewarren.com>
2017-04-29 11:04:57 -0700

    Added filter for algos and adjusted profit calc for equihash
    Need to invetigate other algo profits for additionals switching.

--- Pools/zpool.ps1
+++ Pools/zpool.ps1
@@ -18,18 +18,28 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Locations = 'US'
 
-$Wallet = ''
-
 #Can filter algos here
-$algos = $zpool_Request | Get-Member -Type NoteProperty | Select Name
+$algos = $zpool_Request | Get-Member -Type NoteProperty | Select Name | Where {$_.Name -in "Equihash","ethash","Lyra2z","Cryptonight","Sia","Yescrypt","BlakeVanilla","Lyra2RE2","Skein","Quibit","NeoScrypt","Groestl","lbry"}
 
 $algos | foreach { 
 	$Algorithm = $zpool_Request."$($_.name)".name
 	$Coin = (Get-Culture).TextInfo.ToTitleCase(($zpool_Request."$($_.name)".name -replace "-", " ")) -replace " "
 	
     #Profits appear to be calculated differently between the algos on zpool, need to account for this.
-	$Stat = Set-Stat -Name "$($Name)_$($Algorithm)_Profit" -Value ([decimal]$zpool_Request."$($_.name)".estimate_current/1000000) 
-	$Price = (($Stat.Live*(1-[Math]::Min($Stat.Day_Fluctuation,1)))+($Stat.Day*(0+[Math]::Min($Stat.Day_Fluctuation,1))))
+	switch ("$($_.name)")
+        {
+            'equihash' 
+                {
+                    $Stat = Set-Stat -Name "$($Name)_$($Algorithm)_Profit" -Value ([decimal]$zpool_Request.$Algorithm.estimate_current/1000)
+                    $Price = (($Stat.Live*(1-[Math]::Min($Stat.Day_Fluctuation,1)))+($Stat.Day*(0+[Math]::Min($Stat.Day_Fluctuation,1)))) 
+                }
+            default
+                {
+                    $Stat = Set-Stat -Name "$($Name)_$($Algorithm)_Profit" -Value ([decimal]$zpool_Request.$Algorithm.estimate_current/1000000)
+                    $Price = (($Stat.Live*(1-[Math]::Min($Stat.Day_Fluctuation,1)))+($Stat.Day*(0+[Math]::Min($Stat.Day_Fluctuation,1)))) 
+                }
+        }
+	
 	
 	[PSCustomObject]@{
 		Algorithm = $Algorithm
