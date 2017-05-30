e495efa98675a8c87f4779fd2b55c4f63bb2a7d3
aaronsace <aaronsace@hotmail.com>
2017-05-30 19:16:41 +0100

    Improved Skein

---
+++ Miners/SgminerSkein.ps1
@@ -0,0 +1,25 @@
+﻿$Path = '.\Bin\Skein-AMD\sgminer.exe'
+$Uri = 'https://github.com/miningpoolhub/sgminer/releases/download/5.3.1/Release.zip'
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Algorithms = [PSCustomObject]@{
+    Skein = 'skeincoin'
+}
+
+$Optimizations = [PSCustomObject]@{
+    Skein = ' --gpu-threads 2 --worksize 256 --intensity 23'
+}
+
+$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{
+        Type = 'AMD'
+        Path = $Path
+        Arguments = -Join ('--api-listen -k ', $Algorithms.$_, ' -o $($Pools.', $_, '.Protocol)://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
+        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
+        API = 'Xgminer'
+        Port = 4028
+        Wrap = $false
+        URI = $Uri
+    }
+}
