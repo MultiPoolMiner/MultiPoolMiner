da143446bf2c8bf785f935356a3d96fb39036b2a
aaronsace <aaronsace@hotmail.com>
2017-06-09 19:57:49 +0100

    Housekeeping

--- Miners/CpuminerJayddeeSse42.ps1
+++ Miners/CpuminerJayddeeSse42.ps1
@@ -1,34 +1,48 @@
-﻿$Path = '.\Bin\CPU-JayDDee\cpuminer-sse42.exe'
-$Uri = 'https://github.com/JayDDee/cpuminer-opt/releases/download/v3.6.3/cpuminer-opt-3.6.3-windows.zip'
+﻿. .\Include.ps1
+
+$Path = ".\Bin\CPU-JayDDee\cpuminer-sse42.exe"
+$Uri = "https://github.com/JayDDee/cpuminer-opt/releases/download/v3.6.3/cpuminer-opt-3.6.3-windows.zip"
+
+$Commands = [PSCustomObject]@{
+    #"bitcore" = "" #Bitcore
+    #"blake2s" = "" #Blake2s
+    #"blakecoin" = "" #Blakecoin
+    #"vanilla" = "" #BlakeVanilla
+    "cryptonight" = "" #Cryptonight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = "" #Groestl
+    "hmq1725" = "" #hmq1725
+    #"keccak" = "" #Keccak
+    #"lbry" = "" #Lbry
+    #"lyra2v2" = "" #Lyra2RE2
+    "lyra2z" = "" #Lyra2z
+    #"myr-gr" = "" #MyriadGroestl
+    #"neoscrypt" = "" #NeoScrypt
+    #"nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    #"qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sia" = "" #Sia
+    #"sib" = "" #Sib
+    #"skein" = "" #Skein
+    #"timetravel" = "" #Timetravel
+    #"x11" = "" #X11
+    #"x11evo" = "" #X11evo
+    #"x17" = "" #X17
+    #"yescrypt" = "" #Yescrypt
+}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Algorithms = [PSCustomObject]@{
-    Lyra2z = 'lyra2z' #not supported
-    #Equihash = 'equihash' #not supported
-    Cryptonight = 'cryptonight'
-    #Ethash = 'ethash' #not supported
-    #Sia = 'sia' #not supported
-    #Yescrypt = 'yescrypt' #use TpruvoT
-    #BlakeVanilla = 'vanilla' #use TpruvoT
-    #Lyra2RE2 = 'lyra2v2' #use TpruvoT
-    #Skein = 'skein' #use TpruvoT
-    #Qubit = 'qubit' #use TpruvoT
-    #NeoScrypt = 'neoscrypt' #use TpruvoT
-    #X11 = 'x11' #use TpruvoT
-    #MyriadGroestl = 'myr-gr' #use TpruvoT
-    #Groestl = 'groestl' #use TpruvoT
-    #Keccak = 'keccak' #use TpruvoT
-    #Scrypt = 'scrypt' #use TpruvoT
-}
-
-$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
     [PSCustomObject]@{
-        Type = 'CPU'
+        Type = "CPU"
         Path = $Path
-        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)')
-        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
-        API = 'Ccminer'
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$Name_$(Get-Algorithm($_))_HashRate".Week}
+        API = "Ccminer"
         Port = 4048
         Wrap = $false
         URI = $Uri
--- Miners/CpuminerTpruvotCore2.ps1
+++ Miners/CpuminerTpruvotCore2.ps1
@@ -1,34 +1,48 @@
-﻿$Path = '.\Bin\CPU-TPruvot\cpuminer-gw64-core2.exe'
-$Uri = 'https://github.com/tpruvot/cpuminer-multi/releases/download/v1.3.1-multi/cpuminer-multi-rel1.3.1-x64.zip'
+﻿. .\Include.ps1
+
+$Path = ".\Bin\CPU-TPruvot\cpuminer-gw64-core2.exe"
+$Uri = "https://github.com/tpruvot/cpuminer-multi/releases/download/v1.3.1-multi/cpuminer-multi-rel1.3.1-x64.zip"
+
+$Commands = [PSCustomObject]@{
+    "bitcore" = "" #Bitcore
+    "blake2s" = "" #Blake2s
+    "blakecoin" = "" #Blakecoin
+    "vanilla" = "" #BlakeVanilla
+    "cryptonight" = "" #Cryptonight
+    "decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    "groestl" = "" #Groestl
+    #"hmq1725" = "" #hmq1725
+    "keccak" = "" #Keccak
+    "lbry" = "" #Lbry
+    "lyra2v2" = "" #Lyra2RE2
+    "lyra2z" = "" #Lyra2z
+    "myr-gr" = "" #MyriadGroestl
+    "neoscrypt" = "" #NeoScrypt
+    "nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    "qubit" = "" #Qubit
+    "scrypt" = "" #Scrypt
+    "sia" = "" #Sia
+    "sib" = "" #Sib
+    "skein" = "" #Skein
+    "timetravel" = "" #Timetravel
+    "x11" = "" #X11
+    "x11evo" = "" #X11evo
+    "x17" = "" #X17
+    "yescrypt" = "" #Yescrypt
+}
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Algorithms = [PSCustomObject]@{
-    #Lyra2z = 'lyra2z' #not supported
-    #Equihash = 'equihash' #not supported
-    Cryptonight = 'cryptonight'
-    #Ethash = 'ethash' #not supported
-    Sia = 'sia'
-    Yescrypt = 'yescrypt'
-    BlakeVanilla = 'vanilla'
-    Lyra2RE2 = 'lyra2v2'
-    Skein = 'skein'
-    Qubit = 'qubit'
-    NeoScrypt = 'neoscrypt'
-    X11 = 'x11'
-    MyriadGroestl = 'myr-gr'
-    Groestl = 'groestl'
-    Keccak = 'keccak'
-    Scrypt = 'scrypt'
-}
-
-$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
     [PSCustomObject]@{
-        Type = 'CPU'
+        Type = "CPU"
         Path = $Path
-        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -u $($Pools.', $_, '.User) -p $($Pools.', $_, '.Pass)')
-        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
-        API = 'Ccminer'
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$Name_$(Get-Algorithm($_))_HashRate".Week}
+        API = "Ccminer"
         Port = 4048
         Wrap = $false
         URI = $Uri
