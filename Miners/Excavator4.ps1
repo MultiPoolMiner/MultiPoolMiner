3d9db7ebae5a69da6d8bfd63abea455971db1686
aaronsace <aaronsace@hotmail.com>
2017-06-13 01:03:54 +0100

    Housekeeping

--- Miners/Excavator4.ps1
+++ Miners/Excavator4.ps1
@@ -1,39 +1,31 @@
-﻿$ThreadIndex = 4
+﻿. .\Include.ps1
+
+$ThreadIndex = 4
 $Threads = 2
 
 $Path = ".\Bin\Excavator\excavator.exe"
-$Uri = 'https://github.com/nicehash/excavator/releases/'
+$Uri = "https://github.com/nicehash/excavator/releases/"
 
-$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
-
-$Algorithms = [PSCustomObject]@{
-    #Decred = 'decred'
-    #Pascal = 'pascal'
-    Equihash = 'equihash'
-    Sia = 'sia'
+$Commands = [PSCustomObject]@{
+    "decred" = @() #Decred
+    "equihash" = @() #Equihash
+    "pascal" = @() #Pascal
+    "sia" = @() #Sia
 }
 
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $Port = 3456+($ThreadIndex*10000)
 
-$Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
-    try
-    {
-        $Config = Get-Content "$(Split-Path $Path)\default_command_file.json" -ErrorAction Stop | ConvertFrom-Json
-        $Config[0].commands[0].params[0] = $Algorithms.$_
-        $Config[0].commands[0].params[1] = "$($Pools.$_.Host):$($Pools.$_.Port)"
-        $Config[0].commands[0].params[2] = "$($Pools.$_.User):$($Pools.$_.Pass)"
-        $Config[1].commands = @(@{id = 1; method = "worker.add"; params = @("0","$ThreadIndex")})*$Threads
-        ($Config | ConvertTo-Json -Depth 10) | Set-Content "$(Split-Path $Path)\$_$ThreadIndex.json"
-    }
-    catch
-    {
-    }
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    [PSCustomObject]@{time=0; commands=@([PSCustomObject]@{id=1; method="algorithm.add"; params=@("$_", "$($Pools.$_.Host):$($Pools.$_.Port)", "$($Pools.$_.User):$($Pools.$_.Pass)")})},
+    [PSCustomObject]@{time=3; commands=@([PSCustomObject]@{id=1; method="worker.add"; params=@("0", "$ThreadIndex")+$Commands.$_})*$Threads},
+    [PSCustomObject]@{time=10; commands=@([PSCustomObject]@{id=1; method="algorithm.print.speeds"; params=@("0")})} | ConvertTo-Json -Depth 10 | Set-Content "$(Split-Path $Path)\$_$ThreadIndex.json" -Force -ErrorAction SilentlyContinue
 
     [PSCustomObject]@{
-        Type = 'AMD','NVIDIA'
+        Type = "AMD","NVIDIA"
         Path = $Path
-        Arguments = -Join ('-p ', $Port, ' -c ', $_, $ThreadIndex, '.json')
-        HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
+        Arguments = "-p $Port -c $_$ThreadIndex.json"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = 'NiceHash'
         Port = $Port
         Wrap = $false
