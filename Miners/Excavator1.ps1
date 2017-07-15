6641751a787a02e2af2e47f6be319db5b64ea01d
aaronsace <aaronsace@users.noreply.github.com>
2017-07-16 00:06:35 +0300

    Fix DaggerHashimoto

--- Miners/Excavator1.ps1
+++ Miners/Excavator1.ps1
@@ -18,7 +18,7 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 $Port = 3456 + ($ThreadIndex * 10000)
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
-    [PSCustomObject]@{time = 0; commands = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$_", "$($Pools.$_.Host):$($Pools.$_.Port)", "$($Pools.$_.User):$($Pools.$_.Pass)")})},
+    [PSCustomObject]@{time = 0; commands = @([PSCustomObject]@{id = 1; method = "algorithm.add"; params = @("$_", "$($Pools.$(Get-Algorithm($_)).Host):$($Pools.$(Get-Algorithm($_)).Port)", "$($Pools.$(Get-Algorithm($_)).User):$($Pools.$(Get-Algorithm($_)).Pass)")})},
     [PSCustomObject]@{time = 3; commands = @([PSCustomObject]@{id = 1; method = "worker.add"; params = @("0", "$ThreadIndex") + $Commands.$_}) * $Threads},
     [PSCustomObject]@{time = 10; commands = @([PSCustomObject]@{id = 1; method = "algorithm.print.speeds"; params = @("0")})} | ConvertTo-Json -Depth 10 | Set-Content "$(Split-Path $Path)\$_$ThreadIndex.json" -Force -ErrorAction SilentlyContinue
 
