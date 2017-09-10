9640b20cae8f3ef105ff8a3d012635dc70c9a706
aaronsace <aaronsace@hotmail.com>
2017-09-10 23:05:38 +0100

    Add bat files

---
+++ Start-Zpool.bat
@@ -0,0 +1,7 @@
+setx GPU_FORCE_64BIT_PTR 1
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "&.\multipoolminer.ps1 -wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -username aaronsace -workername multipoolminer -ssl -region europe -currency btc,usd,eur -type amd,nvidia,cpu -poolname zpool -algorithm decred,equihash,groestl,lbry,neoscrypt,sib -donate 10"
