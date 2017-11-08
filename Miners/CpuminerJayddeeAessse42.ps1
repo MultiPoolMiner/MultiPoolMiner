98ea8fca21bcfa7a0ab22a029e1b54671d0118fd
aaronsace <aaronsace@hotmail.com>
2017-11-08 13:31:32 +0000

    Revert "Fix SSL"
    This reverts commit 16b33b8779d783db6d52efa37e18e0613b68d824.

--- Miners/CpuminerJayddeeAessse42.ps1
+++ Miners/CpuminerJayddeeAessse42.ps1
@@ -42,13 +42,11 @@ $Commands = [PSCustomObject]@{
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
-if ($Pools.CryptoNight.Protocol -ne "stratum+tcp") {return} #temp fix
-
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     [PSCustomObject]@{
         Type = "CPU"
         Path = $Path
-        Arguments = "-a $_ -o $($Pools.CryptoNight.Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
         API = "Ccminer"
         Port = 4048
