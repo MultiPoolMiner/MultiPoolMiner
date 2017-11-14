8dbdd0267322569751a0abc83576338dd7b28ad7
aaronsace <aaronsace@hotmail.com>
2017-11-14 21:19:38 +0000

    Fix

--- RemoveLogs.bat
+++ RemoveLogs.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 set /p execute= This process will delete all unnecessary log files created by the miners and MultiPoolMiner to free up space. Are you sure you want to continue? [Y/N] 
 IF /I "%execute%"=="Y" (
 	if exist "Bin\Cryptonight-Claymore\*_log.txt" del "Bin\Cryptonight-Claymore\*_log.txt"
--- ResetProfit.bat
+++ ResetProfit.bat
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 set /p statreset= This process will remove all accumulated coin data and reset your profit statistics. Are you sure you want to continue? [Y/N] 
 IF /I "%statreset%"=="Y" (
 	if exist "Stats\*Profit.txt" del "Stats\*Profit.txt"
