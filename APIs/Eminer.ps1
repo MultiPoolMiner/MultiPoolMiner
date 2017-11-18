63d01c59ddaf72ea2dfbdac7569297e94f6da4bf
aaronsace <aaronsace@hotmail.com>
2017-11-18 12:49:48 +0000

    Added Eminer

---
+++ APIs/Eminer.ps1
@@ -0,0 +1,46 @@
+﻿using module ..\Include.psm1
+
+class Eminer : Miner {
+    [PSCustomObject]GetHashRate ([String[]]$Algorithm, [Bool]$Safe = $false) {
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Delta = 0.05
+        $Interval = 5
+        $HashRates = @()
+
+        $Request = ""
+
+        do {
+            $HashRates += $HashRate = [PSCustomObject]@{}
+
+            try {
+                $Response = Invoke-WebRequest "http://$($Server):$($this.Port)/api/v1/stats" -UseBasicParsing -TimeoutSec $Timeout -ErrorAction Stop
+                $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+            }
+            catch {
+                Write-Warning "Failed to connect to miner ($($this.Name)). "
+                break
+            }
+
+            $HashRate_Name = [String]$Algorithm[0]
+            $HashRate_Value = [Double]($Data.devices.hashrate_1m | Measure-Object -Sum).Sum
+
+            if ($HashRate_Name -and ($Algorithm -like (Get-Algorithm $HashRate_Name)).Count -eq 1) {
+                $HashRate | Add-Member @{(Get-Algorithm $HashRate_Name) = [Int64]$HashRate_Value}
+            }
+
+            $Algorithm | Where-Object {-not $HashRate.$_} | ForEach-Object {break}
+
+            if (-not $Safe) {break}
+
+            Start-Sleep $Interval
+        } while ($HashRates.Count -lt 6)
+
+        $HashRate = [PSCustomObject]@{}
+        $Algorithm | ForEach-Object {$HashRate | Add-Member @{$_ = [Int64]($HashRates.$_ | Measure-Object -Maximum -Minimum -Average | Where-Object {$_.Maximum - $_.Minimum -le $_.Average * $Delta}).Maximum}}
+        $Algorithm | Where-Object {-not $HashRate.$_} | Select-Object -First 1 | ForEach-Object {$Algorithm | ForEach-Object {$HashRate.$_ = [Int64]0}}
+
+        return $HashRate
+    }
+}
