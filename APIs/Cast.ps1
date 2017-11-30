ac27eba93594781b575f07175ebd76ac9fa67ff3
aaronsace <aaronsace@hotmail.com>
2017-11-30 00:32:57 +0000

    Added Cast XMR Miner

---
+++ APIs/Cast.ps1
@@ -0,0 +1,54 @@
+﻿using module ..\Include.psm1
+
+class Cast : Miner {
+    [PSCustomObject]GetHashRate ([String[]]$Algorithm, [Bool]$Safe = $false) {
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Delta = 0.05
+        $Interval = 5
+        $HashRates = @()
+
+        $Request = ""
+
+        do {
+            $HashRates += $HashRate = [PSCustomObject]@{}
+
+            try {
+                $Response = Invoke-WebRequest "http://$($Server):$($this.Port)/" -UseBasicParsing -TimeoutSec $Timeout -ErrorAction Stop
+                $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+            }
+            catch {
+                Write-Warning "Failed to connect to miner ($($this.Name)). "
+                break
+            }
+
+            $HashRate_Name = [String]$Algorithm[0]
+            $HashRate_Value = [Double]($Data.devices.hash_rate | Measure-Object -Sum).Sum / 1000
+
+            $HashRate | Where-Object {$HashRate_Name} | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+
+            $Algorithm | Where-Object {-not $HashRate.$_} | ForEach-Object {break}
+
+            if (-not $Safe) {break}
+
+            $HashRate_Value = [Double]($Data.devices.hash_rate_avg | Measure-Object -Sum).Sum / 1000
+
+            if ($HashRate_Value) {
+                $HashRates += $HashRate = [PSCustomObject]@{}
+
+                $HashRate | Where-Object {$HashRate_Name} | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+
+                $Algorithm | Where-Object {-not $HashRate.$_} | ForEach-Object {break}
+            }
+
+            Start-Sleep $Interval
+        } while ($HashRates.Count -lt 6)
+
+        $HashRate = [PSCustomObject]@{}
+        $Algorithm | ForEach-Object {$HashRate | Add-Member @{$_ = [Int64]($HashRates.$_ | Measure-Object -Maximum -Minimum -Average | Where-Object {$_.Maximum - $_.Minimum -le $_.Average * $Delta}).Maximum}}
+        $Algorithm | Where-Object {-not $HashRate.$_} | Select-Object -First 1 | ForEach-Object {$Algorithm | ForEach-Object {$HashRate.$_ = [Int64]0}}
+
+        return $HashRate
+    }
+}
