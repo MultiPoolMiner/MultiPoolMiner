6fa3481e2bd6799d59343b18368274d6a286565a
Grant Emsley <gemsley@st-leonards.com>
2017-12-20 12:50:28 -0500

    Show current balances on pools

---
+++ Balances/AHashPool.ps1
@@ -0,0 +1,30 @@
+﻿using module ..\Include.psm1
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Request = [PSCustomObject]@{}
+
+if(!$Wallet) {
+  Write-Log -Level Warn "Pool API ($Name) has failed - no wallet address specified."
+  return
+}
+
+try {
+    $Request = Invoke-RestMethod "http://www.ahashpool.com/api/wallet?address=$Wallet" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop
+}
+catch {
+    Write-Log -Level Warn "Pool API ($Name) has failed. "
+}
+
+if (($Request | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
+    Write-Log -Level Warn "Pool API ($Name) returned nothing. "
+    return
+}
+
+
+[PSCustomObject]@{
+  "currency" = $Request.currency
+  "balance" = $Request.balance
+  "pending" = $Request.unsold
+  "total" = $Request.total_unpaid
+}
