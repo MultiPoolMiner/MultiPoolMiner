6c80eaa3a81157be5454a6d716dfabbe177f40f3
Grant Emsley <gemsley@st-leonards.com>
2017-12-23 09:17:28 -0500

    More improvements to web monitoring

--- MultiPoolMiner.ps1
+++ MultiPoolMiner.ps1
@@ -474,19 +474,33 @@ while ($true) {
         }
     }
 	
-	if($MinerStatusURL) {
-		Write-Log "Pinging monitoring server..."
-		# Format the miner values for reporting.  Set relative path so the server doesn't store anything personal (like your system username, if running from somewhere in your profile)
-		$minerreport = ConvertTo-Json @($ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Select-Object Name, @{L='Path';E={Resolve-Path -Relative $_.Path}}, Arguments, Type, 
-			@{L='Active';E={"{0:dd} Days {0:hh} Hours {0:mm} Minutes" -f ((Get-Date) - $_.Process.StartTime)}},
-			@{L='CurrentSpeed';E={$_.Speed_Live | Foreach-Object {"$($_ | ConvertTo-Hash)/s"}}},
-			@{L='EstimatedSpeed';E={$_.Speed | Foreach-Object {"$($_ | ConvertTo-Hash)/s"}}},
-			@{L='PID';E={$_.Process.Id}},
-			@{L='Pool';E={[regex]::Match($_.Arguments, '://(.*):').Groups[1].value}},
-			@{L="BTC/day"; E={"{0:N8}" -f $_.Profit}})
-		$profit = "{0:N8}" -f ($ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Measure-Object Profit -Sum).Sum
-		Invoke-RestMethod -Uri $MinerStatusURL -Body @{address = $WalletBackup; workername = $WorkerNameBackup; miners = $minerreport; profit = $profit} | Out-Null
-	}
+    if($MinerStatusURL) {
+        Write-Log "Pinging monitoring server..."
+        $profit = "{0:N8}" -f ($ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Measure-Object Profit -Sum).Sum
+
+        # Format the miner values for reporting.  Set relative path so the server doesn't store anything personal (like your system username, if running from somewhere in your profile)
+
+        $minerreport = ConvertTo-Json @($ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Foreach-Object {
+            $ActiveMiner = $_
+            # Find the matching entry in $Miners, to get pool information. Perhaps there is a better way to do this?
+            $MatchingMiner = $Miners | Where-Object {$_.Name -eq $ActiveMiner.Name -and $_.Path -eq $ActiveMiner.Path -and $_.Arguments -eq $ActiveMiner.Arguments -and $_.Wrap -eq $ActiveMiner.Wrap -and $_.API -eq $ActiveMiner.API -and $_.Port -eq $ActiveMiner.Port}
+            # Create a custom object to convert to json. Type, Pool, CurrentSpeed and EstimatedSpeed are all forced to be arrays, since they sometimes have multiple values.
+            [pscustomobject]@{
+                Name = $_.Name
+                Path = Resolve-Path -Relative $_.Path
+                Type = @($_.Type)
+                Active = "{0:dd} Days {0:hh} Hours {0:mm} Minutes" -f ((Get-Date) - $_.Process.StartTime)
+                Algorithm = @($_.Algorithm)
+                Pool = @($MatchingMiner.Pools.PsObject.Properties.Value.Name)
+                CurrentSpeed = @($_.Speed_Live | Foreach-Object {"$($_ | ConvertTo-Hash)/s"})
+                EstimatedSpeed = @($_.Speed | Foreach-Object {"$($_ | ConvertTo-Hash)/s"})
+                PID = $_.Process.Id
+                'BTC/day' = "{0:N8}" -f $_.Profit
+            }
+        })
+        # Use backup wallet and worker so workers don't disappear when they are donating.
+        Invoke-RestMethod -Uri $MinerStatusURL -Body @{address = $WalletBackup; workername = $WorkerNameBackup; miners = $minerreport; profit = $profit} | Out-Null
+    }
 
     #Display mining information
     #Clear-Host
