0bddfe7677ec07e93e2f5fb6d2a7f61efe2732b0
aaronsace <aaronsace@hotmail.com>
2017-12-26 14:25:03 +0000

    Fix Wrapper

--- Wrapper.ps1
+++ Wrapper.ps1
@@ -20,8 +20,8 @@ Remove-Item ".\Wrapper\$Id.txt" -Force -ErrorAction Ignore
 $Job = Start-Job -ArgumentList $FilePath, $ArgumentList, $WorkingDirectory {
     param($FilePath, $ArgumentList, $WorkingDirectory)
     if ($WorkingDirectory) {Set-Location $WorkingDirectory}
-    if ($ArgumentList) {& $FilePath $ArgumentList 2>&1}
-    else {& $FilePath 2>&1}
+    if ($ArgumentList) {Invoke-Expression "& $FilePath $ArgumentList 2>&1"}
+    else {Invoke-Expression "& $FilePath 2>&1"}
 }
 
 Write-Host "MultiPoolMiner Wrapper Started" -BackgroundColor Yellow -ForegroundColor Black
@@ -32,33 +32,33 @@ do {
     $Job | Receive-Job | ForEach-Object {
         $Line = $_
 
-        if ($Line -like "*total*" -or $Line -like "*accepted*") {
+        if (($Line -like "*total*" -or $Line -like "*accepted*") -and $Line -like "*/s*") {
             $Words = $Line -split " "
 
             $matches = $null
 
-            if ($Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1))] -match "^((?:\d*\.)?\d+)(.*)$") {
+            if ($Words[$Words.IndexOf(($Words -like "*/s*" | Select-Object -Last 1))] -match "^((?:\d*\.)?\d+)(.*)$") {
                 $HashRate = [Decimal]$matches[1]
                 $HashRate_Unit = $matches[2]
             }
             else {
-                $HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1)) - 1]
-                $HashRate_Unit = $Words[$Words.IndexOf(($Words -like "*/s" | Select-Object -Last 1))]
+                $HashRate = [Decimal]$Words[$Words.IndexOf(($Words -like "*/s*" | Select-Object -Last 1)) - 1]
+                $HashRate_Unit = $Words[$Words.IndexOf(($Words -like "*/s*" | Select-Object -Last 1))]
             }
 
-            switch ($HashRate_Unit) {
-                "kh/s" {$HashRate *= [Math]::Pow(1000, 1)}
-                "mh/s" {$HashRate *= [Math]::Pow(1000, 2)}
-                "gh/s" {$HashRate *= [Math]::Pow(1000, 3)}
-                "th/s" {$HashRate *= [Math]::Pow(1000, 4)}
-                "ph/s" {$HashRate *= [Math]::Pow(1000, 5)}
+            switch -wildcard ($HashRate_Unit) {
+                "kh/s*" {$HashRate *= [Math]::Pow(1000, 1)}
+                "mh/s*" {$HashRate *= [Math]::Pow(1000, 2)}
+                "gh/s*" {$HashRate *= [Math]::Pow(1000, 3)}
+                "th/s*" {$HashRate *= [Math]::Pow(1000, 4)}
+                "ph/s*" {$HashRate *= [Math]::Pow(1000, 5)}
             }
 
             if (-not (Test-Path "Wrapper")) {New-Item "Wrapper" -ItemType "directory" -Force | Out-Null}
             $HashRate | ConvertTo-Json | Set-Content ".\Wrapper\$Id.txt" -Force -ErrorAction Ignore
         }
 
-        Write-Host ($Line -replace "`n|`r", "")
+        if (($Line -replace "\x1B\[[0-?]*[ -/]*[@-~]", "")) {Write-Host ($Line -replace "`n|`r", "")}
     }
 
     if (-not (Get-Process | Where-Object Id -EQ $ControllerProcessID)) {$Job | Stop-Job}
