a4541990edecdaad44a7f58f4e6263ea4dbc29ae
Grant Emsley <grant@emsley.ca>
2018-01-01 11:20:29 -0500

    Add additional miner intensities

---
+++ Miners/CcminerPalgin25.ps1
@@ -0,0 +1,54 @@
+﻿using module ..\Include.psm1
+
+$Path = ".\Bin\NVIDIA-Palgin\ccminer.exe"
+$Uri = "https://github.com/palginpav/ccminer/releases/download/1.1.1/palginmod_1.1_x64.zip"
+
+$Commands = [PSCustomObject]@{
+    #"bitcore" = "" #Bitcore
+    "blake2s" = "" #Blake2s
+    "blakecoin" = "" #Blakecoin
+    #"vanilla" = "" #BlakeVanilla
+    #"c11" = "" #C11
+    #"cryptonight" = "" #CryptoNight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = "" #Groestl
+    #"hmq1725" = "" #HMQ1725
+    #"jha" = "" #JHA
+    "keccak" = "" #Keccak
+    "lbry" = "" #Lbry
+    "lyra2v2" = "" #Lyra2RE2
+    #"lyra2z" = "" #Lyra2z
+    "myr-gr" = "" #MyriadGroestl
+    #"neoscrypt" = "" #NeoScrypt
+    "nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    #"phi" = "" #PHI
+    #"sia" = "" #Sia
+    "sib" = "" #Sib
+    "skein" = "" #Skein
+    #"skunk" = "" #Skunk
+    #"timetravel" = "" #Timetravel
+    #"tribus" = "" #Tribus
+    "veltor" = "" #Veltor
+    "x11evo" = "" #X11evo
+    #"x17" = "" #X17
+    #"yescrypt" = "" #Yescrypt
+    #"xevan" = "" #Xevan
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-i 25 -a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        Wrap = $false
+        URI = $Uri
+    }
+}
