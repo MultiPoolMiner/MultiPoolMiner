a0cdb841fbc5927e00973ce81a205bb743671efb
UselessGuru <useless.guru@gmail.com>
2018-01-01 15:44:15 +0100

    Auto-launch setup if no configuration

--- Start.bat
+++ Start.bat
@@ -1,4 +1,4 @@
-﻿cd /d %~dp0
+cd /d %~dp0
 
 setx GPU_FORCE_64BIT_PTR 1
 setx GPU_MAX_HEAP_SIZE 100
@@ -7,6 +7,7 @@ setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
 set "command=& .\multipoolminer.ps1"
+if not exist "Config.ps1" set "command=& .\Setup.ps1"
 
 #pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 powershell -version 5.1 -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
