8653b8fdb8b7f8132be2fe2a5a9cd6038c908fcf
UselessGuru <useless.guru@gmail.com>
2018-01-05 01:03:24 +0100

    Attempt to fix Get-NetTCPConnection errors

--- Include.psm1
+++ Include.psm1
@@ -1,3 +1,6 @@
+Import-Module "$env:Windir\System32\WindowsPowerShell\v1.0\Modules\NetSecurity\NetSecurity.psd1" -ErrorAction Ignore
+Import-Module "$env:Windir\System32\WindowsPowerShell\v1.0\Modules\Defender\Defender.psd1" -ErrorAction Ignore
+
 Set-Location (Split-Path $MyInvocation.MyCommand.Path)
 
 Add-Type -Path .\OpenCL\*.cs
