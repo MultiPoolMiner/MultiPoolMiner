00e4bcf34a57ae43ddac1384ca4c7f1742b13f49
UselessGuru <useless.guru@gmail.com>
2018-01-05 20:25:51 +0100

    Fix startup error

--- Start.bat
+++ Start.bat
@@ -9,9 +9,9 @@ setx GPU_SINGLE_ALLOC_PERCENT 100
 set "command=& .\multipoolminer.ps1"
 if not exist "Config.ps1" set "command=& .\Setup.ps1"
 
-#pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
+@rem pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 powershell -version 5.1 -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
-#msiexec -i https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc.2/PowerShell-6.0.0-rc.2-win-x64.msi -qb!
-#pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
+msiexec -i https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc.2/PowerShell-6.0.0-rc.2-win-x64.msi -qb!
+@rem pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 
 pause
