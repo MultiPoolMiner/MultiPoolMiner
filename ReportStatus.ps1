03643be0683d01d1d8762a2a10bbe78d70e432ac
James Dimitrov <james.dimitrov@egov.com>
2018-01-05 10:56:01 -0600

    code cleanup/formatting

--- ReportStatus.ps1
+++ ReportStatus.ps1
@@ -1,10 +1,10 @@
 ﻿using module .\Include.psm1
 
 param(
-[Parameter(Mandatory=$true)][String]$WorkerName,
-[Parameter(Mandatory=$true)]$ActiveMiners,
-[Parameter(Mandatory=$true)]$Miners,
-[Parameter(Mandatory=$true)]$MPHApiKey
+    [Parameter(Mandatory=$true)][String]$WorkerName,
+    [Parameter(Mandatory=$true)]$ActiveMiners,
+    [Parameter(Mandatory=$true)]$Miners,
+    [Parameter(Mandatory=$true)]$MPHApiKey
 )
 
 $profit = "{0:N8}" -f (($ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Measure-Object Profit -Sum).Sum)
@@ -14,23 +14,23 @@ $minerreport = ConvertTo-Json @($ActiveMiners | Where-Object {$_.Activated -GT 0
 $ActiveMiner = $_
 # Find the matching entry in $Miners, to get pool information. Perhaps there is a better way to do this?
 $MatchingMiner = $Miners | Where-Object {$_.Name -eq $ActiveMiner.Name -and $_.Path -eq $ActiveMiner.Path -and $_.Arguments -eq $ActiveMiner.Arguments -and $_.Wrap -eq $ActiveMiner.Wrap -and $_.API -eq $ActiveMiner.API -and $_.Port -eq $ActiveMiner.Port}
-# Create a custom object to convert to json. Type, Pool, CurrentSpeed and EstimatedSpeed are all forced to be arrays, since they sometimes have multiple values.
-[pscustomobject]@{
-Name = $_.Name
-Path = Resolve-Path -Relative $_.Path
-Type = @($_.Type)
-Active = "{0:dd} Days {0:hh} Hours {0:mm} Minutes" -f ((Get-Date) - $_.Process.StartTime)
-Algorithm = @($_.Algorithm)
-Pool = @($MatchingMiner.Pools.PsObject.Properties.Value.Name)
-CurrentSpeed = @($_.Speed_Live | Foreach-Object {"$($_ | ConvertTo-Hash)/s"})
-EstimatedSpeed = @($_.Speed | Foreach-Object {"$($_ | ConvertTo-Hash)/s"})
-PID = $_.Process.Id
-'BTC/day' = "{0:N8}" -f $_.Profit
-}
+    # Create a custom object to convert to json. Type, Pool, CurrentSpeed and EstimatedSpeed are all forced to be arrays, since they sometimes have multiple values.
+    [pscustomobject]@{
+        Name = $_.Name
+        Path = Resolve-Path -Relative $_.Path
+        Type = @($_.Type)
+        Active = "{0:dd} Days {0:hh} Hours {0:mm} Minutes" -f ((Get-Date) - $_.Process.StartTime)
+        Algorithm = @($_.Algorithm)
+        Pool = @($MatchingMiner.Pools.PsObject.Properties.Value.Name)
+        CurrentSpeed = @($_.Speed_Live | Foreach-Object {"$($_ | ConvertTo-Hash)/s"})
+        EstimatedSpeed = @($_.Speed | Foreach-Object {"$($_ | ConvertTo-Hash)/s"})
+        PID = $_.Process.Id
+        'BTC/day' = "{0:N8}" -f $_.Profit
+    }
 })
-try{
 
-Invoke-RestMethod -Uri "https://miningpoolhubstats.com/api/worker/$MPHApiKey" -TimeoutSec 10 -ErrorAction Stop -Method Post -Body @{workername = $WorkerName; miners = $minerreport; profit = $profit}
+try{
+    Invoke-RestMethod -Uri "https://miningpoolhubstats.com/api/worker/$MPHApiKey" -TimeoutSec 10 -ErrorAction Stop -Method Post -Body @{workername = $WorkerName; miners = $minerreport; profit = $profit}
 } catch {
-Write-Warning "Unable to post to monitoring URL..."
+    Write-Warning "Unable to post to monitoring URL..."
 }
