e0fbb873f928bac5c9ffa4fb2372d508221aa898
UselessGuru <useless.guru@gmail.com>
2018-01-08 15:29:13 +0100

    Fix MinigPoolHub (API does not report hash rate on algo)
    Thanks @grantemsley
    
    Don't know how that got in there :-(

--- Pools/MiningPoolHub.ps1
+++ Pools/MiningPoolHub.ps1
@@ -19,7 +19,7 @@ if (($MiningPoolHub_Request.return | Measure-Object).Count -le 1) {
 
 $MiningPoolHub_Regions = "europe", "us", "asia"
 
-$MiningPoolHub_Request.return | Where-Object {$MiningPoolHub_Request.$_.hashrate -gt 0} | ForEach-Object {
+$MiningPoolHub_Request.return | ForEach-Object {
     $MiningPoolHub_Hosts = $_.all_host_list.split(";")
     $MiningPoolHub_Port = $_.algo_switch_port
     $MiningPoolHub_Algorithm = $_.algo
