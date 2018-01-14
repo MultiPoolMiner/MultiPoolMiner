8c3dafd1caa8d72e180bfec11ee5dd3884b3abe0
platinum4 <platinumstephen@gmail.com>
2018-01-14 08:16:50 -0600

    Blockmunch Fee 0.25% All Algos

--- Pools/Blockmunch.ps1
+++ Pools/Blockmunch.ps1
@@ -12,7 +12,7 @@
 #$DisabledAlgorithms = @("neoscrypt")
 
 $ProfitFactor = 1 # 1 = 100%, use lower number to compensate for overoptimistic profit estimates sent by pool
-#$Fee = 0 # Default fee for all algos in %; if uncommented fee information from pool/algo is used
+$Fee = 0.25 # Default fee for all algos in %; if uncommented fee information from pool/algo is used
 
 $ShortPoolName = "BM" # Short pool name
 #End of user settable variables
