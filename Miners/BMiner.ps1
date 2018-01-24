890b3248f968afcbd3a2904352269da317f089d0
UselessGuru <useless.guru@gmail.com>
2018-01-24 09:55:41 +0100

    BMiner: Update to version 5.2

--- Miners/BMiner.ps1
+++ Miners/BMiner.ps1
@@ -1,7 +1,7 @@
 using module ..\Include.psm1
 
 $Path = ".\Bin\NVIDIA-BMiner\BMiner.exe"
-$Uri = "https://www.bminercontent.com/releases/bminer-v5.0.0-3a6fcd1-amd64.zip"
+$Uri = "https://www.bminercontent.com/releases/bminer-v5.2.0-4f20af3-amd64.zip"
 
 # Custom commands to be applied to all algorithms
 $CommonCommands = ""
@@ -35,9 +35,9 @@ $Devices | ForEach-Object {
 			Type         = $Device.Type
 			Device       = $Device.Device
 			Path         = $Path
-			Arguments    = ("-api 127.0.0.1:$Port -uri $(if ($Pools.Equihash.SSL) {'stratum+ssl'}else {'stratum'})://$($Pools.$Algorithm.User).$($Pools.$Algorithm.Pass)@$($Pools.$Algorithm.Host):$($Pools.$Algorithm.Port) -nofee $Command $CommonCommands").trim()
+			Arguments    = ("-api 127.0.0.1:$Port -uri $(if ($Pools.Equihash.SSL) {'stratum+ssl'}else {'stratum'})://$($Pools.$Algorithm.User):$($Pools.$Algorithm.Pass)@$($Pools.$Algorithm.Host):$($Pools.$Algorithm.Port) $Command $CommonCommands").trim()
 			HashRates    = [PSCustomObject]@{$Algorithm = ($Stats."$($Name)_$($Algorithm)_HashRate".Week)}
-			API          = "BMiner"
+			API          = "Bminer"
 			Port         = $Port
 			URI          = $Uri
 			PowerDraw    = $Stats."$($Name)_$($Algorithm)_PowerDraw".Week
