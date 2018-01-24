d08ddb8635a2408eeaaed7533249f74758b3f65a
adamje42 <adamje42@gmail.com>
2018-01-23 22:07:57 -0700

    Update JayDDee/cpuminer-opt to 3.8.0

--- Miners/CpuminerJayddeeAesavx.ps1
+++ Miners/CpuminerJayddeeAesavx.ps1
@@ -1,7 +1,7 @@
 ﻿using module ..\Include.psm1
 
 $Path = ".\Bin\CPU-JayDDee\cpuminer-aes-avx.exe"
-$Uri = "https://github.com/JayDDee/cpuminer-opt/files/1636873/cpuminer-opt-3.7.10-windows.zip"
+$Uri = "https://github.com/JayDDee/cpuminer-opt/files/1658246/cpuminer-opt-3.8.0-windows.zip"
 
 $Commands = [PSCustomObject]@{
     #"bitcore" = "" #Bitcore
