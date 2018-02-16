03c9a8edfe3548b4397849a6b0d07ee6ea12c58b
aaronsace <aaronsace@hotmail.com>
2018-02-16 19:09:25 +0000

    Fix Code Format

--- ReportStatus.ps1
+++ ReportStatus.ps1
@@ -12,7 +12,8 @@ Write-Log "Pinging monitoring server. "
 $profit = ($ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Measure-Object Profit -Sum).Sum | ConvertTo-Json
 
 # Format the miner values for reporting.  Set relative path so the server doesn't store anything personal (like your system username, if running from somewhere in your profile)
-$minerreport = ConvertTo-Json @($ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Foreach-Object {
+$minerreport = ConvertTo-Json @(
+    $ActiveMiners | Where-Object {$_.Activated -GT 0 -and $_.Status -eq "Running"} | Foreach-Object {
         $ActiveMiner = $_
         # Find the matching entry in $Miners, to get pool information. Perhaps there is a better way to do this?
         $MatchingMiner = $Miners | Where-Object {$_.Name -eq $ActiveMiner.Name -and $_.Path -eq $ActiveMiner.Path -and $_.Arguments -eq $ActiveMiner.Arguments -and $_.Wrap -eq $ActiveMiner.Wrap -and $_.API -eq $ActiveMiner.API -and $_.Port -eq $ActiveMiner.Port}
@@ -28,6 +29,8 @@ $minerreport = ConvertTo-Json @($ActiveMiners | Where-Object {$_.Activated -GT 0
             EstimatedSpeed = @($_.Speed)
             'BTC/day'      = $_.Profit
         }
-    })
+    }
+)
+
 Write-Log (Invoke-RestMethod -Uri $MinerStatusURL -Method Post -Body @{address = $Key; workername = $WorkerName; miners = $minerreport; profit = $profit})
 Write-Host "Your miner status key is: $Key"
