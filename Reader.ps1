e30849448f88556725eb5ae6d6c02b8cfa3d0d37
aaronsace <aaronsace@hotmail.com>
2018-02-22 01:35:28 +0000

    Add Log Reader

---
+++ Reader.ps1
@@ -0,0 +1,15 @@
+﻿param([String]$Log = ".\.txt")
+
+Set-Location (Split-Path $MyInvocation.MyCommand.Path)
+
+while ($true) {
+    Compare-Object @(Get-Job -ErrorAction Ignore | Select-Object -ExpandProperty Name) @(Get-ChildItem ".\Logs" -ErrorAction Ignore | Select-Object -ExpandProperty Name) | 
+        Sort-Object InputObject | 
+        Where-Object InputObject -match $Log | 
+        Where-Object SideIndicator -EQ "=>" | 
+        ForEach-Object {Start-Job ([ScriptBlock]::Create("Get-Content '$(Convert-Path ".\Logs\$($_.InputObject)")' -Wait")) -Name $_.InputObject | Out-Null}
+
+    Start-Sleep 1
+
+    Get-Job | Receive-Job | Where-Object {$_}
+}
--- Start-MiningPoolHub.bat
+++ Start-MiningPoolHub.bat
@@ -8,6 +8,8 @@ setx GPU_SINGLE_ALLOC_PERCENT 100
 
 set "command=& .\multipoolminer.ps1 -wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -username aaronsace -workername multipoolminer -region europe -currency btc,usd,eur -type amd,nvidia,cpu -poolname miningpoolhub,miningpoolhubcoins -algorithm cryptonight,ethash,ethash2gb,equihash,groestl,lyra2re2,lyra2z,neoscrypt -donate 24 -watchdog -minerstatusurl https://multipoolminer.io/monitor/miner.php -switchingprevention 2"
 
+start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log 'MultiPoolMiner_\d\d\d\d-\d\d-\d\d\.txt'"
+
 pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 msiexec -i https://github.com/PowerShell/PowerShell/releases/download/v6.0.1/PowerShell-6.0.1-win-x64.msi -qb!
