224c2676e27b27cd8201aad158450e501778b91f
aaronsace <aaronsace@hotmail.com>
2018-02-23 00:10:15 +0000

    Maintain Log Order

--- Reader.ps1
+++ Reader.ps1
@@ -1,15 +1,15 @@
-﻿param([String]$Log = ".\.txt")
+﻿param([String]$Log = ".\.txt", [String]$Sort = "")
 
 Set-Location (Split-Path $MyInvocation.MyCommand.Path)
 
 while ($true) {
     Compare-Object @(Get-Job -ErrorAction Ignore | Select-Object -ExpandProperty Name) @(Get-ChildItem ".\Logs" -ErrorAction Ignore | Select-Object -ExpandProperty Name) | 
-        Sort-Object InputObject | 
+        Sort-Object {$_.InputObject -replace $Sort} | 
         Where-Object InputObject -match $Log | 
         Where-Object SideIndicator -EQ "=>" | 
         ForEach-Object {Start-Job ([ScriptBlock]::Create("Get-Content '$(Convert-Path ".\Logs\$($_.InputObject)")' -Wait")) -Name $_.InputObject | Out-Null}
 
     Start-Sleep 1
 
-    Get-Job | Receive-Job | Where-Object {$_}
+    Get-Job | Where-Object {$_ | Receive-Job -Keep} | Select-Object -First 1 | Receive-Job | Where-Object {$_}
 }
