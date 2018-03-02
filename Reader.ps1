4d34ac1da003e6ca4220e00c67c2ae2acb2df254
CasualPasserby <36976756+CasualPasserby@users.noreply.github.com>
2018-03-02 15:23:50 +1300

    Display only last 100 lines in log
    This is useful for large log files, when the miner needs to be restarted.

--- Reader.ps1
+++ Reader.ps1
@@ -7,7 +7,7 @@ while ($true) {
         Sort-Object {$_.InputObject -replace $Sort} | 
         Where-Object InputObject -match $Log | 
         Where-Object SideIndicator -EQ "=>" | 
-        ForEach-Object {Start-Job ([ScriptBlock]::Create("Get-Content '$(Convert-Path ".\Logs\$($_.InputObject)")' -Wait")) -Name $_.InputObject | Out-Null}
+        ForEach-Object {Start-Job ([ScriptBlock]::Create("Get-Content '$(Convert-Path ".\Logs\$($_.InputObject)")' -Wait -Tail 100")) -Name $_.InputObject | Out-Null}
 
     Start-Sleep 1
 
