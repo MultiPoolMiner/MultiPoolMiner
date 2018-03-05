9394ec6f220d509892f8851c2bcfe7d77319c783
angelics <angelics@users.noreply.github.com>
2018-03-05 04:22:40 +0000

    ccminer enemy
    better x17, x116r support

---
+++ Miners/CcminerEnemy.ps1
@@ -0,0 +1,23 @@
+using module ..\Include.psm1
+
+$Path = ".\Bin\Enemy-NVIDIA\ccminer.exe"
+$Uri = "https://github.com/Enemy-Mine/ccminer/releases/download/v1.03-RVN/ccminer.zip"
+
+$Commands = [PSCustomObject]@{
+    "x16r" = "" #Raven
+    "x17" = "" #x17
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        URI = $Uri
+    }
+}
