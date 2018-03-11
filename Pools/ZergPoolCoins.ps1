bf07318a44413174c6c017f535153a802e025cca
UselessGuru <useless.guru@gmail.com>
2018-03-11 19:14:58 +0100

    Update ZergPoolCoins.ps1

--- Pools/ZergPoolCoins.ps1
+++ Pools/ZergPoolCoins.ps1
@@ -74,7 +74,7 @@ $ZergPoolCoins_MiningCurrencies | Where-Object {($Coins.count -eq 0 -or $Coins -
                 Protocol      = "stratum+tcp"
                 Host          = if ($ZergPoolCoins_Region -eq "us") {$ZergPoolCoins_Host}else {"$ZergPoolCoins_Region.$ZergPoolCoins_Host"}
                 Port          = $ZergPoolCoins_Port
-                User          = Get-Variable $_ -ValueOnly
+                User          = Get-Variable $ZergPoolCoins_Currency -ValueOnly
                 Pass          = "$Worker, c=$ZergPoolCoins_Currency, mc=$ZergPoolCoins_Currency"
                 Region        = $ZergPoolCoins_Region_Norm
                 SSL           = $false
