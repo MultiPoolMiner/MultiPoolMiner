6247df7d9a5fa2879b8451c413d42bb4d01e7906
UselessGuru <useless.guru@gmail.com>
2018-03-23 13:58:29 +0100

    Update MultiPoolMiner.ps1

--- MultiPoolMiner.ps1
+++ MultiPoolMiner.ps1
@@ -166,6 +166,7 @@ while ($true) {
         Write-Log -Level Error "*********************************************************** "
         Write-Log -Level Error "Critical error: Config.txt is invalid. MPM cannot continue. "
         Write-Log -Level Error "*********************************************************** "
+        Start-Sleep 10
         Exit
     }
 
