f4871592244f558ef3ad6fd34d367e1fcf1adab2
UselessGuru <useless.guru@gmail.com>
2018-03-24 16:21:30 +0100

    Pools: Do no scan subdirectories

--- MultiPoolMiner.ps1
+++ MultiPoolMiner.ps1
@@ -173,7 +173,7 @@ while ($true) {
     #For backwards compatibility, set the MinerStatusKey to $Wallet if it's not specified
     if ($Wallet -and -not $Config.MinerStatusKey) {$Config.MinerStatusKey = $Wallet}
 
-    Get-ChildItem "Pools" | Where-Object {-not $Config.Pools.($_.BaseName)} | ForEach-Object {
+    Get-ChildItem "Pools" -File | Where-Object {-not $Config.Pools.($_.BaseName)} | ForEach-Object {
         $Config.Pools | Add-Member $_.BaseName (
             [PSCustomObject]@{
                 BTC     = $Wallet
