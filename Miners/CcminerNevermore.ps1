6e8b028c7446a259b23215428c5e37dc8560e24c
UselessGuru <uselessguru@gmail.com>
2018-03-29 17:55:44 +0200

    CcminerNevermore
    https://github.com/nemosminer/ccminernevermore/releases/tag/0.2-nevermore

---
+++ Miners/CcminerNevermore.ps1
@@ -0,0 +1,22 @@
+﻿using module ..\Include.psm1
+
+$Path = ".\Bin\NVIDIA-Nevermore\ccminer.exe"
+$Uri = "https://github.com/nemosminer/ccminernevermore/releases/download/0.2-nevermore/ccminernevermore0.2x64.zip"
+
+$Commands = [PSCustomObject]@{
+    "x16r"  = "" #X16R RavenCoin
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        URI = $Uri
+    }
+}
