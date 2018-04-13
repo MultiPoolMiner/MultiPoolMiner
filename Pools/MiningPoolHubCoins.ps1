603e22bdf1981ce233ff2adce0613c9592735462
Grant Emsley <grant@emsley.ca>
2018-04-12 20:46:33 -0400

    Fix MiningPoolHubCoins.ps1

--- Pools/MiningPoolHubCoins.ps1
+++ Pools/MiningPoolHubCoins.ps1
@@ -34,7 +34,7 @@ $MiningPoolHubCoins_Request.return | Where-Object {$_.pool_hash -gt 0} |ForEach-
     $MiningPoolHubCoins_Algorithm_Norm = Get-Algorithm $MiningPoolHubCoins_Algorithm
     $MiningPoolHubCoins_Coin = (Get-Culture).TextInfo.ToTitleCase(($_.coin_name -replace "-", " " -replace "_", " ")) -replace " "
 
-    if ($MiningPoolHubCoins_Algorithm_Norm -eq "Cryptonight*") { # temp fix
+    if ($MiningPoolHubCoins_Algorithm_Norm -like "Cryptonight*") { # temp fix
         $MiningPoolHubCoins_Algorithm = Get-CryptonightAlgorithm $MiningPoolHubCoins_Coin
         $MiningPoolHubCoins_Algorithm_Norm = Get-Algorithm $MiningPoolHubCoins_Algorithm
     }
@@ -66,7 +66,7 @@ $MiningPoolHubCoins_Request.return | Where-Object {$_.pool_hash -gt 0} |ForEach-
                 Updated       = $Stat.Updated
             }
 
-            if ($MiningPoolHubCoins_Algorithm_Norm -eq "Cryptonight*" -or $MiningPoolHubCoins_Algorithm_Norm -eq "Equihash") {
+            if ($MiningPoolHubCoins_Algorithm_Norm -like "Cryptonight*" -or $MiningPoolHubCoins_Algorithm_Norm -eq "Equihash") {
                 [PSCustomObject]@{
                     Algorithm     = $MiningPoolHubCoins_Algorithm_Norm
                     Info          = $MiningPoolHubCoins_Coin
