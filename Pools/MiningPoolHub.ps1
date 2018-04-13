5441a17b3685ec1df0aaaaca2190c17e99f538ec
Grant Emsley <grant@emsley.ca>
2018-04-12 20:45:25 -0400

    Fix MiningPoolHub pool

--- Pools/MiningPoolHub.ps1
+++ Pools/MiningPoolHub.ps1
@@ -34,7 +34,7 @@ $MiningPoolHub_Request.return | ForEach-Object {
     $MiningPoolHub_Algorithm_Norm = Get-Algorithm $MiningPoolHub_Algorithm
     $MiningPoolHub_Coin = (Get-Culture).TextInfo.ToTitleCase(($_.coin_name -replace "-", " " -replace "_", " ")) -replace " "
 
-    if ($MiningPoolHub_Algorithm_Norm -eq "Cryptonight*") { # temp fix
+    if ($MiningPoolHub_Algorithm_Norm -like "Cryptonight*") { # temp fix
         $MiningPoolHub_Algorithm = Get-CryptonightAlgorithm $MiningPoolHub_Coin
         $MiningPoolHub_Algorithm_Norm = Get-Algorithm $MiningPoolHub_Algorithm
     }
@@ -66,7 +66,7 @@ $MiningPoolHub_Request.return | ForEach-Object {
                 Updated       = $Stat.Updated
             }
 
-            if ($MiningPoolHub_Algorithm_Norm -eq "Cryptonight*" -or $MiningPoolHub_Algorithm_Norm -eq "Equihash") {
+            if ($MiningPoolHub_Algorithm_Norm -like "Cryptonight*" -or $MiningPoolHub_Algorithm_Norm -eq "Equihash") {
                 [PSCustomObject]@{
                     Algorithm     = $MiningPoolHub_Algorithm_Norm
                     Info          = $MiningPoolHub_Coin
