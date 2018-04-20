01698acf61c61d76d3b64d8b68607d33d2b3f55b
iwtym <32946288+iwtym@users.noreply.github.com>
2018-04-19 20:31:14 -0400

    Added ccminerZEnemy 1.08

---
+++ Miners/CcminerZEnemy.ps1
@@ -0,0 +1,27 @@
+﻿using module ..\Include.psm1
+
+$Path = ".\Bin\NVIDIA-ZEnemy\z-enemy.exe"
+$Uri = "https://github.com/iwtym/iwtym-zenemy/archive/master.zip"
+
+$Commands = [PSCustomObject]@{
+    "bitcore" = "" #Bitcore BTX
+    "phi" = "" #Phi Phi1612 LuxCoin
+    "x16r" = "" #X16r RavenCoin
+    "X16s" = "" #X16s PigeonCoin
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        URI = $Uri
+        PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
+        PrerequisiteURI = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+    }
+}
