a94c6f8700bafa82cf5acf669a8d0f0ee6c6b6f9
iwtym <32946288+iwtym@users.noreply.github.com>
2018-04-19 20:28:49 -0400

    ASIC Exclusions: Sib, Nist5, MyriadGroestl,Blakecoin

--- #Start-NVIDIA.bat
+++ #Start-NVIDIA.bat
@@ -6,7 +6,7 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 
-set "command=& .\multipoolminer.ps1 -wallet 1NVxfNNmrdQHzW1SuP2xyF2Fy39KWWMN7b -username nujan -workername %computername% -region us -currency btc,usd -type nvidia -donate 0 -excludealgorithm sha256,blake256,blakevanilla,decred,decrednicehash,sia,pascal,lbry,scrypt,x11,x13,x14,x15,quark,qubit -minerstatusurl https://multipoolminer.io/monitor/miner.php -MinerStatusKey 30b122dd-1579-446f-a542-dbdc24d8066a -switchingprevention 2"
+set "command=& .\multipoolminer.ps1 -wallet 1NVxfNNmrdQHzW1SuP2xyF2Fy39KWWMN7b -username nujan -workername %computername% -region us -currency btc,usd -type nvidia -donate 0 -excludealgorithm sha256,blake256,blakevanilla,decred,decrednicehash,sia,pascal,lbry,scrypt,x11,x13,x14,x15,quark,qubit,sib,nist5,myriadgroestl,blakecoin -minerstatusurl https://multipoolminer.io/monitor/miner.php -MinerStatusKey 30b122dd-1579-446f-a542-dbdc24d8066a -switchingprevention 2"
 
 start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log 'MultiPoolMiner_\d\d\d\d-\d\d-\d\d\.txt' -sort '^[^_]*_' -quickstart"
 
