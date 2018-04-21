564ea1b902246a263f9fe0c0738ed100d8482e91
iwtym <32946288+iwtym@users.noreply.github.com>
2018-04-20 20:56:39 -0400

    Add zFastMiner v2.2.9

---
+++ Miners/CcminerZFastLyra2Z.ps1
@@ -0,0 +1,22 @@
+﻿using module ..\Include.psm1
+
+$Path = ".\Bin\NVIDIA-Lyra2Z-ZFast\ccminer.exe"
+$Uri = "https://github.com/iwtym/iwtym-zfastminer/raw/master/zFastminer-v229.exe"
+
+$Commands = [PSCustomObject]@{
+    "lyra2z" = "" #Lyra2z
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = "-a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        URI = $Uri
+    }
+}
