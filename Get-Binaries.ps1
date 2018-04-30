7899636300e786af5c22b58185b8d631190b38d8
Grant Emsley <grant@emsley.ca>
2018-04-29 21:34:41 -0400

    Add Get-Binaries.ps1
    This script will download all the miner binaries it can, and show the
    user where to get the ones it can't.
    
    It also validates that the correct version is installed by comparing the
    SHA265 hashes of the exe file to the ones stored in a text file.

---
+++ Get-Binaries.ps1
@@ -0,0 +1,110 @@
+using module .\Include.psm1
+
+# Generate configuration and pool info that will ensure all miners get included
+$Timer = (Get-Date).ToUniversalTime()
+$StatStart = $Timer
+$StatEnd = $Timer.AddSeconds(60)
+$StatSpan = New-TimeSpan $StatStart $StatEnd
+
+$Stats = [PSCustomObject]@{}
+if (Test-Path "Stats") {Get-ChildItemContent "Stats" | ForEach-Object {$Stats | Add-Member $_.Name $_.Content}}
+
+$Config = [PSCustomObject]@{
+    Pools = [PSCustomObject]@{}
+    Miners = [PSCustomObject]@{}
+    Interval = 60
+    Region = 'US'
+    SSL = $True
+    Type = @('CPU','NVIDIA','AMD')
+    Algorithm = @()
+    Minername = @()
+    Poolname = @()
+    ExcludeAlgorithm = @()
+    ExcludeMinerName = @()
+    ExcludePoolName = @()
+    Currency = 'USD'
+    Donate = 10
+    Proxy = ''
+    Delay = 0
+    Watchdog = $True
+    SwitchingPrevention = 1
+}
+
+# Generate fake pools for each algorithm
+$algorithms = (Get-Content "Algorithms.txt" | ConvertFrom-Json).PSObject.Properties.Value | Foreach-Object {$_.ToLower() } | Select-Object -Unique
+$Pools = [PSCustomObject]@{}
+$algorithms | Foreach-Object {
+    $FakePool = [PSCustomObject]@{
+        Algorithm = $_
+        Info = ""
+        Price = 1
+        StablePrice = 1
+        MarginOfError = 0
+        Protocol = "stratum+tcp"
+        Host = "fake.host.name"
+        Port = 1234
+        User = "fake"
+        Pass = "fake,c=BTC"
+        Region = "US"
+        SSL = $false
+        Updated = [DateTime](Get-Date).ToUniversalTime()
+        Name = "Fake"
+        Price_Bias = 0
+        Price_Unbias = 0
+    }
+    $Pools | Add-Member $_ $FakePool
+}
+
+# Get all the miners using the fake configuration, then filter to just the ones with unique paths
+$Miners = Get-ChildItemContent "Miners" -Parameters @{Pools = $Pools; Stats = $Stats; Config = $Config} | ForEach-Object {$_.Content | Add-Member Name $_.Name -PassThru -Force} | Group-Object -Property Path | Foreach-Object {$_.Group | Select-Object -First 1}
+
+if(!(Test-Path ".\Bin")) {
+    New-Item ".\Bin" -ItemType "Directory" | Out-Null
+}
+
+# Load the hashes for binary files
+if(Test-Path "binaryhashes.txt" -PathType Leaf) {
+    $hashes = Get-Content "binaryhashes.txt" | ConvertFrom-Json
+} else {
+    Write-Warning "binaryhashes.txt does not exist. Will not be able to verify the correct miner versions are installed."
+    $hashes = $null
+}
+
+$Miners | Foreach-Object {
+    $Miner = $_
+    if(Test-Path $Miner.Path) {
+        Write-Host -NoNewline -ForegroundColor Green "Miner $($Miner.Name) already installed"
+        if($hashes) {
+            if( (Get-FileHash $Miner.Path).Hash -eq $hashes.$($Miner.Path) ) {
+                Write-Host -ForegroundColor Green " - correct version installed"
+            } else {
+                Write-Host -ForegroundColor Red " - incorrect version installed, delete $(Split-Path $Miner.Path) directory and redownload"
+            }
+        } else {
+            Write-Host -ForegroundColor Red " - unable to verify version"
+        }
+    } else {
+        if(!$Miner.URI) {
+            if($Miner.ManualURI) {
+                Write-Warning "Miner $($Miner.Name) must be downloaded manually from $($Miner.ManualURI) and extracted to $($Miner.Path)"
+            } else {
+                Write-Warning "Miner $($Miner.Name) must be downloaded manually and extracted to $($Miner.Path)"
+            }
+        } else {
+            Write-Host "Downloading $($Miner.Name) from $($Miner.URI)..."
+            try {
+                if ( (Split-Path $Miner.URI -Leaf) -eq (Split-Path $Miner.Path -Leaf)) {
+                    # Miner isn't a zip file, download the exe directly
+                    New-Item (Split-Path $Miner.Path) -ItemType "Directory" | Out-Null
+                    Invoke-WebRequest $Miner.URI -OutFile $Miner.Path -UseBasicParsing -Erroraction Stop
+                } else {
+                    Expand-WebRequest $Miner.URI (Split-Path $Miner.Path) -ErrorAction Stop
+                }
+            } catch {
+                Write-Warning "Failed to download $($Miner.Name) - download manually from $($Miner.URI) and extract to $($Miner.Path)"
+            }
+        }
+    }
+}
+
+
