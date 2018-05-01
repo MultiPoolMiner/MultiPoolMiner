d08d70059cb98f54e39ffa26fda17a24ce040242
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-05-01 15:02:37 +0200

    Update code formatting to match core

--- Create-BinaryHashes.ps1
+++ Create-BinaryHashes.ps1
@@ -4,18 +4,18 @@
 # which is used to check that the correct version of the miners is installed.
 # This script will regenerate the binaryhashes.txt file to match what is currently installed on your system
 
-If(!(Test-Path ".\Bin")) {
+If (-not (Test-Path ".\Bin")) {
     Write-Error "Bin directory does not exist!"
 }
 
 # Get list of all the exe files with relative paths, sort to minimize differences when updating git
-$binaries = Get-ChildItem -Path Bin -Recurse -Filter "*.exe" | Resolve-Path -Relative | Sort-Object
+$Binaries = Get-ChildItem -Path Bin -Recurse -Filter "*.exe" | Resolve-Path -Relative | Sort-Object
 
-$hashes = [PSCustomObject]@{}
+$Hashes = [PSCustomObject]@{}
 
-$binaries | Foreach-Object {
-    $hashes | Add-Member $_ (Get-FileHash $_).Hash
+$Binaries | Foreach-Object {
+    $Hashes | Add-Member $_ (Get-FileHash $_).Hash
 }
 
-$hashes | ConvertTo-Json | Out-File -FilePath "binaryhashes.txt" -Encoding ascii
+$Hashes | ConvertTo-Json | Out-File -FilePath "binaryhashes.txt" -Encoding ASCII
 Write-Host "Hashes for binary files updated!"
