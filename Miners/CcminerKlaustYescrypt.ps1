d0f3666eee160fb52cff751cd7b33fea201ac726
Rama-Nujan <32946288+Rama-Nujan@users.noreply.github.com>
2018-05-09 09:09:28 -0400

    Update CcminerKlaustYescrypt.ps1

--- Miners/CcminerKlaustYescrypt.ps1
+++ Miners/CcminerKlaustYescrypt.ps1
@@ -56,6 +56,7 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
     [PSCustomObject]@{
         Type = "NVIDIA"
+        HashSHA256 = $HashSHA256
         Path = $Path
         Arguments = "-a $_ -b 4068 -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
