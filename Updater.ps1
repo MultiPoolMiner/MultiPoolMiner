7b53135c8cedf6ca876b2fb7bd03e7bbb76090f4
Grant Emsley <grant@emsley.ca>
2018-05-09 18:23:15 -0400

    Compare versions instead of just checking if equal

--- Updater.ps1
+++ Updater.ps1
@@ -6,6 +6,14 @@ if ($script:MyInvocation.MyCommand.Path) {Set-Location (Split-Path $script:MyInv
 
 $ProgressPreferenceBackup = $ProgressPreference
 
+
+Function Get-Version ($Version) {
+    # System.Version objects can be compared with -gt and -lt properly
+    # This strips out anything that doens't belong in a version, eg. v at the beginning, or -preview1 at the end, and returns a version object
+    Return [System.Version]($Version -Split "-" -Replace "[^0-9.]")[0]
+}
+
+
 $Name = "MultiPoolMiner"
 try {
     $ProgressPreference = "SilentlyContinue"
@@ -13,12 +21,14 @@ try {
     $Version = ($Request.tag_name -replace '^v')
     $Uri = $Request.assets | Where-Object Name -EQ "$($Name)V$($Version).zip" | Select-Object -ExpandProperty browser_download_url
 
-    if ($Version -ne $MPMVersion) {
-        $ProgressPreference = $ProgressPreferenceBackup
-        Write-Progress -Activity "Updater" -Status $Name -CurrentOperation "Acquiring Online ($URI)"
-        $ProgressPreference = "SilentlyContinue"
-        Write-Log -Level Warn "The software ($Name) is out of date; there is an updated version available at $URI. "
+    if ( (Get-Version($Version)) -gt (Get-Version($MPMVersion)) ) {
+        Write-Log -Level Warn "$Name is out of date; current version $(Get-Version($Version)), lastest release $(Get-Version($Version)) - there is an updated version available at $URI. "
     }
+
+    if ( (Get-Version($Version)) -lt (Get-Version($MPMVersion)) ) {
+        Write-Log -Level Warn "You are running prerelease version $(Get-Version($Version)) of $Name. Use at your own risk."
+    }
+
 }
 catch {
     Write-Log -Level Warn "The software ($Name) failed to update. "
@@ -36,7 +46,7 @@ try {
     $Version = ($Request.tag_name -replace '^v')
     $URI = $Request.assets | Where-Object Name -EQ "$($Name)-$($Version)-win-x64.msi" | Select-Object -ExpandProperty browser_download_url
 
-    if ($Version -ne $PSVersion) {
+    if ( (Get-Version($Version)) -gt (Get-Version($PSVersion)) ) {
         $ProgressPreference = $ProgressPreferenceBackup
         Write-Progress -Activity "Updater" -Status $Name -CurrentOperation "Acquiring Online ($URI)"
         $ProgressPreference = "SilentlyContinue"
