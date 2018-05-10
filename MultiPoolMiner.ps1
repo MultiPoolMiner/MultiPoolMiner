37100eebb078617cc7d2290bc7837501b0301239
Grant Emsley <grant@emsley.ca>
2018-05-10 08:46:14 -0400

    Remove asterisks

--- MultiPoolMiner.ps1
+++ MultiPoolMiner.ps1
@@ -171,9 +171,7 @@ while ($true) {
 
     #Error in Config.txt
     if ($Config -isnot [PSCustomObject]) {
-        Write-Log -Level Error "*********************************************************** "
         Write-Log -Level Error "Config.txt is invalid. Cannot continue. "
-        Write-Log -Level Error "*********************************************************** "
         Start-Sleep 10
         Exit
     }
