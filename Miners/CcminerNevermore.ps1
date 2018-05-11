316c16a07a2b6620267acb4a4a36fce3c0950611
Mar2 <martin.kirov@yahoo.com>
2018-05-11 15:56:10 +0300

    Update CcminerNevermore to 0.4 by Nemos
    The new version works great and its top performing miner for a few algos. Its possible I messed up the hash... Tried a tool online...

--- Miners/CcminerNevermore.ps1
+++ Miners/CcminerNevermore.ps1
@@ -1,12 +1,60 @@
 using module ..\Include.psm1
 
 $Path = ".\Bin\NVIDIA-Nevermore\ccminer.exe"
-$HashSHA256 = "62489722C44CDAD3DF4C22E46A2A16A3E8A81E273D562C7C25398F77684A1C11"
-$Uri = "https://github.com/nemosminer/ccminernevermorex16r/releases/download/nevermorex16rv0.3/ccminernevermorex16rx64v0.3.zip"
+$HashSHA256 = "6148f640e011395df00a59bb0d01af194cee32c729f493c491163de8d695f170"
+$Uri = "https://github.com/nemosminer/ccminerx16r-x16s/releases/download/x16rx16sv0.4/ccminerx16rx16sv0.4.zip"
 
 $Commands = [PSCustomObject]@{
-    "X16r" = "" #X16r RavenCoin
-    "X16s" = "" #X16s PigeonCoin
+    #"bitcore" = "" #Bitcore - Beaten by ccminerZealot
+    "c11" = "" #C11
+    #"cryptonight" = "" #CryptoNight
+    #"equihash" = "" #Equihash - Beaten by Bminer
+    #"ethash" = "" #Ethash - Not Supported
+    #"groestl" = "" #Groestl - Beaten by ccminerKlaust
+    "hmq1725" = "" #HMQ1725
+    #"hsr" = "" #HSR - Beaten by PalginNvidia
+    "jha" = "" #JHA - NOT TESTED
+    #"keccak" = "" #Keccak - Beaten by ccminerPolytimos
+    "keccakc" = "" #Keccakc
+    #"lyra2v2" = "" #Lyra2RE2 - Beaten by ccminerXevan
+    #"lyra2z" = "" #Lyra2z - Beaten by ccminerTpruvot
+    #"neoscrypt" = "" #NeoScrypt - Beaten by PalginNvidia
+    #"pascal" = "" #Pascal - Not Supported
+    "phi" = "" #PHI
+    "poly" = "" #Polytmos
+    #"skein" = "" #Skein - Beaten by ccminerHsr
+    "skunk" = "" #Skunk
+    #"timetravel" = "" #Timetravel - Beaten by ccminerRaven
+    "tribus" = "" #Tribus
+    "veltor" = "" #Veltor - NOT TESTED
+    #"x11evo" = "" #X11evo - Beaten by ccminerSib
+    "x12" = "" #X12 - NOT TESTED
+    "x16r" = "" #Rave
+    "x16s" = "" #Pigeon
+    #"x17" = "" #X17 - Beaten by ccminerEnemy
+    #"yescrypt" = "" #Yescrypt - Not Supported
+    #"xevan" = "" #Xevan - Not Supported
+   
+    # ASIC - never profitable 11/05/2018
+    #"blake" = "" #blake
+    #"blakecoin" = "" #Blakecoin
+    #"blake2s" = "" #Blake2s
+    #"decred" = "" #Decred
+    #"decrednicehash" = "" #Decrednicehash 
+    #"lbry" = "" #Lbry
+    #"myr-gr" = "" #MyriadGroestl
+    #"nist5" = "" #Nist5
+    #"quark" = "" #Quark
+    #"qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sha256d" = "" #sha256d
+    #"sia" = "" #SiaCoin
+    #"sib" = "" #Sib
+    #"vanilla" = "" #BlakeVanilla
+    #"x11" = "" #X11
+    #"x13" = "" #x13
+    #"x14" = "" #x14
+    #"x15" = "" #x15
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
