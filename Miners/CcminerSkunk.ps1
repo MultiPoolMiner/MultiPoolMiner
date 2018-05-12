726467e8c7385ac54855985c4c26b7a5dc6cd07c
Mar2 <martin.kirov@yahoo.com>
2018-05-12 22:06:30 +0300

    Add algos to CcminerSkunk

--- Miners/CcminerSkunk.ps1
+++ Miners/CcminerSkunk.ps1
@@ -6,6 +6,33 @@ $URI = "https://github.com/scaras/ccminer-2.2-mod-r1/releases/download/2.2-r1/2.
 
 $Commands = [PSCustomObject]@{
     "skunk" = "" #Skunk
+    
+    "blake2s"   = "" #Blake2s
+    "blakecoin" = "" #Blakecoin
+    "c11"       = "" #C11
+    "groestl"   = "" #Groestl
+    "hmq1725"   = "" #HMQ1725
+    "lyra2v2"   = "" #Lyra2RE2
+    "lyra2z"    = "" #Lyra2z
+    "neoscrypt" = "" #NeoScrypt
+    "skein"     = "" #Skein
+    "timetravel"= "" #Timetravel
+    "tribus"    = "" #Tribus
+    "x11evo"    = "" #X11evo
+    "x17"       = "" #X17
+    
+    # ASIC - never profitable 12/05/2018
+    #"decred"   = "" #Decred
+    #"lbry"     = "" #Lbry
+    #"myr-gr"   = "" #MyriadGroestl
+    #"nist5"    = "" #Nist5
+    #"qubit"    = "" #qubit
+    #"quark"    = "" #Quark
+    #"sib"      = "" #Sib
+    #"x11"      = "" #X11
+    #"x12"      = "" #X12
+    #"x13"      = "" #X13
+    #"x14"      = "" #X14
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
