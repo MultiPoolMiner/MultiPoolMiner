9897f9c32daf72bb83e37aee8197885cb6429922
symalla <symalla@gmail.com>
2018-05-14 08:39:20 -0500

    Update SgminerKL-101.ps1

--- Miners/SgminerKL-101.ps1
+++ Miners/SgminerKL-101.ps1
@@ -1,6 +1,7 @@
 using module ..\Include.psm1
 
 $Path = ".\Bin\AMD-KL-101\sgminer.exe"
+$HashSHA256 = "9029ba519b2805c24e2168050a9878f16bf5988e5fb90c80200bd451983c9fc9"
 $Uri = "https://github.com/KL0nLutiy/sgminer-kl/releases/download/kl-1.0.1/sgminer-kl-1.0.1-windows.zip"
 
 $Commands = [PSCustomObject]@{
@@ -16,6 +17,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type = "AMD"
         Path = $Path
+		HashSHA256 = $HashSHA256
         Arguments = "--api-listen -k $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_) --text-only --gpu-platform $([array]::IndexOf(([OpenCl.Platform]::GetPlatformIDs() | Select-Object -ExpandProperty Vendor), 'Advanced Micro Devices, Inc.'))"
         HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week * 0.99}
         API = "Xgminer"
