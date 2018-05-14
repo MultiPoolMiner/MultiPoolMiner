794e19eaa9a5ad918fc2704b2e29d78cab5670d0
symalla <symalla@gmail.com>
2018-05-14 08:47:34 -0500

    Update SgminerPhi-5613.ps1

--- Miners/SgminerPhi-5613.ps1
+++ Miners/SgminerPhi-5613.ps1
@@ -1,6 +1,6 @@
 using module ..\Include.psm1
 
-$Path = ".\Bin\AMD-Phi-5613\sgminer.exe"
+$Path = ".\Bin\AMD-Phi\sgminer.exe"
 $HashSHA256 = "3a84c2970b8a7f7adfc28b9ed849675302ae50f92fe9de18a48911f7dfe7d328"
 $Uri = "https://github.com/Quake4/MindMinerPrerequisites/raw/master/AMD/sgminer-phi/sgminer-phi-5.6.1-bitbandi-3.zip"
 
@@ -14,7 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type = "AMD"
         Path = $Path
-		HashSHA256 = $HashSHA256
+	HashSHA256 = $HashSHA256
         Arguments = "--api-listen -k $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_) --text-only --gpu-platform $([array]::IndexOf(([OpenCl.Platform]::GetPlatformIDs() | Select-Object -ExpandProperty Vendor), 'Advanced Micro Devices, Inc.'))"
         HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
         API = "Xgminer"
