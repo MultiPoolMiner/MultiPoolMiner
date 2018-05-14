9d0a67630025f29434183a7e4bc05159e5b19b8b
symalla <symalla@gmail.com>
2018-05-14 08:40:59 -0500

    Update SgminerBitcore-10.ps1

--- Miners/SgminerBitcore-10.ps1
+++ Miners/SgminerBitcore-10.ps1
@@ -1,6 +1,7 @@
 using module ..\Include.psm1
 
 $Path = ".\Bin\AMD-Bitcore-10\sgminer-x64.exe"
+$HashSHA256 = "c00de0d33bd20be3b5014fc019ed8a81d3aa5273c8af96b17d6b51c5d8fa3933"
 $Uri = "https://github.com/Quake4/MindMinerPrerequisites/raw/master/AMD/sgminer-bitcore/sgminer-bitcore-5.6.1.9.zip"
 
 $Commands = [PSCustomObject]@{
@@ -13,6 +14,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type = "AMD"
         Path = $Path
+		HashSHA256 = $HashSHA256
         Arguments = "--api-listen -k $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_) --text-only --gpu-platform $([array]::IndexOf(([OpenCl.Platform]::GetPlatformIDs() | Select-Object -ExpandProperty Vendor), 'Advanced Micro Devices, Inc.'))"
         HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
         API = "Xgminer"
