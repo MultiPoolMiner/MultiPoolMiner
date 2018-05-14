474c10634c6ca57710e3f0f917703bdd368941c9
symalla <symalla@gmail.com>
2018-05-14 09:09:13 -0500

    Update avermore14.ps1

--- Miners/avermore14.ps1
+++ Miners/avermore14.ps1
@@ -6,26 +6,18 @@ $Uri = "https://github.com/brian112358/avermore-miner/releases/download/v1.4/ave
 
 $Commands = [PSCustomObject]@{
     #"bitcore" = "" #Bitcore
-    #"blake2s" = "" #Blake2s
-    "blake" = "" #Blakecoin
     "vanilla" = " --intensity d" #BlakeVanilla
     #"c11" = "" #C11
-    #"cryptonight" = " --gpu-threads 1 --worksize 8 --rawintensity 896" #CryptoNight
-    "decred" = "" #Decred
     #"equihash" = " --gpu-threads 2 --worksize 256" #Equihash
     #"ethash" = " --gpu-threads 1 --worksize 192 --xintensity 1024" #Ethash
     "groestlcoin" = " --gpu-threads 2 --worksize 128 --intensity d" #Groestl
     #"hmq1725" = "" #HMQ1725
     #"jha" = "" #JHA
     "maxcoin" = "" #Keccak
-    "lbry" = "" #Lbry
     "lyra2rev2" = " --gpu-threads 2 --worksize 128 --intensity d" #Lyra2RE2
     #"lyra2z" = " --worksize 32 --intensity 18" #Lyra2z
-    "myriadcoin-groestl" = " --gpu-threads 2 --worksize 64 --intensity d" #MyriadGroestl
     "neoscrypt" = " --gpu-threads 1 --worksize 64 --intensity 15" #NeoScrypt
-    #"nist5" = "" #Nist5
     "pascal" = "" #Pascal
-    "sibcoin-mod" = "" #Sib
     "skeincoin" = " --gpu-threads 2 --worksize 256 --intensity d" #Skein
     #"skunk" = "" #Skunk
     #"timetravel" = "" #Timetravel
@@ -37,6 +29,17 @@ $Commands = [PSCustomObject]@{
     "x16S" = "" #X16s
     "yescrypt" = " --worksize 4 --rawintensity 256" #Yescrypt
     #"xevan-mod" = " --intensity 15" #Xevan
+    
+    # ASIC - never profitable 13/05/2018
+    #"blake2s" = "" #Blake2s
+    #"blake" = "" #Blakecoin
+    #"cryptonight" = " --gpu-threads 1 --worksize 8 --rawintensity 896" #CryptoNight
+    #"decred" = "" #Decred
+    #"lbry" = "" #Lbry
+    #"myriadcoin-groestl" = " --gpu-threads 2 --worksize 64 --intensity d" #MyriadGroestl
+    #"nist5" = "" #Nist5
+    #"sibcoin-mod" = "" #Sib
+    
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
