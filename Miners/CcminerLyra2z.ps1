cdf348e6cf86174008f0e3a9fd65e0c77e829859
Mar2 <martin.kirov@yahoo.com>
2018-05-15 11:01:35 +0300

    Update CcminerLyra2z.ps1

--- Miners/CcminerLyra2z.ps1
+++ Miners/CcminerLyra2z.ps1
@@ -1,11 +1,35 @@
-﻿using module ..\Include.psm1
+using module ..\Include.psm1
 
 $Path = ".\Bin\Lyra2z-NVIDIA\ccminer.exe"
+$HashSHA256 = "F9A69BA3C00E80BBBE7054E8705FE07DC23B7C408FA5405B778441A24D1AD223"
 $Uri = "https://github.com/djm34/ccminer-msvc2015/releases/download/v0.3.0/ccminer.rar"
 
 $Commands = [PSCustomObject]@{
-    "lyra2h" = "" #Lyra2h
-    "lyra2z" = "" #Lyra2z
+    "blake2s"   = "" #Blake2s
+    "blakecoin" = "" #Blakecoin
+    "c11"       = "" #C11
+    "groestl"   = "" #Groestl
+    "keccak"    = "" #Keccak
+    "lyra2h"    = "" #Lyra2h
+    #"lyra2v2"  = "" #Lyra2RE2 - Beaten by ccminerXevan by 80%
+    "lyra2z"    = "" #Lyra2z
+    "neoscrypt" = "" #NeoScrypt
+    "skein"     = "" #Skein
+    "x11evo"    = "" #X11evo
+    "x17"       = "" #X17
+    
+    # ASIC - never profitable 12/05/2018
+    #"decred"   = "" #Decred
+    #"lbry"     = "" #Lbry
+    #"myr-gr"   = "" #MyriadGroestl
+    #"nist5"    = "" #Nist5
+    #"qubit"    = "" #Qubit
+    #"quark"    = "" #Quark
+    #"sib"      = "" #Sib
+    #"x11"      = "" #X11
+    #"x12"      = "" #X12
+    #"x13"      = "" #X13
+    #"x14"      = "" #X14
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -14,6 +38,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
+        HashSHA256 = $HashSHA256
         Arguments = "-a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
         API = "Ccminer"
