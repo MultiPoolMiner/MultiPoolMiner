c5a61ce943e76aa0248c9ea14485efef7b4d67fe
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-05-19 08:36:05 +0200

    Update PalginNvidia.ps1

--- Miners/PalginNvidia.ps1
+++ Miners/PalginNvidia.ps1
@@ -37,7 +37,7 @@ $Commands | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Obj
             Path       = $Path
             HashSHA256 = $HashSHA256
             Arguments  = ("-o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)")
-            HashRates  = [PSCustomObject]@{"$Algorithm_Norm" = $HashRate}
+            HashRates  = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
             API        = $Api
             Port       = $Port
             URI        = $Uri
