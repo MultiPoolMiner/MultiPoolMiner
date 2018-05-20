a28af7bc9535b3989b5da9be237f4abc97954c11
angelics <josywong@hotmail.com>
2018-05-20 19:37:29 +0800

    convert miner to ps1
    convert miner to ps1

---
+++ Miners/ClaymoreCpu.ps1
@@ -0,0 +1,44 @@
+using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+$Path = ".\Bin\CryptoNight-Claymore-Cpu\NsCpuCNMiner64.exe"
+$HashSHA256 = "D7D80BF3F32C20298CAD1D59CA8CB4508BAD43A9BE5E027579D7FC77A8E47BE0"
+$API = "Claymore"
+$Uri = "https://github.com/MultiPoolMiner/miner-binaries/releases/download/claymorecpu/Claymore.CryptoNote.CPU.Miner.v4.0.-.POOL.zip"
+$Port = 3333
+
+$Commands = [PSCustomObject]@{
+    "CryptoNight"          = @("0","") #CryptoNight, first item is algo number, second for additional miner commands
+    "CryptoNightV7"        = @("1","") #CryptoNightV7
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_
+    
+    if ($Pools.$Algorithm_Norm) { # must have a valid pool to mine
+
+        $HashRate = ($Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week)
+
+        [PSCustomObject]@{
+            Name       = "CPU"
+            Type       = $Type
+            Path       = $Path
+            HashSHA256 = $HashSHA256
+            Arguments  = ("-r -1 -mport -$Port -pow7 $($Commands.$_ | Select-Object -Index 0) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_ | Select-Object -Index 1)\")
+            HashRates  = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
+            API        = $Api
+            Port       = $Port
+            URI        = $Uri
+            Fees       = @($null)
+        }
+    }
+}
---
+++ Miners/XmrigCPU.ps1
@@ -0,0 +1,54 @@
+using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject]$Devices
+)
+
+$Path = ".\Bin\CryptoNight-CPU\xmrig.exe"
+$HashSHA256 = "24661A8807F4B991C79E587E846AAEA589720ED84D79AFB41D14709A6FB908CE"
+$API = "XMRig"
+$Uri = "https://github.com/xmrig/xmrig/releases/download/v2.6.2/xmrig-2.6.2-msvc-win64.zip"
+$Port = 3335
+$MinerFeeInPercent = 1
+
+$Commands = [PSCustomObject]@{
+    "cn"       = "" #CryptoNightV7
+    "cn-heavy" = "" #CryptoNight-Heavy
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+if ($Config.IgnoreMinerFee -or $Config.Miners.$Name.IgnoreMinerFee) {
+    $Fees = @($null)
+}
+else {
+    $Fees = @($MinerFeeInPercent)
+}
+
+$Commands | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_
+    
+    if ($Pools.$Algorithm_Norm) { # must have a valid pool to mine
+
+        $HashRate = ($Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week)
+		
+        if ($Fees) {$HashRate = $HashRate * (1 - $MinerFeeInPercent / 100)}
+
+        [PSCustomObject]@{
+            Name       = $Name
+            Type       = "CPU"
+            Path       = $Path
+            HashSHA256 = $HashSHA256
+            Arguments  = ("--api-port $Port -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_) --keepalive --nicehash --donate-level 1")
+            HashRates  = [PSCustomObject]@{$Algorithm_Norm = $HashRate}
+            API        = $Api
+            Port       = $Port
+            URI        = $Uri
+            Fees       = @($Fees)
+        }
+    }
+}
