e7e1d235d7ea1d04ee1eb10a1610ef313262f868
iwtym <32946288+iwtym@users.noreply.github.com>
2018-05-29 20:14:18 -0400

    CcminerKlausT Opt

--- Miners/CcminerKlaust.ps1
+++ Miners/CcminerKlaust.ps1
@@ -5,41 +5,79 @@ $HashSHA256 = "EBF91E27F54DE29F158A4F5EBECEDB7E7E03EB9010331B2E949335BF1144A886"
 $Uri = "https://github.com/KlausT/ccminer/releases/download/8.21/ccminer-821-cuda91-x64.zip"
 
 $Commands = [PSCustomObject]@{
-    #GPU - profitable 20/04/2018
-    "c11"         = "" #C11
-    "deep"        = "" #deep
-    "dmd-gr"      = "" #dmd-gr
-    "fresh"       = "" #fresh
-    "fugue256"    = "" #Fugue256
-    "groestl"     = "" #Groestl
-    "jackpot"     = "" #Jackpot
-    "keccak"      = "" #Keccak
-    "luffa"       = "" #Luffa
-    "lyra2v2"     = "" #Lyra2RE2
-    "neoscrypt"   = "" #NeoScrypt
-    "penta"       = "" #Pentablake
-    "skein"       = "" #Skein
-    "s3"          = "" #S3
-    "veltor"      = "" #Veltor
-    #"whirlpool"  = "" #Whirlpool
-    #"whirlpoolx" = "" #whirlpoolx
-    "X17"         = "" #X17 Verge
+    ### SUPPORTED ALGORITHMS - BEST PERFORMING MINER
+    "groestl" = "" #Groestl
 
-    # ASIC - never profitable 20/04/2018
-    #"blake"      = "" #blake
-    #"blakecoin"  = "" #Blakecoin
-    #"blake2s"    = "" #Blake2s
-    #"myr-gr"     = "" #MyriadGroestl
-    #"nist5"      = "" #Nist5
-    #"quark"      = "" #Quark
-    #"qubit"      = "" #Qubit
-    #"vanilla"    = "" #BlakeVanilla
-    #"sha256d"    = "" #sha256d
-    #"sia"        = "" #SiaCoin
-    #"x11"        = "" #X11
-    #"x13"        = "" #x13
-    #"x14"        = "" #x14
-    #"x15"        = "" #x15
+    ### SUPPORTED ALGORITHMS - BEAT MY ANOTHER MINER
+    #"c11" = "" #c11
+    #"keccak" = "" #Keccak SHA3
+    #"lyra2v2" = "" #lyra2v2
+    #"neoscrypt" = "" #NeoScrypt
+    #"sha256t" = "" #Sha256t Sha256 triple
+    #"skein" = "" #Skein
+    #"x17" = "" #x17
+    
+    ### MAYBE SUPPORTED ALGORITHMS - NOT MINEABLE IN SUPPORTED POOLS AS OF 20/05/2018
+    ### these algorithms were not benchmarked into the leaderboard and 
+    ### should be benchmarked on a per miner basis if supported by pools
+    "deep" = "" #deep
+    "dmd-gr" = "" #dmd-gr
+    "fresh" = "" #fresh
+    "fugue256" = "" #Fugue256
+    "jackpot" = "" #JackPot
+    "luffa" = "" #Luffa
+    "penta" = "" #Pentablake
+    #"whirlpool" = "" #Whirlpool
+    #"whirlpoolx" = "" #whirlpoolx
+
+    ### UNSUPPORTED ALGORITHMS - AS OF 20/05/2018
+    ### these algorithms were tested as unsupported by the miner but
+    ### are usually profitable algorithms and should be rebenchmarked as 
+    ### applicable if the miner is modified or updated
+    #"bitcore" = "" #Bitcore
+    #"blake2s" = "" #Blake2s XVG
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"hmq1725" = "" #HMQ1725
+    #"hsr" = "" #HSR, HShare
+    #"keccakc" = "" #keccakc
+    #"lyra2z" = "" #Lyra2z, ZCoin
+    #"phi" = "" #PHI
+    #"skunk" = "" #Skunk
+    #"timetravel" = "" #Timetravel
+    #"tribus" = "" #Tribus
+    #"x11evo" = "" #X11evo
+    #"X16R" = "" #X16r
+    #"X16S" = "" #X16s
+
+    ### UNTESTED ALGORITHMS - AS OF 20/05/2018
+    ### test and rank as appropriate
+
+    ### UNPROFITABLE ALGORITHMS - AS OF 20/05/2018
+    ### these algorithms have been overtaken by ASIC
+    ### hardware and thus have been rendered unprofitable
+    ### these were omitted from all miners and may or may not
+    ### be mineable by this miner
+    #"blake256R14" = "" #Decred
+    #"blake256R8" = "" #VCash
+    #"blake2b" = "" #SIAcoin
+    #"cryptonight" = "" #Former monero algo
+    #"cryptonight-lite" = "" #AEON
+    #"equihash" = "" #Equihash
+    #"lbry" = "" #Lbry Credits
+    #"nist5" = "" #Bulwark
+    #"myr-gr" = "" #Myriad-Groestl
+    #"pascal" = "" #PascalCoin
+    #"quark" = "" #Quark
+    #"qubit" = "" #Qubit
+    #"sha256" = "" #Bitcoin also known as SHA256D (double)
+    #"scrypt" = "" #Litecoin
+    #"scrypt:n" = "" #ScryptN
+    #"x11" = "" #Dash
+    #"x11gost" = "" #Sibcoin Siberian Chervonets
+    #"x13" = "" #Stratis
+    #"x14" = "" #BERNcash
+    #"x15" = "" #Halcyon
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
