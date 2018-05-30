64c71742eaa1b718b37f5ea5d7b98edc18c749ed
iwtym <32946288+iwtym@users.noreply.github.com>
2018-05-29 20:16:35 -0400

    CcminerSib renamed to CcminerNHSib & Opt
    This is a nicehash fork with some minor changes - previous title was misleading.

---
+++ Miners/CcminerNHSib.ps1
@@ -0,0 +1,96 @@
+﻿using module ..\Include.psm1
+
+$Path = ".\Bin\Sib-NVIDIA\ccminer_x11gost.exe"
+$HashSHA256 = "CD8602A080728894570C9D29FC1423FB62AF1FA9CAF3913E609BA21777284DEE"
+$Uri = "https://github.com/nicehash/ccminer-x11gost/releases/download/ccminer-x11gost_windows/ccminer_x11gost.7z"
+
+$Commands = [PSCustomObject]@{
+    ### SUPPORTED ALGORITHMS - BEST PERFORMING MINER
+    "x11evo" = "" #X11evo
+
+    ### SUPPORTED ALGORITHMS - BEAT MY ANOTHER MINER
+    #"blake2s" = "" #Blake2s XVG
+    #"c11" = "" #c11
+    #"keccak" = "" #Keccak
+    #"lyra2v2" = "" #lyra2v2
+    #"neoscrypt" = "" #NeoScrypt
+    #"skein" = "" #Skein
+    #"x17" = "" #x17
+   
+    ### MAYBE SUPPORTED ALGORITHMS - NOT MINEABLE IN SUPPORTED POOLS AS OF 20/05/2018
+    ### these algorithms were not benchmarked into the leaderboard and 
+    ### should be benchmarked on a per miner basis if supported by pools
+    #"lyra2" = "" #Lyra2RE
+    "skein2" = "" #skein2
+    "veltor" = "" #Veltor
+    #"whirlpool" = "" #Whirlpool
+
+    ### UNSUPPORTED ALGORITHMS - AS OF 20/05/2018
+    ### these algorithms were tested as unsupported by the miner but
+    ### are usually profitable algorithms and should be rebenchmarked as 
+    ### applicable if the miner is modified or updated
+    #"bitcore" = "" #Bitcore
+    #"ethash" = "" #Ethash
+    #"equihash" = "" #Equihash
+    #"groestl" = "" #Groestl
+    #"hsr" = "" #HSR, HShare
+    #"hmq1725" = "" #HMQ1725
+    #"keccakc" = "" #keccakc
+    #"lyra2z" = "" #Lyra2z, ZCoin
+    #"phi" = "" #PHI
+    #"polytimos" = "" #Polytimos
+    #"sha256t" = "" #sha256t
+    #"skunk" = "" #Skunk
+    #"timetravel" = "" #Timetravel
+    #"tribus" = "" #Tribus
+    #"x11evo" = "" #X11evo
+    #"x16r" = "" #X16r
+    #"X16s" = "" #X16s
+
+    ### UNTESTED ALGORITHMS - AS OF 20/05/2018
+    ### test and rank as appropriate
+
+    ### UNPROFITABLE ALGORITHMS - AS OF 20/05/2018
+    ### these algorithms have been overtaken by ASIC
+    ### hardware and thus have been rendered unprofitable
+    ### these were omitted from all miners and may or may not
+    ### be mineable by this miner
+    #"blake256R14" = "" #Decred
+    #"blake256R8" = "" #VCash
+    #"blake2b" = "" #SIAcoin
+    #"cryptonight" = "" #Former monero algo
+    #"cryptonight-lite" = "" #AEON
+    #"equihash" = "" #Equihash
+    #"lbry" = "" #Lbry Credits
+    #"nist5" = "" #Bulwark
+    #"myr-gr" = "" #Myriad-Groestl
+    #"pascal" = "" #PascalCoin
+    #"quark" = "" #Quark
+    #"qubit" = "" #Qubit
+    #"sha256" = "" #Bitcoin also known as SHA256D (double)
+    #"scrypt" = "" #Litecoin
+    #"scrypt:n" = "" #ScryptN
+    #"x11" = "" #Dash
+    #"x11gost" = "" #Sibcoin Siberian Chervonets
+    #"x13" = "" #Stratis
+    #"x14" = "" #BERNcash
+    #"x15" = "" #Halcyon
+
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        HashSHA256 = $HashSHA256
+        Arguments = "-a $_ -o $($Pools.(Get-Algorithm $_).Protocol)://$($Pools.(Get-Algorithm $_).Host):$($Pools.(Get-Algorithm $_).Port) -u $($Pools.(Get-Algorithm $_).User) -p $($Pools.(Get-Algorithm $_).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm $_) = $Stats."$($Name)_$(Get-Algorithm $_)_HashRate".Week}
+        API = "Ccminer"
+        Port = 4068
+        URI = $Uri
+        PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
+        PrerequisiteURI = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+    }
+}
