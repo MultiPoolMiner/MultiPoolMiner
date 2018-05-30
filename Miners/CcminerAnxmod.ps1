3d4935e81e83f9dcf4358ff56f40ec41ea05a819
iwtym <32946288+iwtym@users.noreply.github.com>
2018-05-29 20:13:34 -0400

    CcminerAnxmodPhi renamed to CcminerAnxmod & Opt
    Does a lot more algos fast other than phi.

---
+++ Miners/CcminerAnxmod.ps1
@@ -0,0 +1,91 @@
+﻿using module ..\Include.psm1
+
+$Path = ".\Bin\NVIDIA-PHI\ccminer.exe"
+$HashSHA256 = "6C3AFFABBD39566F4AE3B78CF55BFEA06BE98C1D48FDDB27DEE406DB87B7663F"
+$URI = "https://github.com/216k155/ccminer-phi-anxmod/releases/download/ccminer%2Fphi-1.0/ccminer-phi-1.0.zip"
+
+$Commands = [PSCustomObject]@{
+    ### SUPPORTED ALGORITHMS - BEST PERFORMING MINER
+    "blake2s" = "" #Blake2s XVG
+    "c11" = " -i 20" #c11
+    "hsr" = "" #HSR, HShare
+    "phi" = "" #PHI
+    "x11evo" = " -i 19" #X11evo
+    "x17" = " -i 21" #x17
+
+    ### SUPPORTED ALGORITHMS - BEAT MY ANOTHER MINER
+    #"keccak" = "" #Keccak SHA3
+    #"lyra2v2" = "" #lyra2v2
+    #"neoscrypt" = " -i 14" #NeoScrypt
+    #"skein" = "" #Skein
+    
+    ### MAYBE SUPPORTED ALGORITHMS - NOT MINEABLE IN SUPPORTED POOLS AS OF 20/05/2018
+    ### these algorithms were not benchmarked into the leaderboard and 
+    ### should be benchmarked on a per miner basis if supported by pools
+    "skein2" = "" #skein2
+    #"whirlpool" = "" #Whirlpool
+    "veltor" = "" #Veltor
+
+    ### UNSUPPORTED ALGORITHMS - AS OF 20/05/2018
+    ### these algorithms were tested as unsupported by the miner but
+    ### are usually profitable algorithms and should be rebenchmarked as 
+    ### applicable if the miner is modified or updated
+    #"bitcore" = "" #Bitcore
+    #"ethash" = "" #Ethash
+    #"equihash" = "" #Equihash
+    #"groestl" = "" #Groestl
+    #"hmq1725" = "" #HMQ1725
+    #"keccakc" = "" #keccakc
+    #"lyra2z" = "" #Lyra2z, ZCoin
+    #"polytimos" = "" #Polytimos
+    #"skunk" = "" #Skunk
+    #"timetravel" = "" #Timetravel
+    #"tribus" = "" #Tribus
+
+    ### UNTESTED ALGORITHMS - AS OF 20/05/2018
+    ### test and rank as appropriate
+
+    ### UNPROFITABLE ALGORITHMS - AS OF 20/05/2018
+    ### these algorithms have been overtaken by ASIC
+    ### hardware and thus have been rendered unprofitable
+    ### these were omitted from all miners and may or may not
+    ### be mineable by this miner
+    #"blake256R14" = "" #Decred
+    #"blake256R8" = "" #VCash
+    #"blake2b" = "" #SIAcoin
+    #"cryptonight" = "" #Former monero algo
+    #"cryptonight-lite" = "" #AEON
+    #"equihash" = "" #Equihash
+    #"lbry" = "" #Lbry Credits
+    #"nist5" = "" #Bulwark
+    #"myr-gr" = "" #Myriad-Groestl
+    #"pascal" = "" #PascalCoin
+    #"quark" = "" #Quark
+    #"qubit" = "" #Qubit
+    #"sha256" = "" #Bitcoin also known as SHA256D (double)
+    #"scrypt" = "" #Litecoin
+    #"scrypt:n" = "" #ScryptN
+    #"x11" = "" #Dash
+    #"x11gost" = "" #Sibcoin Siberian Chervonets
+    #"x13" = "" #Stratis
+    #"x14" = "" #BERNcash
+    #"x15" = "" #Halcyon
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+
+    $Algorithm_Norm = Get-Algorithm $_
+
+    [PSCustomObject]@{
+        Type       = "NVIDIA"
+        Path       = $Path
+        HashSHA256 = $HashSHA256
+        Arguments  = "-a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_)"
+        HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week}
+        API        = "Ccminer"
+        Port       = 4068
+        URI        = $Uri
+    }
+}
