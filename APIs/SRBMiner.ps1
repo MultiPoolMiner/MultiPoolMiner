068b8c411a6366c6bdea7d832749f0a256afe0a1
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-06-01 00:58:43 +0200

    Add SRBMiner API

---
+++ APIs/SRBMiner.ps1
@@ -0,0 +1,39 @@
+﻿using module ..\Include.psm1
+
+class Dstm : Miner {
+    [String[]]UpdateMinerData () {
+        if ($this.GetStatus() -ne [MinerStatus]::Running) {return @()}
+
+        $Server = "localhost"
+        $Timeout = 10 #seconds
+
+        $Request = @{id = 1; method = "getstat"} | ConvertTo-Json -Compress
+        $Response = ""
+
+        $HashRate = [PSCustomObject]@{}
+
+        try {
+            $Response = Invoke-TcpRequest $Server $this.Port $Request $Timeout -ErrorAction Stop
+            $Data = $Response | ConvertFrom-Json -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Error "Failed to connect to miner ($($this.Name)). "
+            return @($Request, $Response)
+        }
+
+        $HashRate_Name = [String]$this.Algorithm[0]
+        $HashRate_Value = [Double]($Data.result.sol_ps | Measure-Object -Sum).Sum
+        if (-not $HashRate_Value) {$HashRate_Value = [Double]($Data.result.speed_sps | Measure-Object -Sum).Sum} #ewbf fix
+
+        $HashRate | Where-Object {$HashRate_Name} | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+
+        $this.Data += [PSCustomObject]@{
+            Date     = (Get-Date).ToUniversalTime()
+            Raw      = $Response
+            HashRate = $HashRate
+            Device   = @()
+        }
+
+        return @($Request, $Data | ConvertTo-Json -Compress)
+    }
+}
