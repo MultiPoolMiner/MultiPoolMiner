80dada916987c132cc5f6e91c52e43c18c294274
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-06-12 21:49:30 +0200

    Add missing algos
    Add miner fee support

--- MinersLegacy/sgminer-kl.ps1
+++ MinersLegacy/sgminer-kl.ps1
@@ -1,18 +1,25 @@
-using module ..\Include.psm1
+﻿using module ..\Include.psm1
 
 $Path = ".\Bin\sgminer-kl\sgminer.exe"
 $HashSHA256 = "A24024BEA8789B62D61CB3F41432EA1A62EE5AD97CD3DEAB1E2308F40B127A4D"
 $Uri = "https://github.com/KL0nLutiy/sgminer-kl/releases/download/kl-1.0.5fix/sgminer-kl-1.0.5_fix-windows_x64.zip"
+$ManualUri = "https://github.com/KL0nLutiy"
 
 $Commands = [PSCustomObject]@{
-    "Aergo" = " -X 256 -g 2" #aergo
-	"Phi" = "  -X 256 -g 2 -w 256" #phi
-	"Tribus" = "  -X 256 -g 2" #tribus
-	"x16r" = "  -X 256 -g 2" #x16r
-	"x16s" = "  -X 256 -g 2" #x16s
-	"X17" = "  -X 256 -g 2" #x17
-	"Xevan" = "  -X 256 -g 2" #xevan
-
+  "aergo"     = " -X 256 -g 2" #Aergo
+  "blake"     = "" #Blake
+  "bmw"       = "" #Bmw
+  "echo"      = "" #Echo
+  "hamsi"     = "" #Hamsi
+  "keccak"    = "" #Keccak
+  "phi"       = " -X 256 -g 2 -w 256" # Phi
+  "skein"     = "" #Skein
+  "tribus"    = " -X 256 -g 2" #Tribus
+  "whirlpool" = "" #Whirlpool
+  "xevan"     = " -X 256 -g 2" #Xevan
+  "x16s"      = " -X 256 -g 2" #X16S Pigeoncoin
+  "x16r"      = " -X 256 -g 2" #X16R Ravencoin
+  "x17"       = " -X 256 -g 2"
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -20,6 +27,13 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
 
     $Algorithm_Norm = Get-Algorithm $_
+
+    if ($Config.IgnoreCosts -or $Config.Miners.$Name.IgnoreCosts) {
+        $Miner_Fees = [PSCustomObject]@{"$Algorithm_Norm" = 0 / 100}
+    }
+    else {
+        $Miner_Fees = [PSCustomObject]@{"$Algorithm_Norm" = 1 / 100}
+    }
 
     [PSCustomObject]@{
         Type       = "AMD"
@@ -30,5 +44,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         API        = "Xgminer"
         Port       = 4028
         URI        = $Uri
+        Fees       = $Miner_Fees
     }
 }
+
