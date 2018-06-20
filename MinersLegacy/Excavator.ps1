e973993d8a6e446b3738aaa083abb282f993a8da
Aaron Simmonds <aaronsace@multipoolminer.io>
2018-06-20 21:18:10 +0100

    Update Excavator.ps1

--- MinersLegacy/Excavator.ps1
+++ MinersLegacy/Excavator.ps1
@@ -43,7 +43,7 @@ $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty Ba
 $Devices = $Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "NVIDIA Corporation"
 
 $Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
-    $Miner_Device = $Devices | Where-Object Model -EQ $_.Model
+    $Miner_Device = $Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model 
     $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)
 
     $Commands | ForEach-Object {
