22e5da7e77c1a8124b87a5de940377ad92bc2f1c
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-06-27 07:03:40 +0200

    Update MultiPoolMiner.ps1

--- MultiPoolMiner.ps1
+++ MultiPoolMiner.ps1
@@ -743,7 +743,7 @@ while ($true) {
                 if ($Miner.New -and (-not $Miner_Speed)) {$Miner_Speed = $Miner.GetHashRate($_, ($Config.Interval * $Miner.Benchmarked), ($Miner.Benchmarked -lt $Strikes))}
 
                 if ((-not $Miner.New) -or $Miner_Speed -or $Miner.Benchmarked -ge ($Strikes * $Strikes) -or $Miner.GetActivateCount() -ge $Strikes) {
-                    $Stat = Set-Stat -Name "$($Miner.Name)_$($_)_HashRate" -Value $Miner_Speed -Duration $StatSpan -FaultDetection (-not $_.ExtendInterval)
+                    $Stat = Set-Stat -Name "$($Miner.Name)_$($_)_HashRate" -Value $Miner_Speed -Duration $StatSpan -FaultDetection (-not $Miner.ExtendInterval)
                 }
 
                 #Update watchdog timer
