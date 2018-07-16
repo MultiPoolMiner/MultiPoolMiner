d12075ea8351f65096113ff054a33fc57d21afc4
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-07-16 19:57:52 +0200

    Update XmRic CPU to version 2.6.4
    Changes:
    - cryptonight-lite/ipbc replaced to cryptonight-heavy/tube for Bittube (TUBE).
    - Added cryptonight/rto (cryptonight variant 1 with IPBC/TUBE mod) variant for Arto (RTO) coin.
    - Added cryptonight/xao (original cryptonight with bigger iteration count) variant for Alloy (XAO) coin.
    Better variant detection for nicehash.com and minergate.com.
    -Added support for specify both algorithm and variant via single algo option.

---
+++ MinersLegacy/XmRigCpu.ps1
@@ -0,0 +1,60 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject[]]$Devices
+)
+
+$Path = ".\Bin\CPU-XmRig-Cryptonight\xmrig.exe"
+$HashSHA256 = "73C345AB59538A0B4332BA0FB3659F111AAC4291F169D24DA6D5186FEC0B2712"
+$Uri = "https://github.com/xmrig/xmrig/releases/download/v2.6.4/xmrig-2.6.4-msvc-win64.zip"
+$ManualUri = "https://github.com/xmrig/xmrig-cpu"
+$Port = "339{0:d1}"
+
+$Commands = [PSCustomObject]@{
+    "cryptonight/1"          = "" #CryptoNightV7; Also known as monero7 and CryptoNightV7
+    "cryptonight/xtl"        = "" #CryptoNightXtl; Stellite (XTL)
+    "cryptonight/msr"        = "" #CryptoNightFast; also known as cryptonight-masari (MSR)
+    "cryptonight/xao"        = "" #CryptoNightAlloy (XAO)
+    "cryptonight/rto"        = "" #CryptoNightArto (RTO)
+    "cryptonight-lite"       = "" #CryptoNightLite; Autodetect works only for Aeon
+    "cryptonight-lite/0"     = "" #CryptoNightLite; Original/old CryptoNight-Lite
+    "cryptonight-lite/1"     = "" #CryptoNightLiteV7; also known as aeon7
+    "cryptonight-heavy"      = "" #CryptoNightHeavy; Ryo and Loki
+    "cryptonight-heavy/xhv"  = "" #CryptoNightHaven Protocol
+    "cryptonight-heavy/tube" = "" #CryptoNightHeavyTube (TUBE)
+
+    # ASIC only (09/07/2018)
+    #"cryptonight"            = ""} #Autodetect works only for Monero
+    #"cryptonight"            = ""} #Original/old CryptoNight
+}
+$CommonCommands = ""
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+$Devices = @($Devices | Where-Object Type -EQ "CPU")
+
+$Devices | Select-Object Model -Unique | ForEach-Object {
+    $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model)
+    $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)    
+    $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+
+    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+
+        $Algorithm_Norm = Get-Algorithm $_
+
+        [PSCustomObject]@{
+            Name       = $Miner_Name
+            DeviceName = $Miner_Device.Name
+            Path       = $Path
+            HashSHA256 = $HashSHA256
+            Arguments  = "--api-port $($Miner_Port) -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) --keepalive --nicehash --rig-id=$($Worker) --donate-level 1$($Commands.$_)$CommonCommands"
+            HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+            API        = "XmRig"
+            Port       = $Miner_Port
+            URI        = $Uri
+            Fees       = [PSCustomObject]@{"$Algorithm_Norm" = 1 / 100}
+        }
+    }
+}
