864022939c818f9513c9e3e76b9b3916a3a88b5d
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-07-17 15:49:15 +0200

    Add zhash & aion
    Requires commit https://github.com/MultiPoolMiner/MultiPoolMiner/pull/1979/commits/a66b04692e2d4571678fe0092ce7611c4a69e6fa

--- MinersLegacy/EWBF2.ps1
+++ MinersLegacy/EWBF2.ps1
@@ -14,10 +14,11 @@ $ManualUri = "https://mega.nz/#F!fsAlmZQS!CwVgFfBDduQI-CbwVkUEpQ"
 $Port = "421{0:d2}"
 
 $Commands = [PSCustomObject[]]@(
-    [PSCustomObject]@{Algorithm = "Equihash-96_5";  MinMemGB = 2; Params = ""}
-    [PSCustomObject]@{Algorithm = "Equihash-144_5"; MinMemGB = 2; Params = ""}
-    [PSCustomObject]@{Algorithm = "Equihash-192_7"; MinMemGB = 3; Params = ""}
-    [PSCustomObject]@{Algorithm = "Equihash-210_9"; MinMemGB = 2; Params = ""}
+    [PSCustomObject]@{Algorithm = "Equihash-96_5"; Pers = ""; MinMemGB = 2; Params = ""}
+    [PSCustomObject]@{Algorithm = "Equihash-144_5"; Pers = ""; MinMemGB = 2; Params = ""}
+    [PSCustomObject]@{Algorithm = "Equihash-192_7"; Pers = ""; MinMemGB = 3; Params = ""}
+    [PSCustomObject]@{Algorithm = "aion"; Pers = " --pers AION0PoW"; MinMemGB = 2; Params = ""} #https://bitcointalk.org/index.php?topic=4466962.msg42333802#msg42333802
+    [PSCustomObject]@{Algorithm = "zhash"; Pers = " --pers BitcoinZ"; MinMemGB = 2; Params = ""} # https://twitter.com/bitcoinzteam/status/1008283738999021568?lang=en
 )
 
 $CommonCommands = " --fee 0 --intensity 64"
@@ -38,12 +39,16 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
     $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model)
     $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)    
     $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-                
+
     $Commands | Where-Object {$Pools.(Get-Algorithm $_.Algorithm).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
 
         $Algorithm = ($_.Algorithm) -replace "Equihash-"
         $Algorithm_Norm = Get-Algorithm $_.Algorithm
         $MinMemGB = $_.MinMemGB
+
+        #Pers parameter, can be different per coin
+        if ($_.Pers) {$Pers = $_.Pers}
+        else {$Pers = $($Coins."$($Pools.$Algorithm_Norm.CoinName)")}
         
         if ($Miner_Device = @($Miner_Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGB * 1000000000)})) {
             [PSCustomObject]@{
@@ -51,7 +56,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
                 DeviceName       = $Miner_Device.Name
                 Path             = $Path
                 HashSHA256       = $HashSHA256
-                Arguments        = ("--algo $($Algorithm)$($Coins."$($Pools.$Algorithm_Norm.CoinName)") --eexit 1 --api 127.0.0.1:$($Miner_Port) --server $($Pools.$Algorithm_Norm.Host) --port $($Pools.$Algorithm_Norm.Port) --user $($Pools.$Algorithm_Norm.User) --pass $($Pools.$Algorithm_Norm.Pass)$($_.Params)$CommonCommands --cuda_devices $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join '')" -replace "\s+", " ").trim()
+                Arguments        = ("--algo $Algorithm$Pers --eexit 1 --api 127.0.0.1:$Miner_Port --server $($Pools.$Algorithm_Norm.Host) --port $($Pools.$Algorithm_Norm.Port) --user $($Pools.$Algorithm_Norm.User) --pass $($Pools.$Algorithm_Norm.Pass)$($_.Params)$CommonCommands --cuda_devices $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join '')" -replace "\s+", " ").trim()
                 HashRates        = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API              = "DSTM"
                 Port             = $Miner_Port
