d51596a2a63d8653278ca17e6b675547b52680cc
Grant Emsley <grant@emsley.ca>
2018-07-30 09:26:24 -0400

    Fix setting variable to itself
    It's redundant.

--- Include.psm1
+++ Include.psm1
@@ -119,7 +119,7 @@ function Set-Stat {
     $Stat = Get-Content $Path -ErrorAction SilentlyContinue
     
     try {
-        $Stat = $Stat | ConvertFrom-Json -ErrorAction Stop
+        $Stat | ConvertFrom-Json -ErrorAction Stop
         $Stat = [PSCustomObject]@{
             Live = [Double]$Stat.Live
             Minute = [Double]$Stat.Minute
