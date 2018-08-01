0012f0bea04eec06fdbaaf116a7cdd082b0440d3
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-08-01 13:03:46 +0200

    Unify miner
    Only one miner file for AMD and Nvidia

--- MinersLegacy/Ethminer.ps1
+++ MinersLegacy/Ethminer.ps1
@@ -21,11 +21,17 @@ $Commands = [PSCustomObject[]]@(
 $CommonCommands = ""
 
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
-$Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "NVIDIA Corporation")
+$Devices = @($Devices | Where-Object Type -EQ "GPU")
 
-$Devices | Select-Object Model -Unique | ForEach-Object {
-    $Device = @($Devices | Where-Object Model -EQ $_.Model)
+$Devices | Select-Object Vendor, Model -Unique | ForEach-Object {
+    $Device = @($Devices | Where-Object Vendor -EQ $_.Vendor | Where-Object Model -EQ $_.Model)
     $Miner_Port = $Port -f ($Device | Select-Object -First 1 -ExpandProperty Index)
+
+    switch ($_.Vendor) {
+        "Advanced Micro Devices, Inc." {$Arguments_Platform = " --opencl --opencl-platform "}
+        "NVIDIA Corporation" {$Arguments_Platform = " --cuda --cuda-devices "}
+        Default {$Arguments_Platform = ""}
+    }
 
     $Commands | Where-Object {$Pools.(Get-Algorithm $_.Algorithm).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
 
@@ -44,7 +50,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
                 DeviceName = $Miner_Device.Name
                 Path       = $Path
                 HashSHA256 = $HashSHA256
-                Arguments  = ("--api-port -$Miner_Port -P $($Protocol)://$([System.Web.HttpUtility]::UrlEncode($Pools.$Algorithm_Norm.User)):$([System.Web.HttpUtility]::UrlEncode($Pools.$Algorithm_Norm.Pass))@$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port)$($Commands.$_)$CommonCommands --cuda --cuda-devices $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ' ')" -replace "\s+", " ").trim()
+                Arguments  = ("--api-port -$Miner_Port -P $($Protocol)://$([System.Web.HttpUtility]::UrlEncode($Pools.$Algorithm_Norm.User)):$([System.Web.HttpUtility]::UrlEncode($Pools.$Algorithm_Norm.Pass))@$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port)$($Commands.$_)$($_.Params)$CommonCommands$Arguments_Platform$(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ' ')" -replace "\s+", " ").trim()
                 HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API        = "Claymore"
                 Port       = $Miner_Port
