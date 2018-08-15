2b326381e0cb913910f74a7344951b0e64b468fe
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-08-15 07:16:25 +0200

    Bminer API version 8.0

--- APIs/BMiner.ps1
+++ APIs/BMiner.ps1
@@ -13,24 +13,41 @@ class BMiner : Miner {
         $HashRate = [PSCustomObject]@{}
 
         try {
-            $Response = Invoke-WebRequest "http://$($Server):$($this.Port)/api/status" -UseBasicParsing -TimeoutSec $Timeout -ErrorAction Stop
+            $Response = Invoke-WebRequest "http://$($Server):$($this.Port)/api/v1/status/solver" -UseBasicParsing -TimeoutSec $Timeout -ErrorAction Stop
             $Data = $Response | ConvertFrom-Json -ErrorAction Stop
         }
         catch {
-            Write-Log -Level Error "Failed to connect to miner ($($this.Name)). "
+            if ($this.Process.PSBeginTime -lt (Get-Date).AddSeconds( - 30)) { #Allow some time for the miner to respond
+                Write-Log -Level Error "Failed to connect to miner ($($this.Name)[$($this.Pool)]). "
+                $this.SetStatus("Idle")
+            }
             return @($Request, $Response)
         }
 
-        $HashRate_Name = [String]$this.Algorithm[0]
-        $HashRate_Value = [Double]($Data.miners | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {$Data.miners.$_.solver.solution_rate} | Measure-Object -Sum).Sum
+        $this.Algorithm | Select-Object -Unique | ForEach-Object {
+            $HashRate_Name = [String]($this.Algorithm -like (Get-Algorithm $_))
+            if (-not $HashRate_Name) {$HashRate_Name = [String]($this.Algorithm -like "$(Get-Algorithm $_)*")} #temp fix
 
-        $HashRate | Where-Object {$HashRate_Name} | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+            $HashRate_Value = 0
 
-        $this.Data += [PSCustomObject]@{
-            Date     = (Get-Date).ToUniversalTime()
-            Raw      = $Response
-            HashRate = $HashRate
-            Device   = @()
+            $Data.devices | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | ForEach-Object {
+                $Data.devices.$_.solvers | Where-Object {$HashRate_Name -like "$(Get-Algorithm $_.Algorithm)*"} | ForEach-Object {
+                    if ($_.speed_info.hash_rate) {$HashRate_Value += $_.speed_info.hash_rate}
+                    else {$HashRate_Value += $_.speed_info.solution_rate}
+                }
+            }
+            if ($HashRate_Name -and $HashRate_Value -gt 0) {
+                $HashRate | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+            }
+        }
+
+        if ($HashRate) {
+            $this.Data += [PSCustomObject]@{
+                Date     = (Get-Date).ToUniversalTime()
+                Raw      = $Response
+                HashRate = $HashRate
+                Device   = @()
+            }
         }
 
         return @($Request, $Data | ConvertTo-Json -Compress)
