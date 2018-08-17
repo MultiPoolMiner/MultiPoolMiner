b7099f63ca18bb18759336c5d4807c791b8202b9
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-08-17 18:20:44 +0200

    Removed Groestl, added Skein and m7m

--- Start-MiningPoolHub.bat
+++ Start-MiningPoolHub.bat
@@ -7,7 +7,7 @@
 @if not "%GPU_SINGLE_ALLOC_PERCENT%"=="100" (setx GPU_SINGLE_ALLOC_PERCENT 100) > nul
 @if not "%CUDA_DEVICE_ORDER%"=="PCI_BUS_ID" (setx CUDA_DEVICE_ORDER PCI_BUS_ID) > nul
 
-@set "command=& .\multipoolminer.ps1 -Wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -UserName aaronsace -WorkerName multipoolminer -Region europe -Currency btc,usd,eur -DeviceName amd,nvidia,cpu -PoolName miningpoolhub,miningpoolhubcoins -Algorithm cryptonightv7,cryptonightheavy,ethash,ethash2gb,ethash3gb,equihash,groestl,lyra2re2,lyra2z,neoscrypt -Donate 24 -Watchdog -MinerStatusURL https://multipoolminer.io/monitor/miner.php -SwitchingPrevention 2"
+@set "command=& .\multipoolminer.ps1 -Wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -UserName aaronsace -WorkerName multipoolminer -Region europe -Currency btc,usd,eur -DeviceName amd,nvidia,cpu -PoolName miningpoolhub,miningpoolhubcoins -Algorithm cryptonightv7,cryptonightheavy,ethash,ethash2gb,ethash3gb,equihash,lyra2re2,lyra2z,m7m,neoscrypt,skein -Donate 24 -Watchdog -MinerStatusURL https://multipoolminer.io/monitor/miner.php -SwitchingPrevention 2"
 
 start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log 'MultiPoolMiner_\d\d\d\d-\d\d-\d\d\.txt' -sort '^[^_]*_' -quickstart"
 start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log '^((?!MultiPoolMiner_.+\.txt).)*$' -sort '^[^_]*_' -quickstart"
