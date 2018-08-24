d6dc3ef10fdd1c2ac27758fce5a88252a81d1b57
Mar2 <martin.kirov@yahoo.com>
2018-08-24 14:40:54 +0300

    Update start.bat algos
    Update start.bat to start only with profitable algos out of the box. If anyone likest to can enable any algo by editing -excludealgorithm

--- Start.bat
+++ Start.bat
@@ -1,4 +1,4 @@
-@cd /d %~dp0
+l@cd /d %~dp0
 
 @if not "%GPU_FORCE_64BIT_PTR%"=="1" (setx GPU_FORCE_64BIT_PTR 1) > nul
 @if not "%GPU_MAX_HEAP_SIZE%"=="100" (setx GPU_MAX_HEAP_SIZE 100) > nul
@@ -7,7 +7,7 @@
 @if not "%GPU_SINGLE_ALLOC_PERCENT%"=="100" (setx GPU_SINGLE_ALLOC_PERCENT 100) > nul
 @if not "%CUDA_DEVICE_ORDER%"=="PCI_BUS_ID" (setx CUDA_DEVICE_ORDER PCI_BUS_ID) > nul
 
-@set "command=& .\multipoolminer.ps1 -wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -username aaronsace -workername multipoolminer -region europe -currency btc,usd,eur -DeviceName amd,nvidia,cpu -poolname miningpoolhubcoins,zpool,nicehash -algorithm blake2s,cryptonightV7,decrednicehash,ethash,ethash2gb,equihash,groestl,keccak,lbry,lyra2re2,lyra2z,neoscrypt,pascal,sib,skunk -donate 24 -watchdog -minerstatusurl https://multipoolminer.io/monitor/miner.php -switchingprevention 2"
+@set "command=& .\multipoolminer.ps1 -wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -username aaronsace -workername multipoolminer -region europe -currency btc,usd,eur -DeviceName amd,nvidia,cpu -poolname miningpoolhubcoins,zpool,nicehash -excludealgorithm Bitcore,Blake2s,Blakecoin,BlakeVanilla,CryptoLight,CryptoNight,Groestl,Lbry,Lyra2RE,Decred,Quark,Qubit,MyriadGroestl,Nist5,Scrypt,ScryptN,SHA256d,SHA256t,Sia,Sib,X11,X13,X14,X15 -donate 24 -watchdog -minerstatusurl https://multipoolminer.io/monitor/miner.php -switchingprevention 2"
 
 start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log 'MultiPoolMiner_\d\d\d\d-\d\d-\d\d\.txt' -sort '^[^_]*_' -quickstart"
 start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log '^((?!MultiPoolMiner_.+\.txt).)*$' -sort '^[^_]*_' -quickstart"
