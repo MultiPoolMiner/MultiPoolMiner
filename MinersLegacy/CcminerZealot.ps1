16d9a08cfafd49a373cabaf81351b66046a6b700
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-08-31 14:02:33 +0200

    Fix double display of version number

--- MinersLegacy/CcminerZealot.ps1
+++ MinersLegacy/CcminerZealot.ps1
@@ -38,7 +38,7 @@ $CommonCommands = ""
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 
 # Miner requires CUDA 9.2
-$DriverVersion = (Get-Device | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "NVIDIA Corporation").OpenCL.Platform.Version -replace ".*CUDA ",""
+$DriverVersion = (Get-Device | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "NVIDIA Corporation" | Select-Object -Unique).OpenCL.Platform.Version -replace ".*CUDA ",""
 $RequiredVersion = "9.2.00"
 if ($DriverVersion -and $DriverVersion -lt $RequiredVersion) {
     Write-Log -Level Warn "Miner ($($Name)) requires CUDA version $($RequiredVersion) or above (installed version is $($DriverVersion)). Please update your Nvidia drivers. "
