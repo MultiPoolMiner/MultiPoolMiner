1aa1177463c75c1890531a88d3471e5c3a0e88ec
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-09-01 00:52:24 +0200

    Fix auto coin switch for Equihash144_5

--- MinersLegacy/EWBF2.ps1
+++ MinersLegacy/EWBF2.ps1
@@ -10,7 +10,7 @@ param(
 $Path = ".\Bin\NVIDIA-EWBF2-Equihash\miner.exe"
 $HashSHA256 = "BB17BA6C699F6BC7A4465E641E15E1A7AABF1D884BF908A603DBAA1A705EDCD9"
 $Uri = "https://github.com/MultiPoolMiner/miner-binaries/releases/download/EWBF2/EWBF.Equihash.miner.v0.5.zip"
-$ManualUri = "https://mega.nz/#F!fsAlmZQS!CwVgFfBDduQI-CbwVkUEpQ"
+$ManualUri = "https://bitcointalk.org/index.php?topic=4466962.0"
 $Port = "40{0:d2}"
 
 $Commands = [PSCustomObject[]]@(
@@ -44,34 +44,37 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
 
     $Commands | Where-Object {$Pools.(Get-Algorithm $_.Algorithm).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
 
-        $Algorithm = ($_.Algorithm) -replace "Equihash-"
         $Algorithm_Norm = Get-Algorithm $_.Algorithm
+        $Algorithm = ($_.Algorithm) -replace "Equihash-"
         $MinMemGB = $_.MinMemGB
 
-        #Pers parameter, can be different per coin
-        if ($Coins."$($Pools.$Algorithm_Norm.CoinName)") {
-            $Pers = $Coins."$($Pools.$Algorithm_Norm.CoinName)"
-        }
-        #ZergPool allows pers auto switching; https://bitcointalk.org/index.php?topic=2759935.msg43324268#msg43324268
-        if ($Pools.$Algorithm_Norm.Name -like "ZergPool*") {
-            $Pers = " --pers auto"
-        }
-
         if ($Miner_Device = @($Miner_Device | Where-Object {$_.OpenCL.GlobalMemsize -ge ($MinMemGB * 1000000000)})) {
-            [PSCustomObject]@{
-                Name             = $Miner_Name
-                DeviceName       = $Miner_Device.Name
-                Path             = $Path
-                HashSHA256       = $HashSHA256
-                Arguments        = ("--algo $Algorithm$Pers --eexit 1 --api 127.0.0.1:$Miner_Port --server $($Pools.$Algorithm_Norm.Host) --port $($Pools.$Algorithm_Norm.Port) --user $($Pools.$Algorithm_Norm.User) --pass $($Pools.$Algorithm_Norm.Pass)$($_.Params)$CommonCommands --cuda_devices $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ' ')" -replace "\s+", " ").trim()
-                HashRates        = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
-                API              = "DSTM"
-                Port             = $Miner_Port
-                URI              = $Uri
-                Fees             = [PSCustomObject]@{$Algorithm_Norm = 1 / 100}
-                ExtendInterval   = 2
-                PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
-                PrerequisiteURI  = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+
+            #ZergPool allows pers auto switching; https://bitcointalk.org/index.php?topic=2759935.msg43324268#msg43324268
+            if ($Pools.$Algorithm_Norm.Name -like "ZergPool*") {
+                $Pers = " --pers auto"
+            }
+            #Pers parameter, different per coin
+            else {
+                $Pers = $Coins."$($Pools.$Algorithm_Norm.CoinName)"
+            }
+
+            if ($Algorithm_Norm -ne "Equihash1445" -or $Pers) {
+                [PSCustomObject]@{
+                    Name             = $Miner_Name
+                    DeviceName       = $Miner_Device.Name
+                    Path             = $Path
+                    HashSHA256       = $HashSHA256
+                    Arguments        = ("--algo $Algorithm$Pers --eexit 1 --api 127.0.0.1:$Miner_Port --server $($Pools.$Algorithm_Norm.Host) --port $($Pools.$Algorithm_Norm.Port) --user $($Pools.$Algorithm_Norm.User) --pass $($Pools.$Algorithm_Norm.Pass)$($_.Params)$CommonCommands --cuda_devices $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ' ')" -replace "\s+", " ").trim()
+                    HashRates        = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                    API              = "DSTM"
+                    Port             = $Miner_Port
+                    URI              = $Uri
+                    Fees             = [PSCustomObject]@{$Algorithm_Norm = 1 / 100}
+                    BenchmarkSamples = 10
+                    PrerequisitePath = "$env:SystemRoot\System32\msvcr120.dll"
+                    PrerequisiteURI  = "http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe"
+                }
             }
         }
     }
