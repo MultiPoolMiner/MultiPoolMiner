2672fd77371cdf03add52637ae022e71a2d4fc48
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-09-01 19:16:10 +0200

    Fix CryptoNIght* on Nicehash is broken

--- MinersLegacy/CryptoDredge.ps1
+++ MinersLegacy/CryptoDredge.ps1
@@ -53,7 +53,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
         $Algorithm_Norm = Get-Algorithm $_
         $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
 
-        if ($Algorithm_Norm -like "CryptoNight*" -and $Pools.$Algorithm_Norm.Protocol -ne "Nicehash") { #temp fix, cryptonight algos are not comparible with Nicehash, https://bitcointalk.org/index.php?topic=4807821.msg45036670#msg45036670
+        if ($Algorithm_Norm -notlike "CryptoNight*" -or ($Algorithm_Norm -like "CryptoNight*" -and $Pools.$Algorithm_Norm.Protocol -ne "Nicehash")) { #temp fix, cryptonight algos are not comparible with Nicehash, https://bitcointalk.org/index.php?topic=4807821.msg45036670#msg45036670
             [PSCustomObject]@{
                 Name           = $Miner_Name
                 DeviceName     = $Miner_Device.Name
