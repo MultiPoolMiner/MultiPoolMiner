c163b8c83f0a0875ccf32fb3521727e6b8640c3b
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-09-03 22:46:44 +0200

    XmRig.api: Use json config files

--- APIs/Xmrig.ps1
+++ APIs/Xmrig.ps1
@@ -1,11 +1,53 @@
 ﻿using module ..\Include.psm1
 
-class Xmrig : Miner {
+class XmRig : Miner {
+    hidden StartMining() {
+        $this.Status = [MinerStatus]::Failed
+
+        $this.New = $true
+        $this.Activated++
+
+        $Parameters = $this.Arguments | ConvertFrom-Json
+        $Arguments = "$($Parameters.Commands)"
+
+        #Write config files. Keep separate files and do not overwrite to preserve optional manual customization
+        if ($Parameters.ConfigFile) {
+            $ConfigFile = "$(Split-Path $this.Path)\$($this.Name)-$($this.Pool)-$($this.Algorithm).txt"
+            ($Parameters.ConfigFile | ConvertTo-Json -Depth 10) | Set-Content $ConfigFile -ErrorAction Ignore
+            $Arguments = "$Arguments --config=$ConfigFile"
+        }
+
+        if ($this.Process) {
+            if ($this.Process | Get-Job -ErrorAction SilentlyContinue) {
+                $this.Process | Remove-Job -Force
+            }
+
+            if (-not ($this.Process | Get-Job -ErrorAction SilentlyContinue)) {
+                $this.Active += $this.Process.PSEndTime - $this.Process.PSBeginTime
+                $this.Process = $null
+            }
+        }
+
+        if (-not $this.Process) {
+            if ($this.ShowMinerWindow -and $this.API -ne "Wrapper") {
+                $this.Process = Start-Job ([ScriptBlock]::Create("Start-Process $(@{desktop = "powershell"; core = "pwsh"}.$Global:PSEdition) `"-command ```$Process = (Start-Process '$($this.Path)' '$($Arguments)' -WorkingDirectory '$(Split-Path $this.Path)' -WindowStyle Minimized -PassThru).Id; Wait-Process -Id `$PID; Stop-Process -Id ```$Process`" -WindowStyle Hidden -Wait"))
+            }
+            else {
+                $this.LogFile = $Global:ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(".\Logs\$($this.Name)-$($this.Port)_$(Get-Date -Format "yyyy-MM-dd_HH-mm-ss").txt")
+                $this.Process = Start-SubProcess -FilePath $this.Path -ArgumentList $Arguments -LogPath $this.LogFile -WorkingDirectory (Split-Path $this.Path) -Priority ($this.DeviceName | ForEach-Object {if ((Get-Device $_).Type -eq "CPU") {-2}else {-1}} | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum)
+            }
+
+            if ($this.Process | Get-Job -ErrorAction SilentlyContinue) {
+                $this.Status = [MinerStatus]::Running
+            }
+        }
+    }
+    
     [String[]]UpdateMinerData () {
         if ($this.GetStatus() -ne [MinerStatus]::Running) {return @()}
 
         $Server = "localhost"
-        $Timeout = 10 #seconds
+        $Timeout = 5 #seconds
 
         $Request = ""
         $Response = ""
@@ -28,7 +70,9 @@ class Xmrig : Miner {
         if (-not $HashRate_Value) {$HashRate_Value = [Double]$Data.hashrate.total[1]} #fix
         if (-not $HashRate_Value) {$HashRate_Value = [Double]$Data.hashrate.total[2]} #fix
 
-        $HashRate | Where-Object {$HashRate_Name} | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+        if ($HashRate_Name -and $HashRate_Value -gt 0) {
+            $HashRate | Add-Member @{$HashRate_Name = [Int64]$HashRate_Value}
+        }
 
         $this.Data += [PSCustomObject]@{
             Date     = (Get-Date).ToUniversalTime()
