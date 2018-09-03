949d47a42d3eda69a563cbda794087e14422e343
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-09-03 22:48:23 +0200

    XmRigCpu.ps1: Use json config files

--- MinersLegacy/XmRigCpu.ps1
+++ MinersLegacy/XmRigCpu.ps1
@@ -7,29 +7,27 @@ param(
     [PSCustomObject[]]$Devices
 )
 
-$Path = ".\Bin\CPU-XmRig-Cryptonight\xmrig.exe"
-$HashSHA256 = "EA2E92BB10D0482880F8D389B7915948E11F672CA8559B0901D8A8FA8E9D733E"
+$Path = ".\Bin\CPU-XmRig-CryptoNight\xmrig.exe"
+$HashSHA256 = "73C345AB59538A0B4332BA0FB3659F111AAC4291F169D24DA6D5186FEC0B2712"
 $Uri = "https://github.com/xmrig/xmrig/releases/download/v2.6.4/xmrig-2.6.4-msvc-win64.zip"
-$ManualUri = "https://github.com/xmrig/xmrig-cpu"
-$Port = "339{0:d1}"
+$ManualUri = "https://github.com/xmrig/xmrig"
+$Port = "40{0:d2}"
 
-$Commands = [PSCustomObject]@{
-    "cryptonight/1"          = "" #CryptoNightV7; Also known as monero7 and CryptoNightV7
-    "cryptonight/xtl"        = "" #CryptoNightXtl; Stellite (XTL)
-    "cryptonight/msr"        = "" #CryptoNightFast; also known as cryptonight-masari (MSR)
-    "cryptonight/xao"        = "" #CryptoNightAlloy (XAO)
-    "cryptonight/rto"        = "" #CryptoNightArto (RTO)
-    "cryptonight-lite"       = "" #CryptoNightLite; Autodetect works only for Aeon
-    "cryptonight-lite/0"     = "" #CryptoNightLite; Original/old CryptoNight-Lite
-    "cryptonight-lite/1"     = "" #CryptoNightLiteV7; also known as aeon7
-    "cryptonight-heavy"      = "" #CryptoNightHeavy; Ryo and Loki
-    "cryptonight-heavy/xhv"  = "" #CryptoNightHaven Protocol
-    "cryptonight-heavy/tube" = "" #CryptoNightHeavyTube (TUBE)
+$Commands = [PSCustomObject[]]@(
+    #                             Miner algo name            Params         Algorithm_Norm (from Get-Algorithm)
+    [PSCustomObject]@{Algorithm = "cryptonight/0";           Params = ""} # CryptoNight    
+    [PSCustomObject]@{Algorithm = "cryptonight/1";           Params = ""} # CryptoNightV7
+    [PSCustomObject]@{Algorithm = "cryptonight/msr";         Params = ""} # CryptoNightMsr
+    [PSCustomObject]@{Algorithm = "cryptonight/rto";         Params = ""} # CryptoNightRto
+    [PSCustomObject]@{Algorithm = "cryptonight/xao";         Params = ""} # CryptoNightXao
+    [PSCustomObject]@{Algorithm = "cryptonight/xtl";         Params = ""} # CryptoNightXtl
+    [PSCustomObject]@{Algorithm = "cryptonight-lite/0";      Params = ""} # CryptoNightLite
+    [PSCustomObject]@{Algorithm = "cryptonight-lite/1";      Params = ""} # CryptoNightLiteV7
+    [PSCustomObject]@{Algorithm = "cryptonight-heavy";       Params = ""} # CryptoNightHeavy
+    [PSCustomObject]@{Algorithm = "cryptonight-heavy/tube";  Params = ""} # CryptoNightHeavyTube
+    [PSCustomObject]@{Algorithm = "cryptonight-heavy/xhv";   Params = ""} # CryptoNightHeavyHaven
+)
 
-    # ASIC only (09/07/2018)
-    #"cryptonight"            = ""} #Autodetect works only for Monero
-    #"cryptonight"            = ""} #Original/old CryptoNight
-}
 $CommonCommands = ""
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
@@ -39,22 +37,69 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
     $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model)
     $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)    
     $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+    $Miner_Name = (@($Name) + @("$($Miner_Device.count)x$($Miner_Device.Model_Norm | Sort-Object -unique)") | Select-Object) -join '-'
 
-    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    $Commands | ForEach-Object {
 
-        $Algorithm_Norm = Get-Algorithm $_
+        $Algorithm = $_.Algorithm
+        $Algorithm_Norm = Get-Algorithm $Algorithm
+        $Params = $_.Params
 
-        [PSCustomObject]@{
-            Name       = $Miner_Name
-            DeviceName = $Miner_Device.Name
-            Path       = $Path
-            HashSHA256 = $HashSHA256
-            Arguments  = "--api-port $($Miner_Port) -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) --keepalive --nicehash --rig-id=$($Worker) --donate-level 1$($Commands.$_)$CommonCommands"
-            HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
-            API        = "XmRig"
-            Port       = $Miner_Port
-            URI        = $Uri
-            Fees       = [PSCustomObject]@{"$Algorithm_Norm" = 1 / 100}
+        if ($Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" -and $Miner_Device) {
+            $Arguments = [PSCustomObject]@{
+                ConfigFile = [PSCustomObject]@{
+                    "algo"         = $Algorithm
+                    "api" = [PSCustomObject]@{
+                        "port"         = $Miner_Port
+                        "access-token" = $null
+                        "worker-id"    = $null
+                    }
+                    "background"   = $false
+                    "cuda-bfactor" = 10
+                    "colors"       = $true
+                    "donate-level" = 1
+                    "log-file"     = $null
+                    "print-time"   = 5
+                    "retries"      = 5
+                    "retry-pause"  = 5
+                    "threads"      = @()
+                    "pools"        = @([PSCustomObject]@{
+                        "keepalive" = $true
+                        "nicehash"  = $(if ($Pools.$Algorithm_Norm.Name -eq "Nicehash") {$true} else {$false})
+                        "pass"      = "$($Pools.$Algorithm_Norm.Pass)"
+                        "url"       = "$($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port)"
+                        "user"      = "$($Pools.$Algorithm_Norm.User)"
+                        "rig-id"    = "$WorkerName"
+                    })
+                }
+                Commands = "$Params$CommonCommands"
+            }
+
+            [PSCustomObject]@{
+                Name       = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                Path       = $Path
+                HashSHA256 = $HashSHA256
+                Arguments  = $Arguments
+                HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API        = "XmRig"
+                Port       = $Miner_Port
+                URI        = $Uri
+                Fees       = [PSCustomObject]@{"$Algorithm_Norm" = 1 / 100}
+            }
         }
+#
+#        [PSCustomObject]@{
+#            Name       = $Miner_Name
+#            DeviceName = $Miner_Device.Name
+#            Path       = $Path
+#            HashSHA256 = $HashSHA256
+#            Arguments  = "--api-port $($Miner_Port) -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) --keepalive --nicehash --rig-id=$($Worker) --donate-level 1$($Commands.$_)$CommonCommands"
+#            HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+#            API        = "XmRig"
+#            Port       = $Miner_Port
+#            URI        = $Uri
+#            Fees       = [PSCustomObject]@{"$Algorithm_Norm" = 1 / 100}
+#        }
     }
 }
