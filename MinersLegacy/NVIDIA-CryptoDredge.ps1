05b95a9225a2151ce8dc5afb96525d9e9a43ec0d
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-09-07 11:22:06 +0200

    Update NVIDIA-CryptoDredge.ps1

--- MinersLegacy/NVIDIA-CryptoDredge.ps1
+++ MinersLegacy/NVIDIA-CryptoDredge.ps1
@@ -54,7 +54,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
         $Algorithm_Norm = Get-Algorithm $_
         $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
 
-        if (-not ($Algorithm_Norm -like "CryptoNight*" -and $Pools.$Algorithm_Norm.Name -eq "NiceHash")) { #temp fix, cryptonight algos are not compatible with NiceHash, https://bitcointalk.org/index.php?topic=4807821.msg45036670#msg45036670
+        if (-not ($Algorithm_Norm -like "Cryptonight*" -and $Pools.$Algorithm_Norm.Name -eq "NiceHash")) { #temp fix, cryptonight algos are not compatible with NiceHash, https://bitcointalk.org/index.php?topic=4807821.msg45036670#msg45036670
             [PSCustomObject]@{
                 Name           = $Miner_Name
                 DeviceName     = $Miner_Device.Name
