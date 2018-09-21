3757a5f68a517474439ffca4f3e808319eea4b32
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-09-21 08:14:34 +0100

    NVIDIA_CcminerTrex: Fix extra commands per algorithm
    Fixes https://github.com/MultiPoolMiner/MultiPoolMiner/issues/2179

--- MinersLegacy/Nvidia-CcminerTrex.ps1
+++ MinersLegacy/Nvidia-CcminerTrex.ps1
@@ -56,7 +56,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
             DeviceName     = $Miner_Device.Name
             Path           = $Path
             HashSHA256     = $HashSHA256
-            Arguments      = ("-b 127.0.0.1:$($Miner_Port) -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($_.Params)$CommonCommands -d $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ',')" -replace "\s+", " ").trim()
+            Arguments      = ("-b 127.0.0.1:$($Miner_Port) -a $_ -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) $($Commands.$_)$CommonCommands -d $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ',')" -replace "\s+", " ").trim()
             HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
             API            = "Ccminer"
             Port           = $Miner_Port
