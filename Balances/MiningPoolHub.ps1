c3bf8677e6c3d7e234c81bdd26cde3aeb54781ba
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-11-14 11:31:09 +0100

    Fix API Key

--- Balances/MiningPoolHub.ps1
+++ Balances/MiningPoolHub.ps1
@@ -40,7 +40,7 @@ $APIRequest.getuserallbalances.data | Foreach-Object {
     #Define currency
     $Currency = $_.coin
     try {
-        $Currency = Invoke-RestMethod "http://$($_.coin).miningpoolhub.com/index.php?page=api&action=getpoolinfo&api_key=28328accdd4306c631a881bd8130f0d258a94b0e33569e0eef7e83773d018c98" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop | Select-Object -ExpandProperty getpoolinfo | Select-Object -ExpandProperty data | Select-Object -ExpandProperty currency 
+        $Currency = Invoke-RestMethod "http://$($_.coin).miningpoolhub.com/index.php?page=api&action=getpoolinfo&api_key=$($PoolConfig.API_Key)" -UseBasicParsing -TimeoutSec 10 -ErrorAction Stop | Select-Object -ExpandProperty getpoolinfo | Select-Object -ExpandProperty data | Select-Object -ExpandProperty currency 
     }
     catch {
         Write-Log -Level Warn "Cannot determine currency for coin ($CoinName) - cannot convert some balances to BTC or other currencies. "
