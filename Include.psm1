e0cead3e82832df2f9cd3028ab32783c9b270850
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-11-28 11:13:56 +0100

    Include.psm1: Fixed pool balances display formatting
    works with any regional setting

--- Include.psm1
+++ Include.psm1
@@ -80,34 +80,49 @@ function Get-Balance {
         #Add total of totals
         $Totals = [PSCustomObject]@{Name = "*Total*"}
 
-        $NumberDecimalSeparator = (Get-Culture).NumberFormat.NumberDecimalSeparator
-        $NumberGroupSeparator = (Get-Culture).NumberFormat.NumberGroupSeparator
-
-        #Add Balance (in currency)
-        $Rates.PSObject.Properties.Name | ForEach-Object {
-            $Currency = $_.ToUpper()
-            $Balances | Foreach-Object {
-                if ($NewRates.$Currency -ne $null) {$Digits = ($NewRates.$Currency).length - ([Int]($NewRates.$Currency)).length - 1} else {$Digits = 8}
-                #if ($Digits -lt 0 -or $Digits -gt 8) {$Digits = 8}
-                $_.Total = ("{0:N$($Digits)}" -f ([Float]$($_.Total)))
-                if ($Currency -eq $_.Currency) {
-                    $_ | Add-Member "Balance ($Currency)" $_.Total
-                }
-            }
-            if (($Balances."Balance ($Currency)" | Measure-Object -Sum).sum) {$Totals | Add-Member "Balance ($Currency)" ("{0:N$($Digits)}" -f ([Float]$($Balances."Balance ($Currency)" | Measure-Object -Sum).sum))}
-        }
-
         #Add converted values
         $Config.Currency | ForEach-Object {
             $Currency = $_.ToUpper()
             #Get number of digits from $NewRates
             if ($NewRates.$Currency -ne $null) {$Digits = ($NewRates.$Currency).length - ([Int]($NewRates.$Currency)).length - 1} else {$Digits = 8}
-            #if ($Digits -lt 0 -or $Digits -gt 8) {$Digits = 8}
             $Balances | Foreach-Object {
-                $_ | Add-Member "Value in $Currency" $(if ($Rates.$($_.Currency).$Currency) {("{0:N$($Digits)}" -f ([Float]$_.Total * [Float]$Rates.$($_.Currency).$Currency))}else {"unknown"}) -Force
+                if ($Rates.$($_.Currency).$Currency) {
+                    # Add separate element with numeric value. Measure-Object can not sum strings (-f)
+                    $_ | Add-Member "_Value in $Currency" ($_.Total * $Rates.$($_.Currency).$Currency) -Force
+                    #Format to string
+                    $_ | Add-Member "Value in $Currency" ("{0:N$($Digits)}" -f ($_.Total * $Rates.$($_.Currency).$Currency)) -Force
+                }
+                else {
+                    $_ | Add-Member "Value in $Currency" {"unknown"} -Force
+                }
             }
-            if (($Balances."Value in $Currency" | Measure-Object -Sum -ErrorAction Ignore).sum)  {$Totals | Add-Member "Value in $Currency" ("{0:N$($Digits)}" -f ([Float]$($Balances."Value in $Currency" | Measure-Object -Sum -ErrorAction Ignore).sum)) -Force}
+            if (($Balances."_Value in $Currency" | Measure-Object -Sum -ErrorAction Ignore).sum)  {$Totals | Add-Member "Value in $Currency" ("{0:N$($Digits)}" -f ($Balances."_Value in $Currency" | Measure-Object -Sum -ErrorAction Ignore).sum) -Force}
         }
+
+        #Add Balance (in currency)
+        $Rates.PSObject.Properties.Name | ForEach-Object {
+            $Currency = $_.ToUpper()
+            if ($NewRates.$Currency -ne $null) {$Digits = ($NewRates.$Currency).length - ([Int]($NewRates.$Currency)).length - 1} else {$Digits = 8}
+            $Balances | Foreach-Object {
+                if ($Currency -eq $_.Currency) {
+                    # Add separate element with numeric value. Measure-Object can not sum strings (-f)
+                    $_ | Add-Member "_Balance ($Currency)" $_.Total
+                    #Format to string
+                    $_ | Add-Member "Balance ($Currency)" ("{0:N$($Digits)}" -f $_.Total)
+                }
+            }
+            if (($Balances."_Balance ($Currency)" | Measure-Object -Sum).sum) {$Totals | Add-Member "Balance ($Currency)" ("{0:N$($Digits)}" -f ($Balances."_Balance ($Currency)" | Measure-Object -Sum).sum)}
+            
+        }
+
+        $Balances | Foreach-Object {
+            $Balance = $_
+            #Format to string, cannot be done before calculations are done. Measure-Object can not sum strings (-f)
+            $_.Total = ("{0:N$($Digits)}" -f $_.Total)
+            #Cleanup, remove elements required for calculations
+            $Balance.PSObject.Properties.Name | Where-Object {$_ -like "_*"} | ForEach-Object {$Balance.PSObject.Properties.Remove($_)}
+        }
+
         $Balances += $Totals
         $BalancesData | Add-Member Balances $Balances -force
         $BalancesData | Add-Member Rates $Rates
