55c95cf8c1076eedcbe9530d912a4bc3c7f5eea0
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-12-07 13:40:49 +0100

    Update AMD-WildRig_v0.13.6.2beta.ps1

--- MinersLegacy/AMD-WildRig_v0.13.6.2beta.ps1
+++ MinersLegacy/AMD-WildRig_v0.13.6.2beta.ps1
@@ -117,14 +117,14 @@ $CommonCommands = " --donate-level 1"
 $Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "Advanced Micro Devices, Inc.")
 
 $Devices | Select-Object Model -Unique | ForEach-Object {
-    $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model)
+    $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model | Where-Object {$_.OpenCL.Name -match "^Baffin.*|^Ellesmere.*|^Fiji.*|^gfx900.*|^Tonga.*"})
     $Miner_Port = $Port -f ($Miner_Device | Select-Object -First 1 -ExpandProperty Index)    
 
     if ($Config.CreateMinerInstancePerDeviceModel) {
         Switch (($Miner_Device | Select-Object -First 1 -ExpandProperty OpenCl).Name) {
-            "Baffin" {$Commands = $CommandsBaffin}
             "Ellesmere" {$Commands = Get-Variable $("CommandsEllesmere$(($Miner_Device | Select-Object -First 1 -ExpandProperty OpenCl).MaxComputeUnits)CU") -ValueOnly -ErrorAction SilentlyContinue}
             "gfx900" {$Commands = $CommandsGfx900}
+            default  {$Commands = $CommandsBaffin}
         }
     }
     else {
@@ -134,33 +134,35 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
     $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | Where-Object {$Pools.(Get-Algorithm $_).Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
         $Algorithm_Norm = Get-Algorithm $_
 
-        if ($Config.UseDeviceNameForStatsFileNaming) {
-            $Miner_Name = (@($Name) + @(($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object {$Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm"}) -join '_') | Select-Object) -join '-'
-        }
-        else {
-            $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
-        }
+        if ($Pools.$Algorithm_Norm.Host -and $Miner_Device) {
+            if ($Config.UseDeviceNameForStatsFileNaming) {
+                $Miner_Name = (@($Name) + @(($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object {$Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm"}) -join '_') | Select-Object) -join '-'
+            }
+            else {
+                $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+            }
 
-        #Get commands for active miner devices
-        $Commands.$_ = Get-CommandPerDevice $Commands.$_ $Miner_Device.Type_Vendor_Index
+            #Get commands for active miner devices
+            $Commands.$_ = Get-CommandPerDevice $Commands.$_ $Miner_Device.Type_Vendor_Index
 
-        Switch ($Algorithm_Norm) {
-            "X16R"  {$BenchmarkIntervals = 5}
-            default {$BenchmarkIntervals = 1}
-        }
+            Switch ($Algorithm_Norm) {
+                "X16R"  {$BenchmarkIntervals = 5}
+                default {$BenchmarkIntervals = 1}
+            }
 
-        [PSCustomObject]@{
-            Name               = $Miner_Name
-            DeviceName         = $Miner_Device.Name
-            Path               = $Path
-            HashSHA256         = $HashSHA256
-            Arguments          = ("--algo=$_ --api-port=$Miner_Port --url=$($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) --user=$($Pools.$Algorithm_Norm.User) --pass=$($Pools.$Algorithm_Norm.Pass)$(if($Config.CreateMinerInstancePerDeviceModel -and @($Devices | Select-Object Model_Norm -Unique).count -gt 1){" --multiple-instance"})$($Commands.$_)$CommonCommands --opencl-platform=$($Miner_Device.PlatformId | Sort-Object -Unique) --opencl-devices=$(($Miner_Device | ForEach-Object {'{0:x}' -f $_.Type_Vendor_Index}) -join ',')" -replace "\s+", " ").trim()
-            HashRates          = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
-            API                = "XmRig"
-            Port               = $Miner_Port
-            URI                = $Uri
-            Fees               = [PSCustomObject]@{$Algorithm_Norm = 1 / 100}
-            BenchmarkIntervals = $BenchmarkIntervals
+            [PSCustomObject]@{
+                Name               = $Miner_Name
+                DeviceName         = $Miner_Device.Name
+                Path               = $Path
+                HashSHA256         = $HashSHA256
+                Arguments          = ("--algo=$_ --api-port=$Miner_Port --url=$($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) --user=$($Pools.$Algorithm_Norm.User) --pass=$($Pools.$Algorithm_Norm.Pass)$(if($Config.CreateMinerInstancePerDeviceModel -and @($Devices | Select-Object Model_Norm -Unique).count -gt 1){" --multiple-instance"})$($Commands.$_)$CommonCommands --opencl-platform=$($Miner_Device.PlatformId | Sort-Object -Unique) --opencl-devices=$(($Miner_Device | ForEach-Object {'{0:x}' -f $_.Type_Vendor_Index}) -join ',')" -replace "\s+", " ").trim()
+                HashRates          = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+                API                = "XmRig"
+                Port               = $Miner_Port
+                URI                = $Uri
+                Fees               = [PSCustomObject]@{$Algorithm_Norm = 1 / 100}
+                BenchmarkIntervals = $BenchmarkIntervals
+            }
         }
     }
 }
