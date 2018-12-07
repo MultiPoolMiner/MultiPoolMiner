fa574debe52be33f136445f62f3edb8ba7b134b0
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-12-07 09:35:53 +0100

    Update default algo list in start batch files

--- Start-Zpool.bat
+++ Start-Zpool.bat
@@ -7,7 +7,7 @@
 @if not "%GPU_SINGLE_ALLOC_PERCENT%"=="100" (setx GPU_SINGLE_ALLOC_PERCENT 100) > nul
 @if not "%CUDA_DEVICE_ORDER%"=="PCI_BUS_ID" (setx CUDA_DEVICE_ORDER PCI_BUS_ID) > nul
 
-@set "command=& .\multipoolminer.ps1 -Wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -UserName aaronsace -WorkerName multipoolminer -Region europe -Currency btc,usd,eur -DeviceName amd,nvidia,cpu -PoolName zpool -algorithm blake2s,equihash,keccak,lbry,lyra2re2,lyra2z,m7m,neoscrypt,sib,skein,skunk -Donate 24 -Watchdog -MinerStatusURL https://multipoolminer.io/monitor/miner.php -SwitchingPrevention 2 -UseFastestMinerPerAlgoOnly"
+@set "command=& .\multipoolminer.ps1 -Wallet 1Q24z7gHPDbedkaWDTFqhMF8g7iHMehsCb -UserName aaronsace -WorkerName multipoolminer -Region europe -Currency btc,usd,eur -DeviceName amd,nvidia,cpu -PoolName zpool -algorithm blake2s,equihash,hex,keccak,lbry,lyra2z,m7m,neoscrypt,sib,skein,skunk,x16r,x16s,x17,x22i -Donate 24 -Watchdog -MinerStatusURL https://multipoolminer.io/monitor/miner.php -SwitchingPrevention 2 -UseFastestMinerPerAlgoOnly"
 
 start /min pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log '.*\\MultiPoolMiner_\d\d\d\d-\d\d-\d\d.*\.txt$' -sort '^[^_]*_' -QuickStart"
 
