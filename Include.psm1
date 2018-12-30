8c50b21765ea5baa9af1b59ff5add19efd6f482a
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2018-12-30 10:37:54 +0100

    Added function EquihashPers
    Requires EquihashPers.txt

--- Include.psm1
+++ Include.psm1
@@ -779,6 +779,27 @@ function Get-Region {
     else {$Region}
 }
 
+function Get-EquihashPers {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [String]$CoinName = "",
+        [Parameter(Mandatory = $false)]
+        [AllowEmptyString()]
+        [String]$Default = ""
+    )
+
+    if (-not (Test-Path Variable:Script:EquihashPers)) {
+        $Script:EquihashPers = Get-Content "EquihashPers.txt" | ConvertFrom-Json
+    }
+
+    $CoinName = (Get-Culture).TextInfo.ToTitleCase(($CoinName -replace "-", " " -replace "_", " ")) -replace " "
+    
+    if ($Script:EquihashPers.$CoinName) {$Script:EquihashPers.$CoinName}
+    else {$Default}
+}
+
 enum MinerStatus {
     Running
     Idle
