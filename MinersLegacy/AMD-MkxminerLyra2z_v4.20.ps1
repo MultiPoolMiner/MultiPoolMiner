0dc0835c9c70b31706bbbfe069754f4fab0e9101
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-01-06 13:38:02 +0100

    Create AMD-MkxminerLyra2z_v4.20.ps1

---
+++ MinersLegacy/AMD-MkxminerLyra2z_v4.20.ps1
@@ -0,0 +1,52 @@
+﻿using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject[]]$Devices
+)
+
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Path = ".\Bin\$($Name)\mkxminer.exe"
+$HashSHA256 = "ECFDA1A731157606D2C1AFD2622562F4678F422442E08B965958EFB0F1184389"
+$Uri = "https://github.com/MultiPoolMiner/miner-binaries/releases/download/mkxminer/mkxminer420.zip"
+$ManualURI = "https://bitcointalk.org/index.php?topic=5058058"
+
+$Commands = [PSCustomObject]@{
+    "Lyra2z" = "" #Lyra2z
+}
+$CommonCommands = " --intensity 31"
+
+$Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "Advanced Micro Devices, Inc." | Where-Object {$_.OpenCL.GlobalMemsize -ge 2GB} | Where-Object {$_.OpenCL.Name -match "^Baffin.*|^Ellesmere.*|^Polaris.*"})
+
+$Devices | Select-Object Model -Unique | ForEach-Object {
+    $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model)
+    $Miner_Port = $Config.APIPort + ($Miner_Device | Select-Object -First 1 -ExpandProperty Index) + 3   
+
+    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+        $Algorithm_Norm = Get-Algorithm $_
+
+        if ($Config.UseDeviceNameForStatsFileNaming) {
+            $Miner_Name = (@($Name) + @(($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object {$Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm"}) -join '_') | Select-Object) -join '-'
+        }
+        else {
+            $Miner_Name = (@($Name) + @($Miner_Device.Name | Sort-Object) | Select-Object) -join '-'
+        }
+
+        #Get commands for active miner devices
+        $Commands.$_ = Get-CommandPerDevice $Commands.$_ $Miner_Device.Type_Vendor_Index
+
+        [PSCustomObject]@{
+            Name           = $Miner_Name
+            DeviceName     = $Miner_Device.Name
+            Path           = $Path
+            HashSHA256     = $HashSHA256
+            Arguments      = ("--algorithm $_ --exitsick --api $Miner_Port -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass)$($Commands.$_)$CommonCommands -d $(($Miner_Device | ForEach-Object {'{0:x}' -f $_.Type_Vendor_Index}) -join ',')" -replace "\s+", " ").trim()
+            HashRates      = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+            API            = "Mkxminer"
+            Port           = $Miner_Port
+            URI            = $Uri
+        }
+    }
+}
