c3a31c2ff4b151b1000f5070d9bef6b231d35f6c
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-02-06 22:38:03 +0100

    Update YiiMP.ps1

--- Pools/YiiMP.ps1
+++ Pools/YiiMP.ps1
@@ -41,46 +41,44 @@ if (($APICurrenciesRequest | Get-Member -MemberType NoteProperty -ErrorAction Ig
 }
 
 #Pool allows payout in any currency available in API
-if ($APICurrenciesRequest | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Sort-Object | Select-Object -Unique | Where-Object {$Wallets.$_}) {
-    $APICurrenciesRequest | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Sort-Object | Select-Object -Unique | Where-Object {$APICurrenciesRequest.$_.hashrate -GT 0 -and $Wallets.$_} | Foreach-Object {
+$APICurrenciesRequest | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name | Sort-Object | Select-Object -Unique | Where-Object {$APICurrenciesRequest.$_.hashrate -GT 0 -and $Wallets.$_} | Foreach-Object {
 
-        $APICurrenciesRequest.$_ | Add-Member Symbol $_ -ErrorAction SilentlyContinue
+    $APICurrenciesRequest.$_ | Add-Member Symbol $_ -ErrorAction SilentlyContinue
 
-        $PoolHost       = "yiimp.eu"
-        $Port           = $APICurrenciesRequest.$_.port
-        $Algorithm      = $APICurrenciesRequest.$_.algo
-        $Algorithm_Norm = Get-Algorithm $Algorithm
-        $CoinName       = $APICurrenciesRequest.$_.name
-        $MiningCurrency = $APICurrenciesRequest.$_.symbol
-        $Workers        = $APICurrenciesRequest.$_.workers
-        $Fee            = $APIStatusRequest.$Algorithm.Fees / 100
+    $PoolHost       = "yiimp.eu"
+    $Port           = $APICurrenciesRequest.$_.port
+    $Algorithm      = $APICurrenciesRequest.$_.algo
+    $Algorithm_Norm = Get-Algorithm $Algorithm
+    $CoinName       = $APICurrenciesRequest.$_.name
+    $MiningCurrency = $APICurrenciesRequest.$_.symbol
+    $Workers        = $APICurrenciesRequest.$_.workers
+    $Fee            = $APIStatusRequest.$Algorithm.Fees / 100
 
-        $Divisor = 1000000 * [Double]$APIStatusRequest.$Algorithm.mbtc_mh_factor
+    $Divisor = 1000000 * [Double]$APIStatusRequest.$Algorithm.mbtc_mh_factor
 
-        $Stat = Set-Stat -Name "$($Name)_$($Algorithm_Norm)_Profit" -Value ([Double]$APICurrenciesRequest.$_.estimate / $Divisor) -Duration $StatSpan -ChangeDetection $true
+    $Stat = Set-Stat -Name "$($Name)_$($Algorithm_Norm)_Profit" -Value ([Double]$APICurrenciesRequest.$_.estimate / $Divisor) -Duration $StatSpan -ChangeDetection $true
 
-        $PoolRegions | ForEach-Object {
-            $Region = $_
-            $Region_Norm = Get-Region $Region
+    $PoolRegions | ForEach-Object {
+        $Region = $_
+        $Region_Norm = Get-Region $Region
 
-            [PSCustomObject]@{
-                Algorithm      = $Algorithm_Norm
-                CoinName       = $CoinName
-                Price          = $Stat.Live
-                StablePrice    = $Stat.Week
-                MarginOfError  = $Stat.Week_Fluctuation
-                Protocol       = "stratum+tcp"
-                Host           = $PoolHost
-                Port           = $Port
-                User           = $Wallets.$MiningCurrency
-                Pass           = "$Worker,c=$MiningCurrency"
-                Region         = $Region_Norm
-                SSL            = $false
-                Updated        = $Stat.Updated
-                Fee            = $Fee
-                Workers        = $Workers
-                MiningCurrency = $MiningCurrency
-            }
+        [PSCustomObject]@{
+            Algorithm      = $Algorithm_Norm
+            CoinName       = $CoinName
+            Price          = $Stat.Live
+            StablePrice    = $Stat.Week
+            MarginOfError  = $Stat.Week_Fluctuation
+            Protocol       = "stratum+tcp"
+            Host           = $PoolHost
+            Port           = $Port
+            User           = $Wallets.$MiningCurrency
+            Pass           = "$Worker,c=$MiningCurrency"
+            Region         = $Region_Norm
+            SSL            = $false
+            Updated        = $Stat.Updated
+            Fee            = $Fee
+            Workers        = $Workers
+            MiningCurrency = $MiningCurrency
         }
     }
 }
