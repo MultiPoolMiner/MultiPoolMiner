516d1f3b6ef8442f0c02521346e78eaeb2c8a5fc
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-02-06 22:02:43 +0100

    Update ResetPowerUsage.bat

--- ResetPowerUsage.bat
+++ ResetPowerUsage.bat
@@ -3,6 +3,6 @@ cd /d %~dp0
 set /p powerusagereset= This process will remove all power usage data. Are you sure you want to continue? [Y/N] 
 IF /I "%powerusagereset= %"=="Y" (
 	if exist "Stats\*_HashRate.txt" del "Stats\*_PowerUsage.txt"
-	ECHO Success. You need to measure the power consmuption for all required miners & algorithms to continue using MultiPoolMiner.
+	ECHO Success. You need to measure the power consumption for all required miners & algorithms to continue using MultiPoolMiner.
 	PAUSE
 )
