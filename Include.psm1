8763c51413e03fca4c1574affbb691af020f717c
Aaron Simmonds <aaronsace@hotmail.com>
2019-02-25 23:37:31 +0000

    Revert "Fix StatSpan"
    This reverts commit 9a9e135aa8d19a4276f52f99c7cf9f9f964ae2e6.

--- Include.psm1
+++ Include.psm1
@@ -1023,7 +1023,7 @@ class Miner {
     hidden [TimeSpan]$Active = [TimeSpan]::Zero
     hidden [Int]$Activated = 0
     hidden [MinerStatus]$Status = [MinerStatus]::Idle
-    [TimeSpan[]]$Intervals
+    $IntervalCount
     $LogFile
     $Pool
     hidden [Array]$Data = @()
