8f014e027e10cb774031af08d1b560d6828b71c9
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-06-29 19:48:17 +0200

    Update web deshboard

--- web/scripts/resetzeropowerusageminers.ps1
+++ web/scripts/resetzeropowerusageminers.ps1
@@ -3,7 +3,7 @@ param($Parameters)
 $text = ""
 $count = 0
 
-Get-ChildItem Stats -Recurse | Where-Object {$_.Name -like '*HashRate.txt'} | Foreach-Object {
+Get-ChildItem Stats -Recurse | Where-Object {$_.Name -like '*PowerUsage.txt'} | Foreach-Object {
   $FileName = $_.FullName
   $Stats = Get-Content $Filename | ConvertFrom-Json
   
@@ -14,7 +14,7 @@ Get-ChildItem Stats -Recurse | Where-Object {$_.Name -like '*HashRate.txt'} | Fo
   }
 }  
 
-Write-Output "Removed $count stat files:"
+Write-Output "Removed $count power usage files:"
 Write-Output "<pre>"
 $text | Write-Output
 Write-Output "</pre>"
