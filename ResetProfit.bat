783a1e234e183df94717fa347e1b654da27d9e47
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-06-29 19:53:27 +0200

    Update ResetProfit.bat

--- ResetProfit.bat
+++ ResetProfit.bat
@@ -3,6 +3,7 @@ cd /d %~dp0
 set /p statreset= This process will remove all accumulated coin data and reset your profit statistics. Are you sure you want to continue? [Y/N] 
 IF /I "%statreset%"=="Y" (
 	if exist "Stats\*Profit.txt" del "Stats\*Profit.txt"
+	if exist "Stats\Profit\*Profit.txt" del "Stats\Profit\*Profit.txt"
 	ECHO Your stats have been successfully reset.
 	PAUSE
 )
