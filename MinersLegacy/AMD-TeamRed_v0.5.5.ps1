35a138fcd63e219b29bfa4141f361f998e00b03a
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-07-08 20:10:48 +0200

    Update AMD-TeamRed_v0.5.5.ps1

--- MinersLegacy/AMD-TeamRed_v0.5.5.ps1
+++ MinersLegacy/AMD-TeamRed_v0.5.5.ps1
@@ -79,7 +79,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
                 Path       = $Path
                 HashSHA256 = $HashSHA256
                 Arguments  = ("--algo=$($Algorithm) --api_listen=127.0.0.1:$Miner_Port --url=$($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) --user=$($Pools.$Algorithm_Norm.User) --pass=$($Pools.$Algorithm_Norm.Pass)$Parameters$CommonParameters --platform=$($Miner_Device.PlatformId | Sort-Object -Unique) --devices=$(($Miner_Device | ForEach-Object {'{0:x}' -f $_.Type_Vendor_Index}) -join ',')" -replace "\s+", " ").trim()
-                HashRates  = [PSCustomObject]@{$Algorithm_Norm = $($Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week)}
+                HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
                 API        = "Xgminer"
                 Port       = $Miner_Port
                 URI        = $Uri
