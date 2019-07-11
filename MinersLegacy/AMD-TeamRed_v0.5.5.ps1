a8fa474b1f97403ae5790f53a3a8fd6698505dea
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-07-11 08:11:48 +0200

    Update AMD-TeamRed_v0.5.5.ps1
    enabled detection of failed GPUs

--- MinersLegacy/AMD-TeamRed_v0.5.5.ps1
+++ MinersLegacy/AMD-TeamRed_v0.5.5.ps1
@@ -45,7 +45,7 @@ else {
 
 #CommonCommands from config file take precedence
 if ($Miner_Config.CommonParameters) {$CommonParameters = $Miner_Config.CommonParameters = $Miner_Config.CommonParameters}
-else {$CommonParameters = ""}
+else {$CommonParameters = " --watchdog_script"}
 
 $Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "Advanced Micro Devices, Inc.")
 $Devices | Select-Object Model -Unique | ForEach-Object {
