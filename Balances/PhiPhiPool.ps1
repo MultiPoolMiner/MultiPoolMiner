aa4a6a6a1515c408822eaa31c59d31e9844779d1
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-07-27 14:51:05 +0200

    Update balances

--- Balances/PhiPhiPool.ps1
+++ Balances/PhiPhiPool.ps1
@@ -13,9 +13,9 @@ if (-not ($Wallets | Get-Member -MemberType NoteProperty -ErrorAction Ignore | S
 
 $RetryCount = 3
 $RetryDelay = 2
-while (-not ($APIRequest) -and $RetryCount -gt 0) {
+while (-not ($APIResponse) -and $RetryCount -gt 0) {
     try {
-        if (-not $APIRequest) {$APIRequest = Invoke-RestMethod "http://phi-phi-pool.com/api/currencies" -UseBasicParsing -TimeoutSec 3 -ErrorAction Stop}
+        if (-not $APIResponse) {$APIResponse = Invoke-RestMethod "http://phi-phi-pool.com/api/currencies" -UseBasicParsing -TimeoutSec 3 -ErrorAction Stop}
     }
     catch {
         Start-Sleep -Seconds $RetryDelay # Pool might not like immediate requests
@@ -23,31 +23,31 @@ while (-not ($APIRequest) -and $RetryCount -gt 0) {
     $RetryCount--
 }
 
-if (-not $APIRequest) {
+if (-not $APIResponse) {
     Write-Log -Level Warn "Pool Balance API ($Name) has failed. "
     return
 }
 
-if (($APIRequest | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
+if (($APIResponse | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
     Write-Log -Level Warn "Pool Balance API ($Name) returned nothing. "
     return
 }
 
 #Pool does not do auto conversion to BTC
-$Payout_Currencies = @($APIRequest | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name) | Where-Object {$Wallets.$_} | Sort-Object -Unique
+$Payout_Currencies = @($APIResponse | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Select-Object -ExpandProperty Name) | Where-Object {$Wallets.$_} | Sort-Object -Unique
 if ($Payout_Currencies) {
     Write-Log -Level Verbose "Cannot get balance on pool ($Name) - no wallet address specified. "
     return
 }
 
-$Payout_Currencies | Foreach-Object {
+$Payout_Currencies | ForEach-Object {
     $Payout_Currency = $_
-    $APIRequest = ""        
+    $APIResponse = ""        
     $RetryCount = 3
     $RetryDelay = 2
-    while (-not ($APIRequest) -and $RetryCount -gt 0) {
+    while (-not ($APIResponse) -and $RetryCount -gt 0) {
         try {
-            $APIRequest = Invoke-RestMethod "http://api.yiimp.eu/api/wallet?address=$($Wallets.$Payout_Currency)" -UseBasicParsing -TimeoutSec 3 -ErrorAction Stop
+            $APIResponse = Invoke-RestMethod "http://api.yiimp.eu/api/wallet?address=$($Wallets.$Payout_Currency)" -UseBasicParsing -TimeoutSec 3 -ErrorAction Stop
         }
         catch {
             Start-Sleep -Seconds $RetryDelay # Pool might not like immediate requests
@@ -55,18 +55,18 @@ $Payout_Currencies | Foreach-Object {
         $RetryCount--
     }
 
-    if (($APIRequest | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
+    if (($APIResponse | Get-Member -MemberType NoteProperty -ErrorAction Ignore | Measure-Object Name).Count -le 1) {
         Write-Log -Level Warn "Pool Balance API ($Name) for $Payout_Currency has failed. "
         
     }
     else {
         [PSCustomObject]@{
-            Name        = "$($Name) ($($APIRequest.currency))"
+            Name        = "$($Name) ($($APIResponse.currency))"
             Pool        = $Name
-            Currency    = $APIRequest.currency
-            Balance     = $APIRequest.balance
-            Pending     = $APIRequest.unsold
-            Total       = $APIRequest.unpaid
+            Currency    = $APIResponse.currency
+            Balance     = $APIResponse.balance
+            Pending     = $APIResponse.unsold
+            Total       = $APIResponse.unpaid
             LastUpdated = (Get-Date).ToUniversalTime()
         }
     }
