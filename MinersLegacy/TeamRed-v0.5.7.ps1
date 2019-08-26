954374b079d66fb6e16fef0d9f8d6b825e672b5c
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-08-26 08:10:18 +0200

    Update TeamRed-v0.5.7.ps1
    Fix $Miner_Config.CommonCommands

--- MinersLegacy/TeamRed-v0.5.7.ps1
+++ MinersLegacy/TeamRed-v0.5.7.ps1
@@ -45,7 +45,7 @@ $Commands = [PSCustomObject[]]@(
 if ($Miner_Config.Commands) {$Miner_Config.Commands | ForEach-Object {$Algorithm = $_.Algorithm; $Commands = $Commands | Where-Object {$_.Algorithm -ne $Algorithm}; $Commands += $_}}
 
 #CommonCommands from config file take precedence
-if ($Miner_Config.CommonCommands) {$CommonCommands = $Miner_Config.CommonCommands = $Miner_Config.CommonCommands}
+if ($Miner_Config.CommonCommands) {$CommonCommands = $Miner_Config.CommonCommands}
 else {$CommonCommands = " --watchdog_script"}
 
 $Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "Advanced Micro Devices, Inc.")
