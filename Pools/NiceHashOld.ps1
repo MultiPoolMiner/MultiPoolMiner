e5ed90d6281c3bb8fa1b60e60de98a49d8d0f878
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-08-26 13:56:12 +0200

    Update NiceHashOld.ps1
    CryptonightV7 does not work in Europe

--- Pools/NiceHashOld.ps1
+++ Pools/NiceHashOld.ps1
@@ -64,7 +64,7 @@ $APIRequest.result.simplemultialgo | Where-Object {$_.paying -gt 0} <# algos pay
         $Region = $_
         $Region_Norm = Get-Region $Region
         
-        $Payout_Currencies | Where-Object {$Region -ne "eu" -and $Algorithm_Norm -ne "CryptoNightV7"<#Temp fix, No CryptonightV7 orders in Europe#>} | ForEach-Object {
+        $Payout_Currencies | Where-Object {-not ($Region -eq "eu" -and $Algorithm_Norm -eq "CryptoNightV7"<#Temp fix, No CryptonightV7 orders in Europe#>)} | ForEach-Object {
             [PSCustomObject]@{
                 Algorithm     = $Algorithm_Norm
                 CoinName      = $CoinName
