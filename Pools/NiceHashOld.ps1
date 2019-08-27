515447dfc0a58d857389c9837b400b4c77c96c03
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-08-27 20:07:00 +0200

    Update NiceHashOld.ps1
    - Fix formatting
    - Fix special Nicehash algos

--- Pools/NiceHashOld.ps1
+++ Pools/NiceHashOld.ps1
@@ -2,19 +2,19 @@
 
 param(
     [TimeSpan]$StatSpan,
-    [PSCustomObject]$Config
+    [PSCustomObject]$Config #to be removed
 )
 
-$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+$PoolName = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 
 #Pool currenctly allows payout in BTC only
-$Payout_Currencies = @("BTC") | Where-Object {$Config.Pools.$Name.Wallets.$_}
+$Payout_Currencies = @("BTC") | Where-Object { $Config.Pools.$PoolName.Wallets.$_ }
 
 $PoolRegions = "eu", "usa", "hk", "jp", "in", "br"
 $PoolAPIUri = "http://api.nicehash.com/api?method=simplemultialgo.info"
 
 if (-not $Payout_Currencies) {
-    Write-Log -Level Verbose "No wallet address for Pool ($Name) specified. Cannot mine on pool. "
+    Write-Log -Level Verbose "No wallet address for Pool ($PoolName) specified. Cannot mine on pool. "
     return
 }
 
@@ -22,47 +22,49 @@ $RetryCount = 3
 $RetryDelay = 2
 while (-not ($APIRequest) -and $RetryCount -gt 0) {
     try {
-        if (-not $APIRequest) {$APIRequest = Invoke-RestMethod $PoolAPIUri -UseBasicParsing -TimeoutSec 3 -ErrorAction Stop}
+        if (-not $APIRequest) { $APIRequest = Invoke-RestMethod $PoolAPIUri -UseBasicParsing -TimeoutSec 3 -ErrorAction Stop }
     }
     catch {
         Start-Sleep -Seconds $RetryDelay
-        $RetryCount--        
+        $RetryCount--
     }
 }
 
 if (-not $APIRequest) {
-    Write-Log -Level Warn "Pool API ($Name) has failed. "
+    Write-Log -Level Warn "Pool API ($PoolName) has failed. "
     return
 }
 
 if ($APIRequest.result.simplemultialgo.count -le 1) {
-    Write-Log -Level Warn "Pool API ($Name) returned nothing. "
+    Write-Log -Level Warn "Pool API ($PoolName) returned nothing. "
     return
 }
 
-if ($Config.Pools.$Name.IsInternalWallet) {$Fee = 0.01} else {$Fee = 0.03}
+if ($Config.Pools.$PoolName.IsInternalWallet) { $Fee = 0.01 } else { $Fee = 0.03 }
 
-$APIRequest.result.simplemultialgo | Where-Object {$_.paying -gt 0} <# algos paying 0 fail stratum #> | ForEach-Object {
+Write-Log -Level Verbose "Processing pool data ($PoolName). "
+$APIRequest.result.simplemultialgo | Where-Object { $_.paying -gt 0 } <# algos paying 0 fail stratum #> | ForEach-Object {
 
     $PoolHost = "nicehash.com"
     $Port = $_.port
     $Algorithm = $_.name
     $Algorithm_Norm = Get-Algorithm $Algorithm
     $CoinName = ""
-    
-    if ($Algorithm_Norm -eq "Decred") {$Algorithm_Norm = "DecredNiceHash"} #temp fix
-    if ($Algorithm_Norm -eq "Mtp")    {$Algorithm_Norm = "MtpNiceHash"} #temp fix
-    if ($Algorithm_Norm -eq "Sia")    {$Algorithm_Norm = "SiaNiceHash"} #temp fix
+
+    if ($Algorithm -eq "Beam") { $Algorithm_Norm = "EquihashR15050" } #temp fix
+    if ($Algorithm -eq "Decred") { $Algorithm_Norm = "DecredNiceHash" } #temp fix
+    if ($Algorithm -eq "Mtp") { $Algorithm_Norm = "MtpNiceHash" } #temp fix
+    if ($Algorithm -eq "Sia") { $Algorithm_Norm = "SiaNiceHash" } #temp fix
 
     $Divisor = 1000000000
 
-    $Stat = Set-Stat -Name "$($Name)_$($Algorithm_Norm)_Profit" -Value ([Double]$_.paying / $Divisor) -Duration $StatSpan -ChangeDetection $true
+    $Stat = Set-Stat -Name "$($PoolName)_$($Algorithm_Norm)_Profit" -Value ([Double]$_.paying / $Divisor) -Duration $StatSpan -ChangeDetection $true
 
     $PoolRegions | ForEach-Object {
         $Region = $_
         $Region_Norm = Get-Region $Region
-        
-        $Payout_Currencies | ForEach-Object {
+
+        $Payout_Currencies | Where-Object { -not ($Region -eq "eu" -and $Algorithm_Norm -eq "CryptoNightV7"<#Temp fix, No CryptonightV7 orders in Europe#>) } | ForEach-Object {
             [PSCustomObject]@{
                 Algorithm     = $Algorithm_Norm
                 CoinName      = $CoinName
@@ -72,7 +74,7 @@ $APIRequest.result.simplemultialgo | Where-Object {$_.paying -gt 0} <# algos pay
                 Protocol      = "stratum+tcp"
                 Host          = "$Algorithm.$Region.$PoolHost"
                 Port          = $Port
-                User          = "$($Config.Pools.$Name.Wallets.$_).$($Config.Pools.$Name.Worker)"
+                User          = "$($Config.Pools.$PoolName.Wallets.$_).$($Config.Pools.$PoolName.Worker)"
                 Pass          = "x"
                 Region        = $Region_Norm
                 SSL           = $false
@@ -90,7 +92,7 @@ $APIRequest.result.simplemultialgo | Where-Object {$_.paying -gt 0} <# algos pay
                     Protocol      = "stratum+ssl"
                     Host          = "$Algorithm.$Region.$PoolHost"
                     Port          = $Port + 30000
-                    User          = "$($Config.Pools.$Name.Wallets.$_).$($Config.Pools.$Name.Worker)"
+                    User          = "$($Config.Pools.$PoolName.Wallets.$_).$($Config.Pools.$PoolName.Worker)"
                     Pass          = "x"
                     Region        = $Region_Norm
                     SSL           = $true
