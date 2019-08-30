4caeb6e979fba2516024c3d7c0eab9dd45ce9637
Aaron Simmonds <aaronsace@multipoolminer.io>
2019-08-30 12:31:03 +0100

    Fix API

--- MultiPoolMiner.ps1
+++ MultiPoolMiner.ps1
@@ -569,7 +569,8 @@ while (-not $API.Stop) {
 
     #Load the stats, to improve performance only read PowerUsage stats when required
     Write-Log "Loading saved statistics. "
-    if ($API) { $API.Stats = Get-Stat } #Give API access to the current stats
+    Get-Stat | Out-Null #temp fix for legacy miners
+    if ($API) { $API.Stats = $Stats } #Give API access to the current stats
 
     #Load information about the miners
     #Messy...?
