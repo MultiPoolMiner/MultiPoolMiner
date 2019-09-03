cbccc38e1701044ae0657930c580b63d257787e4
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-09-03 21:44:29 +0200

    Fix stat management
    Problem:
    Stat Management in API cannot access $Global:Stats and therefor not remove data from it.
    
    But it can delete stat files. :-)
    -> Lines 531 - 533 strip deleted stat file data from $Global:Stats

--- Include.psm1
+++ Include.psm1
@@ -525,9 +525,13 @@ function Get-Stat {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $false)]
-        [String[]]$Name = @($Global:Stats.Name | Select-Object) + @(Get-ChildItem "Stats" -ErrorAction Ignore | Select-Object -ExpandProperty BaseName)
+        [String[]]$Name = (Get-ChildItem "Stats" -ErrorAction Ignore | Select-Object -ExpandProperty BaseName)
     )
 
+    if ($Name.Count -gt 1) {
+        $Global:Stats | Get-Member -MemberType NoteProperty -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Name | Where-Object {$_ -notin $Name} | ForEach-Object {$Global:Stats.PSObject.Properties.Remove($_)}
+    }
+    
     $Name | Sort-Object -Unique | ForEach-Object { 
         $Stat_Name = $_
         if (-not $Global:Stats.$Stat_Name) { 
@@ -580,7 +584,7 @@ function Remove-Stat {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $false)]
-        [String[]]$Name = @($Global:Stats.Name | Select-Object) + @(Get-ChildItem "Stats" -ErrorAction Ignore | Select-Object -ExpandProperty BaseName)
+        [String[]]$Name = @(if ($Global:Stats) {$Global:Stats | Get-Member -MemberType NoteProperty | Select-Object | Select-Object -ExpandProperty Name}) + @(Get-ChildItem "Stats" -ErrorAction Ignore | Select-Object -ExpandProperty BaseName)
     )
 
     $Name | Sort-Object -Unique | ForEach-Object { 
