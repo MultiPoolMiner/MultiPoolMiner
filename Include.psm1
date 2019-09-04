91918ec5e0cfcc131fe310c0f459d84f9721196b
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-09-04 10:07:25 +0200

    Update Include.psm1

--- Include.psm1
+++ Include.psm1
@@ -584,7 +584,7 @@ function Remove-Stat {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $false)]
-        [String[]]$Name = @($Global:Stats | Get-Member -MemberType NoteProperty -ErrorAction SilentlyIgnore | Select-Object | Select-Object -ExpandProperty Name) + @(Get-ChildItem "Stats" -ErrorAction Ignore | Select-Object -ExpandProperty BaseName)
+        [String[]]$Name = @($Global:Stats | Get-Member -MemberType NoteProperty -ErrorAction SilentlyContinue | Select-Object | Select-Object -ExpandProperty Name) + @(Get-ChildItem "Stats" -ErrorAction Ignore | Select-Object -ExpandProperty BaseName)
     )
 
     $Name | Sort-Object -Unique | ForEach-Object { 
