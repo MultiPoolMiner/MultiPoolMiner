3d61b11d0e1502de91ecfb55f9d335090f63b71e
Useless Guru <useless.guru@gmail.com>
2019-09-25 12:51:47 +0200

    Update CcminerZenemy-v2.2

--- MinersLegacy/CcminerZenemy-v2.2.ps1
+++ MinersLegacy/CcminerZenemy-v2.2.ps1
@@ -9,6 +9,8 @@ param(
 
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Path = ".\Bin\$($Name)\z-enemy.exe"
+$HashSHA256 = "AB5E17C1C876E35B81FFEC1123864CD6D37E4304918DCF592CB1AFAA6539031C"
+$Uri = "https://github.com/MultiPoolMiner/miner-binaries/releases/download/Zenemy/z-enemy-2.2-cuda10.1.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=3378390.0"
 
 $Miner_BaseName = $Name -split '-' | Select-Object -Index 0
@@ -18,53 +20,46 @@ if (-not $Miner_Config) {$Miner_Config = $Config.MinersLegacy.$Miner_BaseName."*
 
 $Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "NVIDIA Corporation")
 
-# Miner requires CUDA 9.2.00 or higher
+# Miner requires CUDA 10.1.00 or higher
 $CUDAVersion = ($Devices.OpenCL.Platform.Version | Select-Object -Unique) -replace ".*CUDA ",""
-$RequiredCUDAVersion = "9.2.00"
+$RequiredCUDAVersion = "10.1.00"
 if ($CUDAVersion -and [System.Version]$CUDAVersion -lt [System.Version]$RequiredCUDAVersion) {
     Write-Log -Level Warn "Miner ($($Name)) requires CUDA version $($RequiredCUDAVersion) or above (installed version is $($CUDAVersion)). Please update your Nvidia drivers. "
     return
 }
 
-if ($CUDAVersion -lt [System.Version]("10.0.0")) {
-    $HashSHA256 = "AE09498A48CF075153CCA06BB1597CB1237BFB8FD3668743387A055767D692BD"
-    $Uri = "https://github.com/MultiPoolMiner/miner-binaries/releases/download/Zenemy/z-enemy-2.1-cuda9.2.zip"
-}
-else {
-    $HashSHA256 = "66C636BD6B34E5C803748D9224C8448F8EEB4167A46688D04E6112C24C69947D"
-    $Uri = "https://github.com/MultiPoolMiner/miner-binaries/releases/download/Zenemy/z-enemy-2.1-cuda10.0.zip"
-}
-
 $Commands = [PSCustomObject]@{
-    "aergo"      = " --algo=aergo" #Aergo, new in 1.11
-    "bitcore"    = " --algo=bitcore" #Timetravel10 and Bitcore are technically the same
-    "bcd"        = " --algo=bcd" #Bitcoin Diamond, new in 1.20
-    "c11"        = " --algo=c11 --intensity=26 --statsavg 10" #C11, new in 1.11
-    "hex"        = " --algo=hex" #Hex
-    "phi"        = " --algo=phi_" #PHI
-    "phi2"       = " --algo=phi2" #Phi2
-    "poly"       = " --algo=poly" #Polytimos
-    "skunk"      = " --algo=skunk" #Skunk, new in 1.11
-    "sonoa"      = " --algo=sonoa" #SONOA, new in 1.12
-    "timetravel" = " --algo=timetravel" #Timetravel
-    "tribus"     = " --algo=tribus" #Tribus, new in 1.10
-    "x16r"       = " --algo=x16r --statsavg=50" #Raven, number of samples used to compute hashrate (default: 30) 
-    "x16s"       = " --algo=x16s" #Pigeon
-    "x17"        = " --algo=x17" #X17
-    "xevan"      = " --algo=xevan --intensity=26" #Xevan, new in 1.09a
+    "Aergo"      = " --algo=aergo" #Aergo, new in 1.11
+    "Bitcore"    = " --algo=bitcore" #Timetravel10 and Bitcore are technically the same
+    "Bcd"        = " --algo=bcd" #Bitcoin Diamond, new in 1.20
+    "C11"        = " --algo=c11 --intensity=26 --statsavg 10" #C11, new in 1.11
+    "Hex"        = " --algo=hex" #Hex
+    "Phi"        = " --algo=phi" #PHI
+    "Phi2"       = " --algo=phi2" #Phi2
+    "Phi2-Lux"   = " --algo=phi2" #Phi2-Lux
+    "Polytimos"  = " --algo=poly" #Polytimos
+    "Skunk"      = " --algo=skunk" #Skunk, new in 1.11
+    "Sonoa"      = " --algo=sonoa --intensity 26" #SONOA, new in 1.12
+    "Timetravel" = " --algo=timetravel" #Timetravel
+    "Tribus"     = " --algo=tribus" #Tribus, new in 1.10
+    "X16r"       = " --algo=x16r --statsavg=50" #Raven, number of samples used to compute hashrate (default: 30) 
+    "X16s"       = " --algo=x16s" #Pigeon
+    "X16rv2"     = " --algo=x16rcv2" #New with 2.2
+    "X17"        = " --algo=x17" #X17
+    "Xevan"      = " --algo=xevan --intensity=26" #Xevan, new in 1.09a
 }
 #Commands from config file take precedence
 if ($Miner_Config.Commands) {$Miner_Config.Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {$Commands | Add-Member $_ $($Miner_Config.Commands.$_) -Force}}
 
 #CommonCommands from config file take precedence
 if ($Miner_Config.CommonCommands) {$CommonCommands = $Miner_Config.CommonCommands}
-else {$CommonCommands = ""}
+else {$CommonCommands = " --statsavg 10"}
 
 $Devices | Select-Object Model -Unique | ForEach-Object {
     $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model)
     $Miner_Port = $Config.APIPort + ($Miner_Device | Select-Object -First 1 -ExpandProperty Index) + 1
 
-    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {$Algorithm_Norm = Get-Algorithm $_; $_} | Where-Object {$Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
+    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {$Algorithm_Norm = Get-Algorithm $_; $_} | Where-Object {$Pools.$Algorithm_Norm.Host}| ForEach-Object {
         $Miner_Name = (@($Name) + @($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object {$Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm"}) | Select-Object) -join '-'
 
         #Get commands for active miner devices
@@ -84,7 +79,7 @@ $Devices | Select-Object Model -Unique | ForEach-Object {
             DeviceName = $Miner_Device.Name
             Path       = $Path
             HashSHA256 = $HashSHA256
-            Arguments  = ("$Command$CommonCommands --api-bind=127.0.0.1:$($Miner_Port) --api-bind-http=0 --url=$($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) --user=$($Pools.$Algorithm_Norm.User) --pass=$($Pools.$Algorithm_Norm.Pass) --devices=$(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ',')" -replace "\s+", " ").trim()
+            Arguments  = ("$Command$CommonCommands --api-bind=127.0.0.1:$($Miner_Port) --api-bind-http=0 --url=$($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port)$(if ($Pools.$Algorithm_Norm.SSL) { " --no-cert-verify" }) --user=$($Pools.$Algorithm_Norm.User) --pass=$($Pools.$Algorithm_Norm.Pass) --devices=$(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ',')" -replace "\s+", " ").trim()
             HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
             API        = "Ccminer"
             Port       = $Miner_Port
