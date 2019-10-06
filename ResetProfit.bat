6eaf0f92e4118b69f099193886e4849573709d56
Useless Guru <useless.guru@gmail.com>
2019-10-06 08:01:37 +0200

    Fix cleanup files

--- ResetProfit.bat
+++ ResetProfit.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-set /p statreset= This process will remove all accumulated coin data and reset your profit statistics. Are you sure you want to continue? [Y/N] 
+set /p statreset=This process will remove all accumulated coin data and reset your profit statistics. Are you sure you want to continue? [Y/N] 
 IF /I "%statreset%"=="Y" (
 	if exist "Stats\*Profit.txt" del "Stats\*Profit.txt"
 	if exist "Stats\Profit\*Profit.txt" del "Stats\Profit\*Profit.txt"
