6dbd7dea0e462c7bdda735ad2b322b06002e00fc
Useless Guru <30080938+UselessGuru@users.noreply.github.com>
2019-10-06 08:06:23 +0200

    Update ResetPowerUsage.bat

--- ResetPowerUsage.bat
+++ ResetPowerUsage.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d %~dp0
-set /p powerusagereset= This process will remove all power usage data. Are you sure you want to continue? [Y/N] 
+set /p powerusagereset=This process will remove all power usage data. Are you sure you want to continue? [Y/N] 
 IF /I "%powerusagereset= %"=="Y" (
 	if exist "Stats\*_PowerUsage.txt" del "Stats\*_PowerUsage.txt"
 	if exist "Stats\PowerUsage\*_PowerUsage.txt" del "Stats\PowerUsage\*_PowerUsage.txt"
