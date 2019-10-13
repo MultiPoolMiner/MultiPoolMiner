48ebc993305b3dccae3f0b6475fadae9dcdc174c
Useless Guru <useless.guru@gmail.com>
2019-10-13 12:17:28 +0200

    Update CcminerTrex-v0.14.6

--- MinersLegacy/CcminerTrex-v0.14.6.ps1
+++ MinersLegacy/CcminerTrex-v0.14.6.ps1
@@ -9,16 +9,16 @@ param(
 
 $Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
 $Path = ".\Bin\$($Name)\t-rex.exe"
-$HashSHA256 = "8481F8DB9F7CA2F2A1828337A13CFF2BFD680A5A78CC68565F7B65D339042272"
-$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.14.4/t-rex-0.14.4-win-cuda10.0.zip"
+$HashSHA256 = "350711A14786388296DC0465B7E9A96470A84C456B72E64A0153E27C7024AD67"
+$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.14.6/t-rex-0.14.6-win-cuda10.0.zip"
 $ManualUri = "https://bitcointalk.org/index.php?topic=4432704.0"
 
 $Miner_BaseName = $Name -split '-' | Select-Object -Index 0
 $Miner_Version = $Name -split '-' | Select-Object -Index 1
 $Miner_Config = $Config.MinersLegacy.$Miner_BaseName.$Miner_Version
-if (-not $Miner_Config) {$Miner_Config = $Config.MinersLegacy.$Miner_BaseName."*"}
+if (-not $Miner_Config) { $Miner_Config = $Config.MinersLegacy.$Miner_BaseName."*" }
 
-$Commands = [PSCustomObject]@{
+$Commands = [PSCustomObject]@{ 
     "AstralHash"  = " -a astralhash -i 23" #GltAstralHash, new in 0.8.6
     "Balloon"     = " -a balloon -i 23" #Balloon, new in 0.6.2
     "BCD"         = " -a bcd -i 24" #BitcoinDiamond, new in 0.6.5
@@ -52,43 +52,43 @@ $Commands = [PSCustomObject]@{
     "X25x"        = " -a x25x -i 21" #X25x, new in 0.11.0
 }
 #Commands from config file take precedence
-if ($Miner_Config.Commands) {$Miner_Config.Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {$Commands | Add-Member $_ $($Miner_Config.Commands.$_) -Force}}
+if ($Miner_Config.Commands) { $Miner_Config.Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Commands | Add-Member $_ $($Miner_Config.Commands.$_) -Force } }
 
 #CommonCommands from config file take precedence
-if ($Miner_Config.CommonCommands) {$CommonCommands = $Miner_Config.CommonCommands}
-else {$CommonCommands = " --no-watchdog"}
+if ($Miner_Config.CommonCommands) { $CommonCommands = $Miner_Config.CommonCommands }
+else { $CommonCommands = " --no-watchdog" }
 
 $Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "NVIDIA Corporation")
-$Devices | Select-Object Model -Unique | ForEach-Object {
+$Devices | Select-Object Model -Unique | ForEach-Object { 
     $Miner_Device = @($Devices | Where-Object Model -EQ $_.Model)
     $Miner_Port = $Config.APIPort + ($Miner_Device | Select-Object -First 1 -ExpandProperty Index) + 1
 
-    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {$Algorithm_Norm = Get-Algorithm $_; $_} | Where-Object {$Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" <#temp fix#>} | ForEach-Object {
-        $Miner_Name = (@($Name) + @($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object {$Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm"}) | Select-Object) -join '-'
+    $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object { $Algorithm_Norm = Get-Algorithm $_; $_ } | Where-Object { $Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" <#temp fix#> } | ForEach-Object { 
+        $Miner_Name = (@($Name) + @($Miner_Device.Model_Norm | Sort-Object -unique | ForEach-Object { $Model_Norm = $_; "$(@($Miner_Device | Where-Object Model_Norm -eq $Model_Norm).Count)x$Model_Norm" }) | Select-Object) -join '-'
 
         #Get commands for active miner devices
         $Command = Get-CommandPerDevice -Command $Commands.$_ -ExcludeParameters @("a", "algo") -DeviceIDs $Miner_Device.Type_Vendor_Index
 
-        Switch ($Algorithm_Norm) {
-            "C11"         {$WarmupTime = 60}
-            "MTP"         {$WarmupTime = 60}
-            "MTPNicehash" {$WarmupTime = 60}
-            default       {$WarmupTime = 45}
+        Switch ($Algorithm_Norm) { 
+            "C11"         { $WarmupTime = 60 }
+            "MTP"         { $WarmupTime = 60 }
+            "MTPNicehash" { $WarmupTime = 60 }
+            default       { $WarmupTime = 45 }
         }
 
-        [PSCustomObject]@{
+        [PSCustomObject]@{ 
             Name       = $Miner_Name
             BaseName   = $Miner_BaseName
             Version    = $Miner_Version
             DeviceName = $Miner_Device.Name
             Path       = $Path
             HashSHA256 = $HashSHA256
-            Arguments  = ("$Command$CommonCommands -b 127.0.0.1:$($Miner_Port) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) -d $(($Miner_Device | ForEach-Object {'{0:x}' -f ($_.Type_Vendor_Index)}) -join ',')" -replace "\s+", " ").trim()
-            HashRates  = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week}
+            Arguments  = ("$Command$CommonCommands -b 127.0.0.1:$($Miner_Port) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) -d $(($Miner_Device | ForEach-Object { '{0:x}' -f ($_.Type_Vendor_Index) }) -join ',')" -replace "\s+", " ").trim()
+            HashRates  = [PSCustomObject]@{ $Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week }
             API        = "Ccminer"
             Port       = $Miner_Port
             URI        = $Uri
-            Fees       = [PSCustomObject]@{$Algorithm_Norm = 1 / 100}
+            Fees       = [PSCustomObject]@{ $Algorithm_Norm = 1 / 100 }
             WarmupTime = $WarmupTime
         }
     }
