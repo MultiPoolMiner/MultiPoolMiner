b22d8ef7e27c948b7980afe2c2b48faaf15d5824
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2020-01-30 12:48:43 +1300

    Create Ccminersha3d-v1.0.ps1

---
+++ MinersLegacy/Ccminersha3d-v1.0.ps1
@@ -0,0 +1,56 @@
+using module ..\Include.psm1
+
+param(
+    [PSCustomObject]$Pools,
+    [PSCustomObject]$Stats,
+    [PSCustomObject]$Config,
+    [PSCustomObject[]]$Devices
+)
+
+$Name = "$(Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName)"
+$Path = ".\Bin\$($Name)\ccminer.exe"
+$HashSHA256 = "B5A6FDF60D644F807CB2BECD4206EA4C78CA03DDECBFA67263A8AF705A955E71"
+$Uri = "https://github.com/Minerx117/ccminer-bsha3/releases/download/1.0/ccminersha3dV1.zip"
+$ManualUri = "https://github.com/BSHA3/ccminer-bsha3"
+
+$Miner_Config = Get-MinerConfig -Name $Name -Config $Config
+
+$Commands = [PSCustomObject[]]@(
+    [PSCustomObject]@{ Algorithm = "sha3d"; MinMemGB = 4; Command = " -a keccak -i 27" } #Sha3d
+)
+#Commands from config file take precedence
+if ($Miner_Config.Commands) { $Miner_Config.Commands | ForEach-Object { $Algorithm = $_.Algorithm; $Commands = $Commands | Where-Object { $_.Algorithm -ne $Algorithm }; $Commands += $_ } }
+
+#CommonCommands from config file take precedence
+if ($Miner_Config.CommonCommands) { $CommonCommands = $Miner_Config.CommonCommands }
+else { $CommonCommands = "" }
+
+$Devices = @($Devices | Where-Object Type -EQ "GPU" | Where-Object Vendor -EQ "NVIDIA")
+$Devices | Select-Object Model -Unique | ForEach-Object { 
+    $Device = @($Devices | Where-Object Model -EQ $_.Model)
+    $Miner_Port = [UInt16]($Config.APIPort + ($Device | Select-Object -First 1 -ExpandProperty Id) + 1)
+
+    $Commands | ForEach-Object { $Algorithm_Norm = Get-Algorithm $_.Algorithm; $_ } | Where-Object { $Pools.$Algorithm_Norm.Protocol -eq "stratum+tcp" <#temp fix#> } | ForEach-Object { 
+        $MinMemGB = $_.MinMemGB
+
+        if ($Miner_Device = @($Device | Where-Object { ([math]::Round((10 * $_.OpenCL.GlobalMemSize / 1GB), 0) / 10) -ge $MinMemGB })) { 
+            $Miner_Name = (@($Name) + @($Miner_Device.Model | Sort-Object -unique | ForEach-Object { $Model = $_; "$(@($Miner_Device | Where-Object Model -eq $Model).Count)x$Model" }) | Select-Object) -join '-'
+
+            #Get commands for active miner devices
+            $Command = Get-CommandPerDevice -Command $_.Command -ExcludeParameters @("a", "algo") -DeviceIDs $Miner_Device.Type_Vendor_Index
+
+            [PSCustomObject]@{ 
+                Name       = $Miner_Name
+                DeviceName = $Miner_Device.Name
+                Path       = $Path
+                HashSHA256 = $HashSHA256
+                Arguments  = ("$Command$CommonCommands -b 127.0.0.1:$($Miner_Port) -o $($Pools.$Algorithm_Norm.Protocol)://$($Pools.$Algorithm_Norm.Host):$($Pools.$Algorithm_Norm.Port) -u $($Pools.$Algorithm_Norm.User) -p $($Pools.$Algorithm_Norm.Pass) -d $(($Miner_Device | ForEach-Object { '{0:x}' -f ($_.Type_Vendor_Index) }) -join ',')" -replace "\s+", " ").trim()
+                HashRates  = [PSCustomObject]@{ $Algorithm_Norm = $Stats."$($Miner_Name)_$($Algorithm_Norm)_HashRate".Week }
+                API        = "Ccminer"
+                Port       = $Miner_Port
+                URI        = $Uri
+                WarmupTime = 60 #seconds
+            }
+        }
+    }
+}
